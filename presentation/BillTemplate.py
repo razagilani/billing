0f616ad03e3889ec1f30e84b7617362303c1c8fc
Rich Andrews <stuart.r.andrews@gmail.com>
2010-01-14 15:53:58 -0500

    interim commit after some cleanup but before introducing xpath

--- presentation/BillTemplate.py
+++ presentation/BillTemplate.py
@@ -1,4 +1,4 @@
-from reportlab.platypus import BaseDocTemplate, Paragraph, Spacer, Image, PageTemplate, Frame, PageBreak, NextPageTemplate
+from reportlab.platypus import BaseDocTemplate, Paragraph, Table, Spacer, Image, PageTemplate, Frame, PageBreak, NextPageTemplate
 from reportlab.platypus.flowables import UseUpSpace
 
 from reportlab.lib.styles import getSampleStyleSheet,ParagraphStyle
@@ -10,15 +10,29 @@ from pprint import pprint
 from reportlab.pdfgen import canvas
 from reportlab.pdfgen.pathobject import PDFPathObject
 
+
+
+
+import os  
+import reportlab  
+from reportlab.pdfbase import pdfmetrics  
+from reportlab.pdfbase.ttfonts import TTFont  
+from reportlab.pdfgen.canvas import Canvas  
+
+
+
+#
+#  Load Fonts
+#
+# add your font directories to the T1SearchPath in reportlab/rl_config.py as an alternative.
+folder = os.path.dirname(reportlab.__file__) + os.sep + 'fonts'  
+ttfFile = os.path.join(folder, 'Vera.ttf') 
+pdfmetrics.registerFont(TTFont("Vera", ttfFile))  
+
+
+
 class SIBillDocTemplate(BaseDocTemplate):
     """Structure Skyline Innovations Bill. """
-    #_invalidInitArgs = ('pageTemplates',)
-
-    #def handle_pageBegin(self):
-    #    '''override base method to add a change of page template after the firstpage.
-    #    '''
-    #    self._handle_pageBegin()
-    #    self._handle_nextPageTemplate('Later')
 
     def build(self,flowables, canvasmaker=canvas.Canvas):
         """build the document using the flowables.  Annotate the first page using the onFirstPage
@@ -33,14 +47,7 @@ class SIBillDocTemplate(BaseDocTemplate):
                footers, etcetera. They can use external variables to vary
                the look (for example providing page numbering or section names).
         """
-        #self._calc()    #in case we changed margins sizes etc
-        #frameT = Frame(self.leftMargin, self.bottomMargin, self.width, self.height, id='normal')
-        #self.addPageTemplates([PageTemplate(id='First',frames=frameT, onPage=onFirstPage,pagesize=self.pagesize),
-        #                PageTemplate(id='Later',frames=frameT, onPage=onLaterPages,pagesize=self.pagesize)])
-        #if onFirstPage is _doNothing and hasattr(self,'onFirstPage'):
-        #    self.pageTemplates[0].beforeDrawPage = self.onFirstPage
-        #if onLaterPages is _doNothing and hasattr(self,'onLaterPages'):
-        #    self.pageTemplates[1].beforeDrawPage = self.onLaterPages
+ 
         BaseDocTemplate.build(self,flowables, canvasmaker=canvasmaker)
         
     def beforePage(self):
@@ -48,7 +55,14 @@ class SIBillDocTemplate(BaseDocTemplate):
         
     def afterPage(self):
         print "After Page"
-	self.canv.line(0,0,100,100)
+        if self.pageTemplate.id == firstPageName:
+            self.canv.saveState()
+            self.canv.setStrokeColorRGB(255.0,255.0,255.0)
+            self.canv.setLineWidth(.2)
+            self.canv.setDash(1,3)
+            self.canv.line(0,265,612,265)
+            self.canv.line(0,550,612,550)
+            self.canv.restoreState()
         
         
     def handle_pageBegin(self):
@@ -56,6 +70,8 @@ class SIBillDocTemplate(BaseDocTemplate):
         BaseDocTemplate.handle_pageBegin(self)
 
 
+firstPageName = "FirstPage"
+secondPageName = "SecondPage"
 
 defaultPageSize = letter
 PAGE_HEIGHT=letter[1]; PAGE_WIDTH=letter[0]
@@ -90,45 +106,74 @@ def progress(type,value):
 def go():
 
     styles = getSampleStyleSheet()
-    styles.add(ParagraphStyle(name='BillLabel', fontName='Helvetica', fontSize=10, leading=1))
+    styles.add(ParagraphStyle(name='BillLabel', fontName='Vera', fontSize=10, leading=1))
     style = styles["BillLabel"]
     pprint(dir(style))
     pprint(dir(styles["Normal"]))
 
 
-    # 612 792
-    backgroundF = Frame(0,0, letter[0], letter[1], leftPadding=0, bottomPadding=0, rightPadding=0, topPadding=0, id="background", showBoundary=0)
+    showBoundaries = 1
 
-    billIssueDateF = Frame(50, 700, 130, 6, leftPadding=0, bottomPadding=0, rightPadding=0, topPadding=0, id="billIssueDate", showBoundary=1)
-    billDueDateF = Frame(180, 700, 140, 6, leftPadding=0, bottomPadding=0, rightPadding=0, topPadding=0, id="billDueDate", showBoundary=1)
+    # 612w 792h
+    backgroundF = Frame(0,0, letter[0], letter[1], leftPadding=0, bottomPadding=0, rightPadding=0, topPadding=0, id="background", showBoundary=showBoundaries)
 
-    contentFrame = Frame(100,100, 200,200, leftPadding=10, bottomPadding=10, rightPadding=10, topPadding=10, showBoundary=1)
-    serviceAddrFrame = Frame(200,200, 300, 300, leftPadding=10, bottomPadding=10, rightPadding=10, topPadding=10, showBoundary=1)
+    # bill dates block
+    billIssueDateF = Frame(90, 690, 110, 6, leftPadding=0, bottomPadding=0, rightPadding=0, topPadding=0, id="billIssueDate", showBoundary=showBoundaries)
+    billDueDateF = Frame(210, 690, 130, 6, leftPadding=0, bottomPadding=0, rightPadding=0, topPadding=0, id="billDueDate", showBoundary=showBoundaries)
+    billPeriodTableF = Frame(90, 640, 250, 40, leftPadding=0, bottomPadding=0, rightPadding=0, topPadding=0, id="billDueDate", showBoundary=showBoundaries)
 
-    firstPage = PageTemplate(id="FirstPage",frames=[backgroundF, billIssueDateF, billDueDateF])
+
+    #
+    chargesWithoutF = Frame(360, 690, 125, 6, leftPadding=0, bottomPadding=0, rightPadding=0, topPadding=0, id="chargesWithout", showBoundary=showBoundaries)
+    chargesWithF = Frame(500, 690, 80, 6, leftPadding=0, bottomPadding=0, rightPadding=0, topPadding=0, id="chargesWith", showBoundary=showBoundaries)
+    chargesTableF = Frame(360, 640, 220, 40, leftPadding=0, bottomPadding=0, rightPadding=0, topPadding=0, id="chargesWith", showBoundary=showBoundaries)
+
+    contentFrame = Frame(100,100, 200,200, leftPadding=10, bottomPadding=10, rightPadding=10, topPadding=10, showBoundary=showBoundaries)
+    serviceAddrFrame = Frame(200,200, 300, 300, leftPadding=10, bottomPadding=10, rightPadding=10, topPadding=10, showBoundary=showBoundaries)
+
+    firstPage = PageTemplate(id=firstPageName,frames=[backgroundF, billIssueDateF, billDueDateF, billPeriodTableF, chargesWithoutF, chargesWithF, chargesTableF])
 
 
-    rbackgroundFrame = Frame(400,400, 100, 100, leftPadding=0, bottomPadding=0, rightPadding=0, topPadding=0, showBoundary=1)
-    rcontentFrame = Frame(600,600, 100,100, leftPadding=10, bottomPadding=10, rightPadding=10, topPadding=10, showBoundary=1)
+    rbackgroundFrame = Frame(400,400, 100, 100, leftPadding=0, bottomPadding=0, rightPadding=0, topPadding=0, showBoundary=showBoundaries)
+    rcontentFrame = Frame(600,600, 100,100, leftPadding=10, bottomPadding=10, rightPadding=10, topPadding=10, showBoundary=showBoundaries)
 
-    secondPage = PageTemplate(id="SecondPage",frames=[rbackgroundFrame, rcontentFrame])
+    secondPage = PageTemplate(id=secondPageName,frames=[backgroundF, rcontentFrame])
 
     doc = SIBillDocTemplate("bill.pdf", pagesize=letter, showBoundary=0, allowSplitting=0)
     doc.addPageTemplates([firstPage, secondPage])
 
     Elements = []
 
+
+    # First page
     image = Image("images/EmeraldCityBackground.png",letter[0], letter[1])
     Elements.append(image)
 
-    Elements.append(Paragraph("<b>Bill Issued:</b> MMM DD YYYY", style))
+    Elements.append(Paragraph("<b>Bill Issued</b> XX/XX/XX", style))
     Elements.append(UseUpSpace())
 
-    Elements.append(Paragraph("<b>Bill Due Date:</b> MMM DD YYYY", style))
-    #Elements.append(UseUpSpace())
+    Elements.append(Paragraph("<b>Bill Due Date</b> XX/XX/XX", style))
+    Elements.append(UseUpSpace())
+
+    Elements.append(Table([["Service A Period", "XX/XX/XX", "XX/XX/XX"], ["Service B Period", "XX/XX/XX", "XX/XX/XX"]], [75,75,50]))
+    Elements.append(UseUpSpace())
+
+
+    Elements.append(Paragraph("<u>Charges Without Skyline</u>", style))
+    Elements.append(UseUpSpace())
+
+    Elements.append(Paragraph("<u>With Skyline</u>", style))
+    Elements.append(UseUpSpace())
+
+    Elements.append(Table([["Service A", "$100.00", "$50.00"], ["Service B", "$200.00", "$65.00"]], [75,75,50]))
+    Elements.append(UseUpSpace())
+
+
 
     Elements.append(NextPageTemplate("SecondPage"));
     Elements.append(PageBreak());
+
+    Elements.append(image)
 
     Elements.append(Paragraph("Content Frame", style))
     #Elements.append(UseUpSpace())
