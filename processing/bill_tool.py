939d5dacd7447aa18f86f5db19005a5f8c7ce564
Rich Andrews <stuart.r.andrews@gmail.com>
2010-07-19 18:46:26 -0400

    tweaks to model and implementation of bill rolling (partial)

--- processing/bill_tool.py
+++ processing/bill_tool.py
@@ -15,14 +15,11 @@ from optparse import OptionParser
 from urlparse import urlparse
 
 # for xml processing
-import amara
-from amara import bindery
+from lxml import etree
 
-#
-# Globals
-#
-user=None
-password=None
+
+# for testing
+import StringIO
 
 class BillTool():
     """ Class with a variety of utility procedures for processing bills """
@@ -30,16 +27,122 @@ class BillTool():
     def __init__(self):
         pass
 
-    def roll_bill(self, prevbill, nextbill, amountPaid):
+    def get_elem(self, tree, xpath):
+        return tree.xpath(xpath, namespaces={"ub":"bill"})
+
+    def roll_bill(self, prevbill, nextbill, amountPaid, user=None, password=None):
+
         # Bind to XML bill
-        dom = bindery.parse(prevbill)
-         
+        tree = etree.parse(prevbill)
+        #print etree.tostring(tree, pretty_print=True)
+
+        # increment bill id
+        bill = self.get_elem(tree, "/ub:bill")[0]
+        newId = int(bill.get("id"))
+        bill.set("id", str(newId+1))
+
+        # process /ub:bill/ub:rebill
+
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:totaladjustment")[0].text = "0.00"
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:hypotheticalecharges")[0].clear()
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:actualecharges")[0].clear()
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:revalue")[0].clear()
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:recharges")[0].clear()
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:resavings")[0].clear()
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:duedate")[0].clear()
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:issued")[0].clear()
+
+        message = self.get_elem(tree, "/ub:bill/ub:rebill/ub:message")
+        if(len(message)):
+            message.clear()
+
+        # next period begin is prev period end
+        # ToDo business logic specific dates are selected here
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:billperiodbegin")[0].text = \
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:billperiodend")[0].text
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:billperiodend")[0].clear()
+
+        # compute payments 
+        totalDue = self.get_elem(tree, "/ub:bill/ub:rebill/ub:totaldue")[0].text
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:totaldue")[0].clear()
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:priorbalance")[0].text = totalDue
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:paymentreceived")[0].text = str(amountPaid)
+        self.get_elem(tree, "/ub:bill/ub:rebill/ub:balanceforward")[0].text = str(float(totalDue) - amountPaid)
+
+
+        # process /ub:bill/ub:utilbill
+
+        # utility billing periods are likely utility specific
+        self.get_elem(tree, "/ub:bill/ub:utilbill/ub:billperiodbegin")[0].text = \
+        self.get_elem(tree, "/ub:bill/ub:utilbill/ub:billperiodend")[0].text
+        self.get_elem(tree, "/ub:bill/ub:utilbill/ub:billperiodend")[0].clear()
+
+        services = self.get_elem(tree, "/ub:bill/ub:utilbill")
+
+        # process each utility service and clear values
+        for service in services:
+            self.get_elem(service, "ub:billperiodbegin")[0].clear()
+            self.get_elem(service, "ub:billperiodend")[0].clear()
+            self.get_elem(service, "ub:hypotheticalecharges")[0].clear()
+            self.get_elem(service, "ub:actualecharges")[0].clear()
+            self.get_elem(service, "ub:revalue")[0].clear()
+            self.get_elem(service, "ub:recharges")[0].clear()
+            self.get_elem(service, "ub:resavings")[0].clear()
+
+        # process /ub:bill/ub:details/
+
+        subtotals = self.get_elem(tree, "/ub:bill/ub:details/ub:total")
+        for subtotal in subtotals:
+            # set text to "" since we don't want to clobber the element attributes
+            subtotal.text = ""
+
+
+        # remove cdata but preserve unit attributes (.clear() clears attrs) -  last period actual charge values 
+        actualCharges = self.get_elem(tree, "/ub:bill/ub:details/ub:chargegroup/ub:charges[@type='actual']")
+        for actualCharge in actualCharges:
+            quantities = self.get_elem(actualCharge, "ub:charge/ub:quantity")
+            for quantity in quantities:
+                quantity.text = ""
+            rates = self.get_elem(actualCharge, "ub:charge/ub:rate")
+            for rate in rates:
+                rate.text = ""
+            totals = self.get_elem(actualCharge, "ub:charge/ub:total")
+            for total in totals: 
+                total.text = ""
+            # clear chargegroup totals
+            totals = self.get_elem(actualCharge, "ub:total")
+            for total in totals:
+                total.clear()
+
+        # remove the hypothetical charges since they will be recreated from the actual charges
+        hypotheticalCharges = self.get_elem(tree, "/ub:bill/ub:details/ub:chargegroup/ub:charges[@type='hypothetical']")
+
+        for hypotheticalCharge in hypotheticalCharges:
+            hypotheticalCharge.getparent().remove(hypotheticalCharge)
+
+
+        # reset measured usage
+
+        # set meter read date
+        # ToDo: utility specific behavior
+        self.get_elem(tree, "/ub:bill/ub:measuredusage/ub:meter/ub:priorreaddate")[0].text = \
+        self.get_elem(tree, "/ub:bill/ub:measuredusage/ub:meter/ub:presentreaddate")[0].text
+        self.get_elem(tree, "/ub:bill/ub:measuredusage/ub:meter/ub:presentreaddate")[0].clear()
+
+        registerTotals = self.get_elem(tree, "/ub:bill/ub:measuredusage/ub:meter/ub:register/ub:total")
+        for registerTotal in registersTotals:
+            registerTotal.clear()
+
+        print etree.tostring(tree, pretty_print=True)
+
+        
         # roll the bill
 
-        xml = dom.xml_encode()
 
         # determine URI scheme
         parts = urlparse(nextbill)
+
+        xml = etree.tostring(tree, pretty_print=True)
 
         # nice then to factor out and put into a common lib
         if (parts.scheme == 'http'): 
@@ -87,5 +190,4 @@ if __name__ == "__main__":
         print "Next bill must be specified"
         exit()
 
-    username=options.user
-    password=options.password
+    roll_bill(options.prevbill, options.nextbill, options.amountpaid, options.user, options.password,)
