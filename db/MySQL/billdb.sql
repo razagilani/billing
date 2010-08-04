ca3634a2e6aa30a6b2d26305ff6b4cdebde889ba
Rich Andrews <stuart.r.andrews@gmail.com>
2010-08-04 11:27:31 -0400

    File moving/cleanup

---
+++ db/MySQL/billdb.sql
@@ -0,0 +1,23 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `mydb`;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`bill`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`bill` ;
+
+CREATE  TABLE IF NOT EXISTS `mydb`.`bill` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `uri` VARCHAR(256) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
