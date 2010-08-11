8bb4b4662ba448dd506d08a073d4270f5be7e1ce
Rich Andrews <stuart.r.andrews@gmail.com>
2010-08-11 15:06:43 -0400

    stub test in anticipation of the implementation. :-)

--- test/test_bill_tool/test_bill_tool.py
+++ test/test_bill_tool/test_bill_tool.py
@@ -22,9 +22,9 @@ class TestBillTool(unittest.TestCase):
 
     def test_roll_bill_1(self):
         """ Test rolling a previous bill into the next bill"""
-        prevBill = "test/test_bill_tool/roll_bill_1_pre.xml"
-        nextBill = "test/test_bill_tool/roll_bill_1_in.xml"
-        correctBill = "test/test_bill_tool/roll_bill_1_post.xml"
+        prevBill = os.path.join("test", "test_bill_tool", "roll_bill_1_pre.xml")
+        nextBill = os.path.join("test", "test_bill_tool", "roll_bill_1_in.xml")
+        correctBill = os.path.join("test", "test_bill_tool", "roll_bill_1_post.xml")
 
         BillTool().roll_bill(prevBill, nextBill, 19.99)
 
@@ -33,12 +33,29 @@ class TestBillTool(unittest.TestCase):
 
         (result, reason) = XMLUtils().compare_xml(etree_in, etree_post)
 
-        print result
-        print reason
-
         self.assertEquals(result, True, "Bill roll failed because " + nextBill + " does not match " + correctBill)
         
         os.remove(nextBill)
+
+    def test_hypothetical_charge_summation(self):
+        """ For all of the hypothetical charges in <ub:details/>, test that they properly roll up into """
+        """ <ub:utilbill/> and <ub:rebill/>"""
+
+        unprocessedBill = os.path.join("test", "test_bill_tool", "hypo_charge_summation_1_pre.xml")
+        resultantBill = os.path.join("test", "test_bill_tool", "hypo_charge_summation_1_in.xml")
+        correctBill = os.path.join("test", "test_bill_tool", "hypo_charge_summation_1_post.xml")
+
+        BillTool().sum_hypothetical_charges(unprocessedBill, resultantBill)
+
+        etree_in = etree.parse(resultantBill)
+        etree_post = etree.parse(correctBill)
+
+        (result, reason) = XMLUtils().compare_xml(etree_in, etree_post)
+
+        self.assertEquals(result, True, "Hypothetical charges did not totalize properly because " + 
+            resultantBill + " does not match " + correctBill)
+
+        os.remove(resultantBill)
 
 if __name__ == '__main__':
     unittest.main()
