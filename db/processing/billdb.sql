48c7cbc1aac393dead737a885247545256e46b25
Rich Andrews <stuart.r.andrews@gmail.com>
2010-09-13 19:57:48 -0400

    The correct database this time

--- db/processing/billdb.sql
+++ db/processing/billdb.sql
@@ -1,149 +1,121 @@
--- MySQL dump 10.13  Distrib 5.1.39, for Win32 (ia32)
---
--- Host: localhost    Database: skyline
--- ------------------------------------------------------
--- Server version	5.1.39-community
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE SCHEMA IF NOT EXISTS `skyline` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+SHOW WARNINGS;
+USE `skyline` ;
 
---
--- Table structure for table `address`
---
+-- -----------------------------------------------------
+-- Table `skyline`.`customer`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `skyline`.`customer` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NOT NULL ,
+  `account` VARCHAR(45) NOT NULL ,
+  `discountrate` DECIMAL(3,2) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `address`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `address` (
-  `id` int(11) NOT NULL,
-  `AddressType_id` int(11) NOT NULL,
-  `Customer_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_Address_AddressType1` (`AddressType_id`),
-  KEY `fk_Address_Customer1` (`Customer_id`),
-  CONSTRAINT `fk_Address_AddressType1` FOREIGN KEY (`AddressType_id`) REFERENCES `addresstype` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Address_Customer1` FOREIGN KEY (`Customer_id`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+SHOW WARNINGS;
 
---
--- Dumping data for table `address`
---
+-- -----------------------------------------------------
+-- Table `skyline`.`rebill`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `skyline`.`rebill` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `uri` VARCHAR(1024) NOT NULL ,
+  `sequence` INT NOT NULL ,
+  `customer_id` INT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_rebill_customer` (`customer_id` ASC) ,
+  CONSTRAINT `fk_rebill_customer`
+    FOREIGN KEY (`customer_id` )
+    REFERENCES `skyline`.`customer` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-LOCK TABLES `address` WRITE;
-/*!40000 ALTER TABLE `address` DISABLE KEYS */;
-/*!40000 ALTER TABLE `address` ENABLE KEYS */;
-UNLOCK TABLES;
+SHOW WARNINGS;
 
---
--- Table structure for table `addresstype`
---
+-- -----------------------------------------------------
+-- Table `skyline`.`ledger`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `skyline`.`ledger` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `date` DATE NOT NULL ,
+  `description` VARCHAR(45) NULL ,
+  `debit` DECIMAL(6,2) NULL ,
+  `credit` DECIMAL(6,2) NULL ,
+  `customer_id` INT NOT NULL ,
+  `rebill_id` INT NULL ,
+  PRIMARY KEY (`id`, `customer_id`) ,
+  INDEX `fk_ledger_customer` (`customer_id` ASC) ,
+  INDEX `fk_ledger_bill` (`rebill_id` ASC) ,
+  CONSTRAINT `fk_ledger_customer`
+    FOREIGN KEY (`customer_id` )
+    REFERENCES `skyline`.`customer` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ledger_bill`
+    FOREIGN KEY (`rebill_id` )
+    REFERENCES `skyline`.`rebill` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `addresstype`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `addresstype` (
-  `id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+SHOW WARNINGS;
 
---
--- Dumping data for table `addresstype`
---
+-- -----------------------------------------------------
+-- Table `skyline`.`utility`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `skyline`.`utility` (
+  `id` INT NOT NULL ,
+  `identifier` VARCHAR(45) NOT NULL ,
+  `name` VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
 
-LOCK TABLES `addresstype` WRITE;
-/*!40000 ALTER TABLE `addresstype` DISABLE KEYS */;
-/*!40000 ALTER TABLE `addresstype` ENABLE KEYS */;
-UNLOCK TABLES;
+SHOW WARNINGS;
 
---
--- Table structure for table `customer`
---
+-- -----------------------------------------------------
+-- Table `skyline`.`utilbill`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `skyline`.`utilbill` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `customer_id` INT NULL ,
+  `rebill_id` INT NULL ,
+  `uri` VARCHAR(1024) NULL ,
+  `period_start` DATE NOT NULL ,
+  `period_end` DATE NOT NULL ,
+  `estimated` TINYINT(1)  NULL ,
+  `processed` TINYINT(1)  NULL ,
+  `received` TINYINT(1)  NULL ,
+  `utility_id` INT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_utilbill_customer` (`customer_id` ASC) ,
+  INDEX `fk_utilbill_rebill` (`rebill_id` ASC) ,
+  INDEX `fk_utilbill_utility` () ,
+  CONSTRAINT `fk_utilbill_customer`
+    FOREIGN KEY (`customer_id` )
+    REFERENCES `skyline`.`customer` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_utilbill_rebill`
+    FOREIGN KEY (`rebill_id` )
+    REFERENCES `skyline`.`rebill` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_utilbill_utility`
+    FOREIGN KEY ()
+    REFERENCES `skyline`.`utility` ()
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `customer` (
-  `id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+SHOW WARNINGS;
 
---
--- Dumping data for table `customer`
---
 
-LOCK TABLES `customer` WRITE;
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `system`
---
-
-DROP TABLE IF EXISTS `system`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `system` (
-  `id` int(11) NOT NULL,
-  `Customer_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`,`Customer_id`),
-  KEY `fk_System_Customer` (`Customer_id`),
-  CONSTRAINT `fk_System_Customer` FOREIGN KEY (`Customer_id`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `system`
---
-
-LOCK TABLES `system` WRITE;
-/*!40000 ALTER TABLE `system` DISABLE KEYS */;
-/*!40000 ALTER TABLE `system` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `useraccount`
---
-
-DROP TABLE IF EXISTS `useraccount`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `useraccount` (
-  `id` int(11) NOT NULL,
-  `Customer_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_UserAccount_Customer1` (`Customer_id`),
-  CONSTRAINT `fk_UserAccount_Customer1` FOREIGN KEY (`Customer_id`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `useraccount`
---
-
-LOCK TABLES `useraccount` WRITE;
-/*!40000 ALTER TABLE `useraccount` DISABLE KEYS */;
-/*!40000 ALTER TABLE `useraccount` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2010-09-13 19:46:35
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
