9524a6544cacd9a4e3cc38b70687c328c7c4172c
Rich Andrews <stuart.r.andrews@gmail.com>
2010-09-22 17:02:12 -0400

    latest billing

--- processing/bill_tool.py
+++ processing/bill_tool.py
@@ -266,14 +266,14 @@ if __name__ == "__main__":
         print "Output bill must be specified"
         exit()
 
-    if (options.inputbill == options.outputbill):
-        print "Input bill and output bill should not match!"
-        exit()
 
     if (options.roll == None and options.sumhypothetical == None and options.sumactual == None):
         print "Specify operation"
 
     if (options.roll):
+        if (options.inputbill == options.outputbill):
+            print "Input bill and output bill should not match!"
+            exit()
         if (options.amountpaid == None):
             print "Specify --amountpaid"
         else:
