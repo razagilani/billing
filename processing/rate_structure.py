a439cc6378cea0d24356f3220b1b9e3b8d3f13fd
Rich Andrews <stuart.r.andrews@gmail.com>
2010-09-30 17:10:14 -0400

    Provisional commmit - reasonably complex rate structure evaluates.

--- processing/rate_structure.py
+++ processing/rate_structure.py
@@ -36,21 +36,6 @@ class rate_structure(yaml.YAMLObject):
             # so that RSIs have access to this objects namespace
             rsi.ratestructure = self
 
-        #cycle_flag = 0
-        #jvisited = {}
-        #def walk_rsis(self, rsi):
-        #    assert(visited[rsi] == None)
-        #    visited[rsi] = rsi
-
-        
-
-
-        #for rsi in self.rates:
-        #    visited[rsi] = rsi
-            
-        #    cycle_flag += 1
-
-
     def evaluate_rsi(self):
         """ Ask each rate_structure_item to evaluate itself """
         self.configure()
@@ -82,8 +67,11 @@ class rate_structure_item(yaml.YAMLObject):
     #    return "%s(descriptor=%r, description=%r, quantity=%r, rate=%r, total=%r)" % (
     #        self.__class__.__name__, self.descriptor, self.description, self.quantity, self.rate, self.total)
 
+
+    # hack that allows python code fragments to recursively evaluate, however there is no good means to check for cycles
     def __getattribute__(self, name):
-        # object. call supermethod, preventing unexpected recursion here
+
+        # object.__geta... call supermethod to preventing unexpected recursion here
         if (name == 'quantity'):
             #print str(type(object.__getattribute__(self, 'quantity'))) + str(object.__getattribute__(self, 'quantity'))
             # if the quantity is a string, an expression, eval it
@@ -98,11 +86,10 @@ class rate_structure_item(yaml.YAMLObject):
             if (type(object.__getattribute__(self, 'total')) == str):
                 # assign eval results to attribute if re-evaluation doesn't have to be done. self.total = eval(...)
                 return eval(object.__getattribute__(self, 'total'), object.__getattribute__(self, 'ratestructure').__dict__)
+            if (type(object.__getattribute__(self, 'total')) == type(None)):
+                # total is not set, therefore, evaluate it.
+                self.total = self.quantity * self.rate
             # otherwise just return the total since it does not have to be evaluated
             return object.__getattribute__(self, 'total')
         else:
             return object.__getattribute__(self, name)
-
-    def depends(self):
-        """ Return a list of other RSIs needed for calculating this one. """
-
