f018298b538f6212bdcdf6f190a2d5066bfab44d
Rich Andrews <stuart.r.andrews@gmail.com>
2011-04-21 15:31:55 -0400

    updates to facilitate deploying bill_tool_bridge

--- presentation/fabfile-billtoolbridge.py
+++ presentation/fabfile-billtoolbridge.py
@@ -12,15 +12,18 @@ exclude_from = 'fabexcludes.txt'
 
 def prepare_deploy():
     fabops.local('tar czvf /tmp/%s.tar.z --exclude-from=%s --exclude-caches-all --exclude-vcs ../%s' % (project_name, exclude_from, project_name))
-    fabops.local('tar czvf /tmp/billing_processing.tar.z ../bill.py ../__init__.py ../processing/process.py ../processing/__init__.py ../processing/state.py')
+    fabops.local('tar czvf /tmp/billing_processing.tar.z ../nexus_util.py ../bill.py ../__init__.py ../processing/process.py ../processing/__init__.py ../processing/state.py ../processing/fetch_bill_data.py ../processing/rate_structure.py')
+    fabops.local('tar czvf /tmp/billing_presentation.tar.z ../presentation/__init__.py ../presentation/render.py ../presentation/images ../presentation/fonts')
 
 def deploy():
     prepare_deploy()
     fabapi.put('/tmp/%s.tar.z' % (project_name), '/tmp')
     fabapi.put('/tmp/billing_processing.tar.z', '/tmp')
+    fabapi.put('/tmp/billing_presentation.tar.z', '/tmp')
     fabops.sudo('cd /var/local/ && tar xvzf /tmp/%s.tar.z' % (project_name, ), user='root')
     fabops.sudo('if [ -d /var/local/billtool/lib/python2.6/site-packages/billing ]; then echo "Directory exists"; else mkdir /var/local/billtool/lib/python2.6/site-packages/billing; fi') 
     fabops.sudo('cd /var/local/billtool/lib/python2.6/site-packages/billing/ && tar xvzf /tmp/billing_processing.tar.z', user='root')
+    fabops.sudo('cd /var/local/billtool/lib/python2.6/site-packages/billing/ && tar xvzf /tmp/billing_presentation.tar.z', user='root')
     # hack until we clean up the presentation directory
     fabops.sudo('cp -r /var/local/%s/* /var/local/billtool' % (project_name, ), user='root')
     fabops.sudo('chown -R billtool:billtool /var/local/billtool', user='root')
