b579d2a38fd7c163b300e422ce9905557ad4ef69
Daniel Klothe <dklothe@skylineinnovations.com>
2011-07-20 18:53:58 -0400

    cleaned up commented out raw SQL code in StateDB; it's all SQLAlchemy now

--- processing/state.py
+++ processing/state.py
@@ -11,7 +11,6 @@ from db import Customer, UtilBill, ReeBill
 import sqlalchemy
 
 # TODO: make configurable
-# TODO: clean up commented-out raw SQL code
 
 class StateDB:
 
@@ -22,13 +21,6 @@ class StateDB:
         pass
 
     def commit_bill(self, account, sequence, start, end):
-        '''
-        #query = "call commit_bill(%s, %s, %s, %s)"
-        query = "update utilbill set rebill_id = (select id from rebill where customer_id = (select id from customer where account = %s) and sequence = %s), processed = true where customer_id = (select id from customer where account = %s) and period_start >= %s and period_end <= %s"
-        params = (account, sequence, account, start, end)
-        return self.fetch(query, params, False)
-        '''
-
         # get customer id from account and the reebill from account and sequence
         customer_id = db.session.query(Customer.id).filter(Customer.account==account).one()[0]
         reebill_id = db.session.query(ReeBill.id).filter(ReeBill.customer_id==customer_id)\
@@ -49,27 +41,10 @@ class StateDB:
 
 
     def discount_rate(self, account):
-        '''
-        query = "select discountrate from customer where account = %s"
-        params = (account)
-        rows = self.fetch(query, params)
-        # TODO: error checking...
-        return rows[0]['discountrate']
-       ''' 
         # one() raises an exception if more than one row was found
         return db.session.query(Customer).filter_by(account=account).one().discountrate
 
     def last_sequence(self, account):
-        '''
-        query = "select max(sequence) as maxseq from rebill where customer_id = (select id from customer where account = %s)"
-        params = (account)
-        rows = self.fetch(query, params)
-        # TODO: because of the way 0.xml templates are made (they are not in the database) rebill needs to be 
-        # primed otherwise the last sequence for a new bill is None. Design a solution to this issue.
-        if rows[0]['maxseq'] is None:
-            return 0
-        return rows[0]['maxseq']
-        '''
         customer_id = db.session.query(Customer.id).filter(Customer.account==account).one()[0]
         max_sequence = db.session.query(sqlalchemy.func.max(ReeBill.sequence)) \
                 .filter(ReeBill.customer_id==customer_id).one()[0]
@@ -80,37 +55,21 @@ class StateDB:
         return max_sequence
         
     def new_rebill(self, account, sequence):
-        '''
-        query = "insert into rebill (id, sequence, customer_id, issued) values (null, %s, (select id from customer where account = %s),false)" 
-        params = (sequence, account)
-        # TODO: error checking...
-        rows = self.fetch(query, params, False)
-        '''
         # TODO: is there a way to do this without multiple database transactions?
         customer_id = db.session.query(Customer.id).filter(Customer.account==account)
         new_reebill = ReeBill(sequence, customer_id, issued)
         db.session.add(new_reebill)
         db.commit()
-        '''Apparently this is called only by Process.zero_charges(), which is never called by anybody. Remove it?'''
+        '''Apparently this is called only by Process.zero_charges(), which is never called by anybody. TODO: remove it?'''
 
     def issue(self, account, sequence):
-        '''
-        query = "update rebill set issued = 1 where sequence = %s and customer_id = (select id from customer where account = %s)"
-        params = (sequence, account)
-        # TODO: error checking...
-        rows = self.fetch(query, params, False)
-        '''
         customer_id = db.session.query(Customer.id).filter(Customer.account==account).one()[0]
         reeBill = db.session.query(ReeBill).filter(ReeBill.customer_id==customer_id).filter(ReeBill.sequence==sequence).one()
         reeBill.issued = 1
         db.session.commit()
 
-
     def listAccounts(self):
-        '''
-        query = "select account from customer"
-        return self.fetch(query, None, True)
-        '''
+        # SQLAlchemy returns a list of tuples, so convert it into a plain list
         return map((lambda x: x[0]), db.session.query(Customer.account).all())
 
     def listSequences(self, account):
@@ -120,7 +79,7 @@ class StateDB:
         customer_id = db.session.query(Customer.id).filter(Customer.account==account).one()[0]
         sequences = db.session.query(ReeBill.sequence).filter(ReeBill.customer_id==customer_id).all()
 
-        # sequences is a list of tuples of numbers, so convert it into a plain list of strings
+        # sequences is a list of tuples of numbers, so convert it into a plain list
         return map((lambda x: x[0]), sequences)
 
 
@@ -128,28 +87,15 @@ class StateDB:
     end date of bills in a slice of the utilbills table; returns the slice and the
     total number of rows in the table (for paging).'''
     def getUtilBillRows(self, start, limit):
-        '''
-        # get appropriate slice of table
-        query = "select account, period_start, period_end from customer, utilbill where customer.id = utilbill.customer_id limit %s,%s"
-        params = (start, limit)
-
-        theSlice = self.fetch(query, params, True)
-        
-        # count total number of rows in the whole table (not just the slice)
-        # note: this must be the exact same query as above, not just "select
-        # count(*) from utilbill", which would count rows in utilbill that
-        # have null ids even though they don't show up in the query above.
-        query = "select count(*) as count from customer, utilbill where customer.id = utilbill.customer_id"
-
-        # TODO any one thing goes wrong, this statement will fail - test values/types more diligently
-        totalCount = int(self.fetch(query, None)[0]['count'])
-        
-        # return the slice (dictionary) and the overall count (integer)
-        return theSlice, totalCount
-        '''
+        # SQLAlchemy query to get account & dates for all utilbills
         query = db.session.query(Customer.account, UtilBill.period_start, \
                 UtilBill.period_end).filter(UtilBill.customer_id==Customer.id)
+
+        # SQLAlchemy does SQL 'limit' with Python list slicing
         slice = query[start:start + limit]
+
+        # count total number of utilbills (note that some rows in utilbill may
+        # have null customer_ids, even though that's not supposed to happen)
         count = query.count()
         return slice, count
 
