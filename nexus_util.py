220d0b372428f9c758a2a77dc9011d45b177b36c
Daniel Klothe <dklothe@skylineinnovations.com>
2011-08-16 17:42:56 -0400

    modified utilbill_summary_charges in bill.py to return CAR data along with other utilbill information

---
+++ nexus_util.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+import urllib, urllib2
+import json
+
+from optparse import OptionParser
+
+class NexusUtil(object):
+
+    def __init__(self, host = "nexus"):
+        super(NexusUtil,self).__init__()
+        self.host = host
+
+    def olap_id(self, bill_account):
+        """ For a billing account number, return an olap_id """
+
+        url = "http://%s/nexus_query/lookup?system=billing&systemid=%s&forsystem=olap" % (
+            self.host,
+            bill_account
+        )
+
+        f = urllib2.urlopen(url)
+
+        return f.read()
+
+    def all(self, system, system_id):
+
+        url = "http://%s/nexus_query/lookup_all?system=%s&systemid=%s" % (
+            self.host,
+            system,
+            system_id
+        )
+        f = urllib2.urlopen(url)
+        record = json.load(f)
+
+        if record["total_rows"] > 0:
+            return record["rows"][0]
+        else:
+            return {}
+
+if __name__ == "__main__":
+    parser = OptionParser()
+
+    parser.add_option("--host", dest="host", help="Host where Nexus ReSTful service may be found.")
+
+    parser.add_option("--billaccount", dest="billaccount", help="Account number for Billing System")
+    parser.add_option("--system", dest="system", help="Name of the system to be queried")
+    parser.add_option("--systemid", dest="systemid", help="Identifier of the system to be queried")
+
+    (options, args) = parser.parse_args()
+
+    if (options.billaccount):
+        print NexusUtil(options.host).olap_id(options.billaccount)
+        exit()
+
+    if (options.system and options.systemid):
+        print NexusUtil(options.host).all(options.system, options.systemid)
+        exit()
+        
+
+
+
