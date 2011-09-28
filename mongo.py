4b4ab94c6eb33ccd7e5d84d9bb8f0fb664a99048
Rich Andrews <stuart.r.andrews@gmail.com>
2011-09-28 13:00:44 -0400

    new program to convert reebills from exist to mongo

--- mongo.py
+++ mongo.py
@@ -951,33 +951,3 @@ class ReebillDAO:
         else:
             pass
 
-# config data to be passed into ReebillDAO constructor when debugging
-debug_config = {
-    "host":"localhost", 
-    "port":27017, 
-    "database":"skyline", 
-    "collection":"reebills", 
-    "destination_prefix":"http://localhost:8080/exist/rest/db/skyline/bills"
-}
-
-if __name__ == '__main__':
-    dao = ReebillDAO(debug_config)
-    reebill = dao.load_reebill("10002","12")
-    dao.save_reebill(reebill)
-
-    success_count = 0
-    error_count = 0
-    for account in range(10001, 10025):
-        for sequence in range(1,20):
-            try:
-                reebill = dao.load_reebill(account, sequence)
-                dao.save_reebill(reebill)
-                success_count += 1
-            except AttributeError as e:
-                print '%s %s: %s' % (account, sequence,
-                        'AttributeError ' + str(e))
-                error_count += 1
-            except IOError:
-                pass
-    print 'imported %s bills' % success_count
-    print error_count, 'errors'
