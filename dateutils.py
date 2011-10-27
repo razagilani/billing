670a1a7d30232a03fbc74c20347a684d8acf2819
Daniel Klothe <dklothe@skylineinnovations.com>
2011-10-27 18:22:22 -0400

    moved date-related code out of fetch_bill_data.py into month.py, which is now called dateutils.py. also some small fixes in the holiday-related functions

--- dateutils.py
+++ dateutils.py
@@ -1,7 +1,8 @@
 '''Month-related utility functions. estimate_month() can be used for deciding
 which calendar month to associate with a utility billing period.'''
 import calendar
-from datetime import date
+from datetime import date, timedelta
+#from datetime import date, datetime,timedelta, time
 import unittest
 
 def days_in_month(year, month, start_date, end_date):
@@ -147,5 +148,90 @@ class MonthTest(unittest.TestCase):
         # start & end very far apart: prefer first month with 31 days
         self.assertEquals(estimate_month(jul15, aug122012), (2011, 8))
 
+
+
+
+
+def date_generator(from_date, to_date):
+    """Yield dates based on from_date up to and excluding to_date.  The reason
+    for the exclusion of to_date is that utility billing periods do not include
+    the whole day for the end date specified for the period.  That is, the
+    utility billing period range of 2/15 to 3/4, for example, is for the usage
+    at 0:00 2/15 to 0:00 3/4.  0:00 3/4 is before the 3/4 begins."""
+    if (from_date > to_date):
+        return
+    while from_date < to_date:
+        yield from_date
+        from_date = from_date + timedelta(days = 1)
+    return
+
+def nth_weekday(n, weekday_number, month):
+    '''Returns a function mapping years to the 'n'th weekday of 'month' in the
+    given year, so "nth weekday of month"-style holidays (like "3rd monday of
+    February") can be defined without dealing with the insanity of the Python
+    calendar library. 'n' is a 1-based number or 'last'; weekday_number is
+    0-based starting at Sunday.'''
+    cal = calendar.Calendar()
+    def result(year):
+        # calendar.itermonthdays2() returns (day number, weekday number)
+        # tuples, where days outside the month are included (with day number =
+        # 0) to get a complete week. as if that weren't bad enough, weekdays
+        # are 0-indexed starting at monday (european-style, apparently). also
+        # note that calendar.setfirstweekday(calendar.SUNDAY) has no effect.
+        days = [day[0] for day in cal.itermonthdays2(year, month)
+                if day[0] != 0 and day[1] == (weekday_number + 6) % 7]
+        return date(year, month, days[-1 if n == 'last' else n-1])
+    return result
+
+# for time-of-use billing, holidays are stored as functions mapping a year to a
+# date within the year: this allows potentially arbitarily rules (such as
+# holiday dates changing from one year to another) with the reasonable
+# assumption that a holiday occurs at most once per year (you could return None
+# if it does not occur at all).
+# for simplicity, we're assuming that utility billing holidays match defined
+# federal holiday dates, not the dates on which federal employees get a
+# vacation. this decision is not based on actual data, so we may have to
+# correct it later.
+
+# the names (values in the dictionary below) just serve as documentation.
+HOLIDAYS = {
+    # fixed-date holidays: date is independent of year
+    lambda year: date(year, 1, 1) : "New Year's Day",
+    lambda year: date(year, 7, 4) : "Independence Day",
+    lambda year: date(year, 11, 11) : "Veterans' Day",
+    lambda year: date(year, 12, 25) : "Christmas Day",
+
+    # "nth weekday of month" holidays: nth_weekday(n, weekday, month)
+    nth_weekday(3, 1, 1): "Martin Luther King Day",
+    nth_weekday(3, 1, 2): "Presidents' Day",
+    nth_weekday('last', 1, 5): "Memorial Day",
+    nth_weekday(1, 1, 9): "Labor Day",
+    nth_weekday(2, 1, 10): "Columbus Day",
+    nth_weekday(4, 5, 11): "Thanksgiving Day"
+}
+
+def all_holidays(year):
+    '''Returns all the holidays in the given year as a list of dates.'''
+    return [holiday(year) for holiday in HOLIDAYS if holiday(year) is not None]
+
+def get_day_type(day):
+    '''Returns 'weekday', 'weekend', or 'holiday' to classify the given date.
+    Holidays override regular weekday/weekend classifications.'''
+    if day in all_holidays(day.year):
+        return 'holiday'
+    # python weeks start on monday
+    if day.weekday() in [5,6]:
+        return 'weekend'
+    return 'weekday'
+    
+
+
 if __name__ == '__main__':
     unittest.main()
+
+    # manual holiday test. check against:
+    # http://www.opm.gov/Operating_Status_Schedules/fedhol/2011.asp
+    # http://www.opm.gov/oca/worksch/html/holiday.asp
+    import pprint
+    pprint.PrettyPrinter().pprint(sorted([(name, holiday(2011)) for holiday, name in HOLIDAYS.iteritems()], key=lambda t:(t[1], t[0])))
+    # TODO unit-test the holiday code
