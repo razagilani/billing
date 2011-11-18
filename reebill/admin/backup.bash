06bc2e2b80cde8674fa4b231102369f78f708fc2
Rich Andrews <stuart.r.andrews@gmail.com>
2011-11-18 18:33:02 -0500

    21159523 - backup and destage scripts

---
+++ reebill/admin/backup.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# backup 
+now=`date +"%Y%m%j"`
+cd /tmp
+mysqldump -uroot -pvLGhTZu9eq4ULvbbKzlE skyline > ${now}billing_mysql.dmp
+mongodump --db skyline-prod --collection ratestructure --out ${now}ratestructure_mongo
+mongodump --db skyline-prod --collection reebills --out ${now}reebills_mongo
+
+# compress
+tar czvf ${now}reebill-prod.tar.z /db-prod ${now}ratestructure_mongo ${now}reebills_mongo
+
+# permanently store a backup every friday
+if [ `date +"%w"` -eq 5 ]; then 
+cp ${now}reebill-prod.tar.z /home/reebill-prod;
+fi
