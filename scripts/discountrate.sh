27126642f2b8dafcef5c8ed4ce6a5aaecac8f13e
Daniel Klothe <dklothe@skylineinnovations.com>
2012-01-19 12:57:42 -0500

    fixed header row in discount csv script

--- scripts/discountrate.sh
+++ scripts/discountrate.sh
@@ -5,5 +5,5 @@ DB=skyline_prod
 USER=reebill-prod
 PW=AXUPU4XGMSN
 QUERY="use $DB; select account, name, discountrate from customer;"
-AWK='BEGIN { print "account,name,discountrate"} NR>1 {printf "%s,%s,%s\n", $1, $2, $3 }'
+AWK='BEGIN { print "Account,Name,Discount Rate"} NR>1 {printf "%s,%s,%s\n", $1, $2, $3 }'
 echo "$QUERY" | mysql -h$HOST -u$USER -p$PW | sort -n | awk -F '\t' "$AWK"
