783785d37761d50f62265cc94d531f489154b654
Daniel Klothe <dklothe@skylineinnovations.com>
2012-01-30 15:40:49 -0500

    switched to static reconciliation report and moved reconciliation report code out of BTB into a script so it can be run by cron

---
+++ scripts/reconciliation.py
@@ -0,0 +1,93 @@
+'''Reconciliation report for reebills.'''
+import os
+import traceback
+from billing import mongo
+from billing.reebill import render
+from billing.processing import state
+from skyliner.splinter import Splinter
+from skyliner.skymap.monguru import Monguru
+from billing.nexus_util import NexusUtil
+from billing import json_util
+from billing import dateutils
+
+# setup
+billdb_config = {
+    'billpath': '/db-dev/skyline/bills/',
+    'database': 'skyline',
+    'utilitybillpath': '/db-dev/skyline/utilitybills/',
+    'collection': 'reebills',
+    'host': 'localhost',
+    'port': '27017'
+}
+statedb_config = {
+    'host': 'tyrell',
+    'password': 'dev',
+    'database': 'skyline_dev',
+    'user': 'dev'
+}
+reebill_dao = mongo.ReebillDAO(billdb_config)
+state_db = state.StateDB(statedb_config)
+session = state_db.session()
+splinter = Splinter('http://duino-drop.appspot.com/', "tyrell", 'dev')
+monguru = Monguru('tyrell', 'dev')
+
+# file where the report goes: json format
+output_file = open(os.path.join(os.path.dirname(os.path.realpath('billing')),
+        'reconciliation_report.json'), 'w')
+
+accounts = sorted(state_db.listAccounts(session))
+# TODO it would be faster to do this sorting in MySQL instead of Python when
+# this list gets long
+
+for account in accounts:
+    install = splinter.get_install_obj_for(NexusUtil().olap_id(account))
+    for sequence in state_db.listSequences(session, account):
+        print 'reconciliation report for %s-%s' % (account, sequence)
+        reebill = reebill_dao.load_reebill(account, sequence)
+        try:
+            # get energy from the bill
+            bill_therms = reebill.total_renewable_energy
+            #oltp_therms = sum(install.get_energy_consumed_by_service(
+                    #day, 'service type is ignored!', [0,23]) for day
+                    #in dateutils.date_generator(reebill.period_begin,
+                    #reebill.period_end))
+
+            # OLTP is more accurate but way too slow to generate this report in a reasonable time
+            
+            # now get energy from OLAP: start by adding up energy
+            # sold for each day, whether billable or not (assuming
+            # that periods of missing data from OLTP will have
+            # contributed 0 to the OLAP aggregate)
+            olap_btu = 0
+            for day in dateutils.date_generator(reebill.period_begin,
+                    reebill.period_end):
+                olap_btu += monguru.get_data_for_day(install,
+                        day).energy_sold
+
+            # now find out how much energy was unbillable by
+            # subtracting energy sold during all unbillable
+            # annotations from the previous total
+            for anno in [anno for anno in install.get_annotations() if
+                    anno.unbillable]:
+                # i think annotation datetimes are in whole hours
+                # and their ends are exclusive
+                for hour in sky_handlers.cross_range(anno._from, anno._to):
+                    hourly_doc = monguru.get_data_for_hour(install, hour)
+                    olap_btu -= hourly_doc.energy_sold
+            olap_therms = olap_btu / 100000
+        except Exception as error:
+            output_file.write(json_util.dumps({
+                'success': False,
+                'account': account,
+                'sequence': sequence,
+                'error': '%s\n%s' % (error, traceback.format_exc())
+            }))
+        else:
+            output_file.write(json_util.dumps({
+                'success': True,
+                'account': account,
+                'sequence': sequence,
+                'bill_therms': bill_therms,
+                'olap_therms': olap_therms
+            }))
+        output_file.write('\n')
