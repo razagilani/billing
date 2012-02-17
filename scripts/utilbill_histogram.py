42276ae2aaaec72012f08de8ea707db33a161eea
Daniel Klothe <dklothe@skylineinnovations.com>
2012-02-17 15:50:28 -0500

    utilbill histogram shows distribution of start/end dates over month

--- scripts/utilbill_histogram.py
+++ scripts/utilbill_histogram.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-'''Prints a histogram of bill period lengths in days using utility bills in the
+'''Prints histograms of bill period lengths in days and the day-of-month
+components of utility bill start and end dates, using utility bills in the
 MySQL database below.'''
 from billing.processing.state import StateDB
 state_db = StateDB({
@@ -8,16 +9,34 @@ state_db = StateDB({
     'user':'stage',
     'password':'stage'
 })
-histogram = {}
+
+print 'Period lenth distribution (days):'
+length_histogram = {}
 total = 0
 count = 0
 for account in state_db.listAccounts(state_db.session()):
     for ub in state_db.list_utilbills(state_db.session(), account)[0]:
         delta = ub.period_end - ub.period_start
-        histogram[delta.days] = histogram.get(delta.days, 0) + 1
+        length_histogram[delta.days] = length_histogram.get(delta.days, 0) + 1
         total += delta.days
         count += 1
 
-for length, num in sorted(histogram.items()):
-    print '%s:\t%s' % (length, num)
-print 'average utility bill period length:', total/float(count)
+least_length = min(length_histogram.items(), key=lambda x: x[0])[0]
+greatest_length = max(length_histogram.items(), key=lambda x: x[0])[0]
+for length in range(least_length, greatest_length+1):
+    count = length_histogram.get(length, 0)
+    print '%3d: %3d %s' % (length, count, '*'*int(count))
+print 'average: %s\n' % (total/float(count))
+
+
+print 'Day-of-month distribution:'
+day_histogram = {}
+for account in state_db.listAccounts(state_db.session()):
+    for ub in state_db.list_utilbills(state_db.session(), account)[0]:
+        start_day = ub.period_start.day
+        end_day = ub.period_end.day
+        for day in (start_day, end_day):
+            day_histogram[day] = day_histogram.get(day, 0) + 1
+for day in range(1,32):
+    count = day_histogram.get(day, 0)
+    print '%3d: %3d %s' % (day, count, '*'*int(count))
