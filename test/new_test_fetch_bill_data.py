134f5fb1e1aa426b7243eb3e76a435f4ade92a7a
Daniel Klothe <dklothe@skylineinnovations.com>
2012-03-05 18:38:33 -0500

    fixed bug in fetch_bill_data.get_interval_meter_data_source when end of query period exactly matches end of csv file's time range. added a test for fetch_interval_meter_data with a real bill

--- test/new_test_fetch_bill_data.py
+++ test/new_test_fetch_bill_data.py
@@ -15,9 +15,10 @@ def make_big_interval_meter_test_csv(start_date, end_date, csv_file):
     Command-line arguments are file name, start date (ISO 8601, inclusive), end
     date (exclusive). E.g:
         make_sample_csv.py interval_meter_sample.csv 2012-11-10 2012-12-12
+    Returns the sum of all energy in the file.
     '''
     writer = csv.writer(csv_file)
-    for day in dateutils.date_generator(start_date, end_date - timedelta(days=1)):
+    for day in dateutils.date_generator(start_date, end_date):
         dt = datetime(day.year, day.month, day.day, 0)
         while dt.day == day.day:
             dt += timedelta(hours=0.25)
@@ -31,7 +32,7 @@ class FetchTest(unittest.TestCase):
     def setUp(self):
         pass
         
-    def test_get_interval_meter_data(self):
+    def test_get_interval_meter_data_source(self):
         csv_file = StringIO('\n'.join([
             # note that 01:00:00 is not included
             '2012-01-01T01:15:00Z, 2, therms',
@@ -45,7 +46,7 @@ class FetchTest(unittest.TestCase):
             '2012-01-01T03:15:00Z, 10, therms',
             '2012-01-01T03:30:00Z, 11, therms',
             '2012-01-01T03:45:00Z, 12, therms',
-        ]))
+            ]))
         get_energy_for_hour = fbd.get_interval_meter_data_source(csv_file)
 
         # outside allowed time range
@@ -62,13 +63,22 @@ class FetchTest(unittest.TestCase):
         self.assertEquals(30, get_energy_for_hour(date(2012,1,1), [2,2]))
         self.assertEquals(44, get_energy_for_hour(date(2012,1,1), [1,2]))
 
+        # a case where the query lines up with the end of the data
+        csv2 = StringIO('\n'.join([
+            '2012-01-01T01:15:00Z, 2, therms',
+            '2012-01-01T01:30:00Z, 3, therms',
+            '2012-01-01T01:45:00Z, 4, therms',
+            '2012-01-01T02:00:00Z, 5, therms',
+            ]))
+        get_energy_for_hour = fbd.get_interval_meter_data_source(csv2)
+        self.assertEquals(14, get_energy_for_hour(date(2012,1,1),[1,1]))
 
         # TODO test a csv with bad timestamps
 
 
     def test_fetch_interval_meter_data(self):
         '''Realistic test of loading interval meter data with an entire utility
-        bill date range. Tests that it worked but not that it was correct.'''
+        bill date range. Tests lack of errors but not correctness.'''
         # TODO would be great to have a way of getting a fake reebill for
         # testing. here i just count on this reebill existing and then modify
         # it but don't save it
@@ -81,9 +91,15 @@ class FetchTest(unittest.TestCase):
             'port': '27017'
         })
         reebill = reebill_dao.load_reebill('10002', 21)
+        # these are the dates i expect that bill to have
+        assert reebill.period_begin == date(2011,11,10)
+        assert reebill.period_end == date(2011,12,12)
 
+        # generate example csv file whose time range exactly matches the bill
+        # period
         csv_file = StringIO()
-        make_big_interval_meter_test_csv(reebill.period_begin, reebill.period_end, csv_file)
+        make_big_interval_meter_test_csv(reebill.period_begin,
+                reebill.period_end, csv_file)
         # writing the file puts the file pointer at the end, so move it back
         csv_file.seek(0)
 
