03acb0c8755785676e935cdcd523b0ad8e3b7768
Daniel Klothe <dklothe@skylineinnovations.com>
2012-03-09 19:20:07 -0500

    graph script takes account argument, shows cumulative or per-bill savings

--- scripts/graph.py
+++ scripts/graph.py
@@ -21,6 +21,18 @@ class Grapher:
             savings.append(float(reebill.savings))
         return time_offsets, savings
 
+    #def get_conventional_energy_charges(self, account):
+        #time_offsets = [] # in days
+        #charges = []
+        #first_reebill_start = self.reebill_dao.get_first_bill_date_for_account(account)
+        #for reebill in self.reebill_dao.load_reebills_in_period(account):
+            #time_offsets.append((reebill.period_begin - first_reebill_start.date()).days)
+            #ce_total = 0
+            #for charge in reebill.actual_chargegroups_flattened:
+                #...
+            #savings.append(float(reebill.savings))
+        #return time_offsets, savings
+
     def plot_savings(self, account):
         time_offsets, savings = self.get_savings(account)
         plt.plot(time_offsets, savings, linewidth=2, color='g')
@@ -37,21 +49,13 @@ class Grapher:
 
 def main():
     # command-line arguments
-    parser = argparse.ArgumentParser(description='Generate reconciliation report.')
+    parser = argparse.ArgumentParser(description='Draw some graphs.')
+    parser.add_argument('account', metavar='ACCOUNT',
+           help='account of customer to graph')
     parser.add_argument('--host',  default='localhost',
             help='host for all databases (default: localhost)')
-    parser.add_argument('--statedb', default='skyline_dev',
-            help='name of state database (default: skyline_dev)')
-    parser.add_argument('--stateuser', default='dev',
-            help='username for state database (default: dev)')
-    parser.add_argument('--statepw', default='dev',
-            help='name of state database (default: dev)')
-    parser.add_argument('--billdb', default='skyline_dev',
-            help='name of bill database (default: skyline_dev)')
-    parser.add_argument('--olapdb',  default='dev',
-            help='name of OLAP database (default: dev)')
-    parser.add_argument('--skip-oltp',  action='store_true',
-            help="Don't include OLTP data (much faster)")
+    parser.add_argument('--billdb', default='skyline',
+            help='name of bill database (default: skyline)')
     args = parser.parse_args()
 
     # set up config dicionaries for data access objects used in generate_report
@@ -61,24 +65,10 @@ def main():
         'host': args.host,
         'port': '27017'
     }
-    statedb_config = {
-        'host': args.host,
-        'password': args.statepw,
-        'database': args.statedb,
-        'user': args.stateuser
-    }
-    splinter_config = {
-        'url': 'http://duino-drop.appspot.com/',
-        'host': args.host,
-        'db': args.olapdb
-    }
-    monguru_config = {
-        'host': args.host,
-        'db': args.olapdb
-    }
 
+    print args
     g = Grapher(ReebillDAO(billdb_config))
-    g.plot_cumulative_savings('10002')
+    g.plot_cumulative_savings(args.account)
 
 if __name__ == '__main__':
     main()
