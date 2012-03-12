b2f4bc5df40b5e6781309f23d33778f97a240a02
Daniel Klothe <dklothe@skylineinnovations.com>
2012-03-12 13:16:25 -0400

    grapher loads bills by sequence instead of date & uses mysql instead of mongo to get them (avoids sequence 0)

--- scripts/graph.py
+++ scripts/graph.py
@@ -3,6 +3,7 @@ import argparse
 import pychartdir
 import matplotlib.pyplot as plt
 from billing.mongo import ReebillDAO
+from billing.processing.state import StateDB
 '''Creates a sample CSV file with random energy values every 15 minutes in the
 format used by fetch_interval_meter_data. Command-line arguments are file name,
 start date (ISO 8601, inclusive), end date (exclusive). E.g:
@@ -15,14 +16,17 @@ def integrate(array):
         array[i] = array[i] + array[i-1]
 
 class Grapher:
-    def __init__(self, reebill_dao):
+    def __init__(self, state_db, reebill_dao):
+        self.state_db = state_db
         self.reebill_dao = reebill_dao
 
     def get_time_series(self, account, x_func, y_func):
         x_values = []
         y_values = []
         first_issue_date = self.reebill_dao.get_first_issue_date_for_account(account)
-        for reebill in self.reebill_dao.load_reebills_in_period(account):
+        #for reebill in self.reebill_dao.load_reebills_in_period(account):
+        for sequence in self.state_db.listSequences(self.state_db.session(), account):
+            reebill = self.reebill_dao.load_reebill(account, sequence)
             #time_offsets.append((reebill.period_begin - first_reebill_start.date()).days)
             #time_offsets.append((reebill.issue_date - first_issue_date.date()).days)
             x_values.append(x_func(reebill))
@@ -72,6 +76,12 @@ def main():
             help='host for all databases (default: localhost)')
     parser.add_argument('--billdb', default='skyline',
             help='name of bill database (default: skyline)')
+    parser.add_argument('--statedb', default='skyline_dev',
+            help='name of state database (default: skyline_dev)')
+    parser.add_argument('--stateuser', default='dev',
+            help='username for state database (default: dev)')
+    parser.add_argument('--statepw', default='dev',
+            help='name of state database (default: dev)')
     args = parser.parse_args()
 
     # set up config dicionaries for data access objects used in generate_report
@@ -81,8 +91,14 @@ def main():
         'host': args.host,
         'port': '27017'
     }
+    statedb_config = {
+        'host': args.host,
+        'password': args.statepw,
+        'database': args.statedb,
+        'user': args.stateuser
+    }
 
-    g = Grapher(ReebillDAO(billdb_config))
+    g = Grapher(StateDB(statedb_config), ReebillDAO(billdb_config))
     g.plot_cumulative_actual_and_hypothetical_ce_charces(args.account)
     plt.show()
 
