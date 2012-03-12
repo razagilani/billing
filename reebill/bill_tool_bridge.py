f427f2b2313ada59298cbe3175ada8e06ee216ed
Daniel Klothe <dklothe@skylineinnovations.com>
2012-03-12 19:59:04 -0400

    many events are now logged with eventlog

--- reebill/bill_tool_bridge.py
+++ reebill/bill_tool_bridge.py
@@ -460,7 +460,8 @@ class BillToolBridge:
             reebill = self.reebill_dao.load_reebill(account, sequence)
             self.process.roll_bill(session, reebill)
             self.reebill_dao.save_reebill(reebill)
-            self.journal_dao.journal(account, sequence, "ReeBill rolled")
+            self.journal_dao.log_event(account, sequence,
+                    JournalDAO.ReeBillRolled)
             session.commit()
             return self.dumps({'success': True})
         except Exception as e:
@@ -512,7 +513,8 @@ class BillToolBridge:
                     reebill
                 )
             self.reebill_dao.save_reebill(reebill)
-            self.journal_dao.journal(account, sequence, "RE&E Bound")
+            self.journal_dao.log_event(account, sequence,
+                    JournalDAO.ReeBillBoundtoREE)
             return self.dumps({'success': True})
         except Exception as e:
             return self.handle_exception(e)
@@ -532,7 +534,8 @@ class BillToolBridge:
             reebill = self.reebill_dao.load_reebill(account, sequence)
             fbd.fetch_interval_meter_data(reebill, csv_file.file)
             self.reebill_dao.save_reebill(reebill)
-            self.journal_dao.journal(account, sequence, "RE&E Bound")
+            self.journal_dao.log_event(account, sequence,
+                    JournalDAO.ReeBillBoundtoREE)
             return self.dumps({'success': True})
         except Exception as e:
             return self.handle_exception(e)
@@ -624,6 +627,7 @@ class BillToolBridge:
             self.process.attach_utilbills(session, reebill.account,
                     reebill.sequence)
 
+            # TODO change to event log
             self.journal_dao.journal(reebill.account, reebill.sequence,
                     "User %s attached utilbills to reebill %s-%s" % (
                     cherrypy.session['user'].username, account, sequence))
@@ -694,9 +698,10 @@ class BillToolBridge:
                         account), bill_file_names);
 
             for reebill in all_bills:
-                self.journal_dao.journal(reebill.account, reebill.sequence,
-                        "Mailed to %s by %s" % (recipients,
-                        cherrypy.session['user'].username))
+                self.journal_dao.log_event(reebill.account, reebill.sequence,
+                        JournalDAO.ReeBillMailed,
+                        address=','.join(map(str, recipients))
+                        user=cherrypy.session['user'].username)
                 self.process.issue(session, reebill.account, reebill.sequence)
                 self.process.attach_utilbills(session, reebill.account,
                         reebill.sequence)
@@ -1549,7 +1554,7 @@ class BillToolBridge:
 
                 for sequence in sequences:
                     self.process.delete_reebill(session, account, sequence)
-                    self.journal_dao.journal(account, sequence, "Deleted")
+                    self.journal_dao.log_event(account, sequence, JournalDAO.ReeBillDeleted)
                 session.commit()
                 return self.dumps({'success': True})
 
