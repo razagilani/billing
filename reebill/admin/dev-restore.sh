aef91b699bb2d417bcc2f3114335f696ec5b5fe4
Rich Andrews <stuart.r.andrews@gmail.com>
2012-03-13 10:55:59 -0400

    26356879 - removed the name dklothe

--- reebill/admin/dev-restore.sh
+++ reebill/admin/dev-restore.sh
@@ -5,18 +5,18 @@ now=`date +"%Y%m%d"`
 
 # copy mysql dump file (generated last night via cron) from tyrell to local
 # /tmp, then restore mysql dev database (on localhost) from that file
-scp dklothe@tyrell:/tmp/${now}billing_mysql.dmp /tmp
+scp DEVELOPERNAMESNEVERGOINSCRIPTSDIEDIEDIE@tyrell:/tmp/${now}billing_mysql.dmp /tmp
 #mysql --verbose -udev -pdev -htyrell -D skyline_dev < /tmp/${now}billing_mysql.dmp
 # apparently only root can restore the database?
 # "Access denied; you need the SUPER privilege for this operation"
 mysql -uroot -proot -D skyline_dev < /tmp/${now}billing_mysql.dmp
 
 # copy mongo dump files from tyrell to local /tmp and restore mongo collections from them
-scp -r dklothe@tyrell:/tmp/${now}ratestructure_mongo /tmp
-scp -r dklothe@tyrell:/tmp/${now}reebills_mongo /tmp
-scp -r dklothe@tyrell:/tmp/${now}journal_mongo /tmp
-scp -r dklothe@tyrell:/tmp/${now}eventlog_mongo /tmp
-scp -r dklothe@tyrell:/tmp/${now}users_mongo /tmp
+scp -r DEVELOPERNAMESNEVERGOINSCRIPTSDIEDIEDIE@tyrell:/tmp/${now}ratestructure_mongo /tmp
+scp -r DEVELOPERNAMESNEVERGOINSCRIPTSDIEDIEDIE@tyrell:/tmp/${now}reebills_mongo /tmp
+scp -r DEVELOPERNAMESNEVERGOINSCRIPTSDIEDIEDIE@tyrell:/tmp/${now}journal_mongo /tmp
+scp -r DEVELOPERNAMESNEVERGOINSCRIPTSDIEDIEDIE@tyrell:/tmp/${now}eventlog_mongo /tmp
+scp -r DEVELOPERNAMESNEVERGOINSCRIPTSDIEDIEDIE@tyrell:/tmp/${now}users_mongo /tmp
 
 # restore
 mongorestore --drop --db skyline --collection ratestructure /tmp/${now}ratestructure_mongo/skyline-prod/ratestructure.bson
@@ -28,4 +28,4 @@ mongorestore --drop --db skyline --collection users /tmp/${now}users_mongo/skyli
 # delete local bill files and replace with a copy from staging on tyrell
 # (hopefully copied last night from production to staging)
 #rm -rf /db-dev/
-#scp -r dklothe@tyrell:/db-stage /db-dev
+#scp -r DEVELOPERNAMESNEVERGOINSCRIPTSDIEDIEDIE@tyrell:/db-stage /db-dev
