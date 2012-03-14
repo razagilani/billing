1042f9972056ce19e4860cc0d10d5f83e0aa227f
Rich Andrews <stuart.r.andrews@gmail.com>
2012-03-14 18:05:44 -0400

    provisional commit

--- reebill/fabfile.py
+++ reebill/fabfile.py
@@ -117,7 +117,11 @@ def mercurial_actual_tag():
     actual_tag = fabops.local("hg parent --template '{tags}'", capture=True)
     if actual_tag != "" and actual_tag != "tip":
         return actual_tag
-    return fabops.local("hg log -r tip --template '{latesttag}'", capture=True)
+    actual_tag = fabops.local("hg log -r tip --template '{latesttag}'", capture=True)
+    if actual_tag != "null":
+        return actual_tag
+    
+    return -1
 
 def upgrade_scripts_release():
     return int(fabops.local("ls -1 ../upgrade_scripts/ | sort | tail -1", capture=True).split()[0])
