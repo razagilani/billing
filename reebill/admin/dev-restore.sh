0c9f2a44bd15beab49c9134da8ffd231dabc4c5a
Rich Andrews <stuart.r.andrews@gmail.com>
2012-03-29 16:31:11 -0400

    24852467 - late rate charge can be set in UI on a per reebill basis 27253777 - new server side form validation pattern

---
+++ reebill/admin/dev-restore.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Script to restore development MySQL, Mongo collections, and filesystem from
+# backups on tyrell. Based on billing/reebill/admin/destage.bash.
+now=`date +"%Y%m%d"`
+tarball=${now}reebill-prod.tar.z
+
+cd /tmp
+
+scp `whoami`@tyrell:/tmp/$tarball .
+tar xvzf $tarball
+#mysql --verbose -udev -pdev -htyrell -D skyline_dev < /tmp/${now}billing_mysql.dmp
+# apparently only root can restore the database?
+# "Access denied; you need the SUPER privilege for this operation"
+mysql -uroot -proot -D skyline_dev < ${now}billing_mysql.dmp
+
+# restore
+mongorestore --drop --db skyline --collection ratestructure ${now}ratestructure_mongo/skyline-prod/ratestructure.bson
+mongorestore --drop --db skyline --collection reebills ${now}reebills_mongo/skyline-prod/reebills.bson
+mongorestore --drop --db skyline --collection journal ${now}journal_mongo/skyline-prod/journal.bson
+mongorestore --drop --db skyline --collection eventlog ${now}eventlog_mongo/skyline-prod/eventlog.bson
+mongorestore --drop --db skyline --collection users ${now}users_mongo/skyline-prod/users.bson
+
+# delete local bill files and replace with a copy from staging on tyrell
+rm -fr /db-dev/*
+cp -r db-prod/* /db-dev
