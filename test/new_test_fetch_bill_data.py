43ed47d1ab84413e9381facde71bdb865ba914e8
Daniel Klothe <dklothe@skylineinnovations.com>
2012-04-03 14:21:20 -0400

    not working (committing so i can merge)

--- test/new_test_fetch_bill_data.py
+++ test/new_test_fetch_bill_data.py
@@ -14,12 +14,8 @@ pp = pprint.PrettyPrinter().pprint
 
 def make_big_interval_meter_test_csv(start_date, end_date, csv_file):
     '''Writes a sample CSV to csv_file with file with random energy values
-    every 15 minutes in the format used by fetch_interval_meter_data.
-    Command-line arguments are file name, start date (ISO 8601, inclusive), end
-    date (exclusive). E.g:
-        make_sample_csv.py interval_meter_sample.csv 2012-11-10 2012-12-12
-    Returns the sum of all energy in the file.
-    '''
+    every 15 minutes from start_date to end_date (exclusive). Uses default
+    interval meter data format.'''
     writer = csv.writer(csv_file)
     for day in dateutils.date_generator(start_date, end_date):
         dt = datetime(day.year, day.month, day.day, 0)
@@ -29,6 +25,26 @@ def make_big_interval_meter_test_csv(start_date, end_date, csv_file):
                 datetime.strftime(dt, dateutils.ISO_8601_DATETIME),
                 random.random(),
                 'therms'
+            ])
+
+def make_atsite_test_csv(start_date, end_date, csv_file):
+    '''Writes a sample CSV like the above, but imitating the format of AtSite's
+    example file.'''
+    writer = csv.writer(csv_file)
+    for day in dateutils.date_generator(start_date, end_date):
+        dt = datetime(day.year, day.month, day.day, 0)
+        writer.writerow() # header
+        while dt.day == day.day:
+            dt += timedelta(hours=0.25)
+            row = [
+                datetime.strftime(dt, '%Y-%m-%d %H:%M:%S')
+
+                random.random(),
+                ]
+            writer.writerow([
+                datetime.strftime(dt, '%Y-%m-%d %H:%M:%S')
+
+                random.random(),
             ])
 
 class FetchTest(unittest.TestCase):
