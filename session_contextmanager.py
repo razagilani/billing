6b4e54e621791236b75cf0f1301ad20709b58cd2
Daniel Klothe <dklothe@skylineinnovations.com>
2012-05-21 15:37:57 -0400

    converted session_contextmanager.py from example code into real code by moving DBSession class from bill_tool_bridge.py into that file. it can now be used in tests.

--- session_contextmanager.py
+++ session_contextmanager.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
-'''Prototype/example code for SQLAlchemy database session context manager.'''
+'''SQLAlchemy database session context manager. Use it like:
+    with DBSession(state_db) as session:
+        # do stuff with database...
+        session.commit()
+If an exception interrupts the code before session.commit(), the session is
+rolled back automatically.
+'''
 import traceback
 import threading
 import time
@@ -10,6 +16,7 @@ from billing.processing import state
 from billing.processing.db_objects import ReeBill, Customer
 
 class DBSession(object):
+    '''Context manager for using "with" for database session.'''
     def __init__(self, state_db):
         self.state_db = state_db
 
@@ -19,53 +26,56 @@ class DBSession(object):
 
     def __exit__(self, type, value, traceback):
         print 'exit: %type={type}, value=%{value}, traceback=%{traceback}'.format(**vars())
-        #self.session.rollback()
+        self.session.rollback()
 
-class QueryThread(threading.Thread):
-    def __init__(self, state_db, number):
-        super(QueryThread, self).__init__()
-        self.state_db = state_db
-        self.number = number
 
-    def run(self):
-        with DBSession(self.state_db) as session:
-            time.sleep(random.random())
-            print 'session is', id(session)
-            # some selects
-            self.state_db.listAccounts(session)
-            customer = session.query(Customer).filter(Customer.account=='10001').one()
 
-            # an insert
-            r = ReeBill(customer, self.number)
-            session.add(r)
+if __name__ == '__main__':
+    class QueryThread(threading.Thread):
+        def __init__(self, state_db, number):
+            super(QueryThread, self).__init__()
+            self.state_db = state_db
+            self.number = number
 
-            #session.commit()
-            session.flush()
-            session.delete(r)
-            session.flush()
+        def run(self):
+            with DBSession(self.state_db) as session:
+                time.sleep(random.random())
+                print 'session is', id(session)
+                # some selects
+                self.state_db.listAccounts(session)
+                customer = session.query(Customer).filter(Customer.account=='10001').one()
 
-global_state_db = state.StateDB(
-    host='localhost',
-    password='dev',
-    database='skyline_dev',
-    user='dev',
-    db_connections=1
-)
+                # an insert
+                r = ReeBill(customer, self.number)
+                session.add(r)
 
-for i in range(100,200):
-    q = QueryThread(global_state_db, i)
-    q.start()
+                #session.commit()
+                session.flush()
+                session.delete(r)
+                session.flush()
 
-#for i in range(5):
-    #with DBSession(state_db) as session:
-        #try:
-            #session = state_db.session()
-            #accounts = state_db.listAccounts(session)
-            #print accounts
-            #customer = session.query(Customer).filter(Customer.account=='10001').one()
-            #r = ReeBill(customer, 99)
-            #session.add(r)
-            #session.commit()
-            #session.flush()
-        #except:
-            #pass
+    global_state_db = state.StateDB(
+        host='localhost',
+        password='dev',
+        database='skyline_dev',
+        user='dev',
+        db_connections=1
+    )
+
+    for i in range(100,200):
+        q = QueryThread(global_state_db, i)
+        q.start()
+
+    #for i in range(5):
+        #with DBSession(state_db) as session:
+            #try:
+                #session = state_db.session()
+                #accounts = state_db.listAccounts(session)
+                #print accounts
+                #customer = session.query(Customer).filter(Customer.account=='10001').one()
+                #r = ReeBill(customer, 99)
+                #session.add(r)
+                #session.commit()
+                #session.flush()
+            #except:
+                #pass
