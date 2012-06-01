3e2fdd5973576816cbefe3e0e2e1843ea01b0d0d
Daniel Klothe <dklothe@skylineinnovations.com>
2012-06-01 19:57:29 -0400

    attempted to make 002_version_key.py upgrade script fix CPRSs as well as reebills, but it doesn't work: 30582151

--- upgrade_scripts/7/002_version_key.py
+++ upgrade_scripts/7/002_version_key.py
@@ -3,18 +3,42 @@ import pymongo
 db = 'skyline'
 
 connection = pymongo.Connection('localhost')
-collection = connection[db]['reebills']
+reebills_collection = connection[db]['reebills']
+rs_collection = connection[db]['ratestructure']
 
-fixed, total = 0, 0
-bills = collection.find(sort=[('_id.account', pymongo.ASCENDING), ('_id.sequence', pymongo.ASCENDING)])
+# reebills
+bills = reebills_collection.find(sort=[('_id.account', pymongo.ASCENDING), ('_id.sequence', pymongo.ASCENDING)])
+bills_fixed, bills_total = 0, 0
 for bill in bills:
     if 'branch' in bill['_id']:
+        # remove old doc
+        reebills_collection.remove(bill['_id'])
+        # fix it and save
         del bill['_id']['branch']
         bill['_id']['version'] = 0
-        collection.save(bill)
-        print bill['_id'], 'fixed'
-        fixed += 1
+        reebills_collection.save(bill)
+        print 'reebill', bill['_id'], 'fixed'
+        bills_fixed += 1
     else:
-        print bill['_id'], 'skipped'
-    total += 1
-print '%s of %s bills fixed' % (fixed, total)
+        print 'reebill', bill['_id'], 'skipped'
+    bills_total += 1
+
+# customer periodic rate structures
+cprss = rs_collection.find({'_id.type': 'CPRS'}, sort=[('_id.account', pymongo.ASCENDING), ('_id.sequence', pymongo.ASCENDING)])
+rs_fixed, rs_total = 0, 0
+for cprs in cprss:
+    if 'branch' in cprs['_id']:
+        # remove old doc
+        rs_collection.remove(cprs['_id'])
+        # fix it and save
+        del cprs['_id']['branch']
+        cprs['_id']['version'] = 0
+        rs_collection.save(cprs)
+        print 'CPRS', cprs['_id'], 'fixed'
+        rs_fixed += 1
+    else:
+        print 'CPRS', cprs['_id'], 'skipped'
+    rs_total += 1
+
+print '%s of %s reebills fixed' % (bills_fixed, bills_total)
+print '%s of %s CPRSs fixed' % (rs_fixed, rs_total)
