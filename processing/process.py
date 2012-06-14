fa727764929684049f5633dad540ef8ddd8743b0
Daniel Klothe <dklothe@skylineinnovations.com>
2012-06-14 18:36:50 -0400

    broken (need to switch to other branch)

--- processing/process.py
+++ processing/process.py
@@ -374,13 +374,10 @@ class Process(object):
     def get_unissued_correction_sequences(self, session, account):
         return [c[0] for c in self.get_unissued_corrections(session, account)]
 
-    def apply_correction(self, session, account, correction_sequence,
-            target_sequence):
+    def apply_corrections(self, session, account, target_sequence):
         '''Applies adjustments from the unissued correction given by
         'correction_sequence' to the reebill given by 'target_sequence' for the
         given account. The unissued correction is marked as issued.'''
-        all_unissued_corrections = self.get_unissued_corrections(session,
-                account)
 
         # corrections can only be applied to an un-issued reebill whose version
         # is 0
@@ -388,20 +385,11 @@ class Process(object):
                 target_sequence)
         if self.state_db.is_issued(session, account, target_sequence) \
                 or target_max_version > 0:
-            raise ValueError(("Can't apply correction %s to %s-%s, "
+            raise ValueError(("Can't apply corrections %s to %s-%s, "
                     "because the latter is an issued reebill or another "
-                    "correction.") % (correction_sequence, account, target_sequence))
+                    "correction.") % (correction_sequences, account,
+                    target_sequence))
         
-        print '***** all_unissued_corrections', all_unissued_corrections
-        print '***** correction_sequence', correction_sequence, type(correction_sequence)
-        print '***', [c[0] == correction_sequence for c in all_unissued_corrections]
-
-        # validate correction sequence
-        if not any([c[0] == correction_sequence for c in
-                all_unissued_corrections]):
-            raise ValueError(("Sequence %s doesn't have an un-issued "
-                "correction") % correction_sequence)
-
         # load target reebill from mongo (and, for recomputation, version 0 of
         # its predecessor)
         target_reebill = self.reebill_dao.load_reebill(account,
@@ -409,16 +397,21 @@ class Process(object):
         target_reebill_predecessor = self.reebill_dao.load_reebill(account,
                 target_sequence - 1, version=0)
 
-        # apply adjustment
-        correction = [c for c in all_unissued_corrections if c[0] ==
-                correction_sequence][0]
-        _, _, adjustment = correction
-        target_reebill.total_adjustment += adjustment
+        all_unissued_corrections = self.get_unissued_corrections(session,
+                account)
+        for correction_sequence in all_unissued_corrections:
+            # get adjustment & add it to the total
+            _, _, adjustment = [c for c in all_unissued_corrections if c[0] ==
+                    correction_sequence][0]
+            total_adjustment += adjustment
+
+            # mark the correction as issued
+            self.issue(session, account, correction_sequence)
+
+        # set total adjustment in the target reebill, recompute, and save
+        target_reebill.total_adjustment = total_adjustment
         self.sum_bill(session, target_reebill_predecessor, target_reebill)
         self.reebill_dao.save_reebill(target_reebill)
-
-        # issue
-        self.issue(session, account, correction_sequence)
 
     def get_late_charge(self, session, reebill, day=date.today()):
         '''Returns the late charge for the given reebill on 'day', which is the
