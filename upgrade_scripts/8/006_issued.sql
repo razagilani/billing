4d3ec174b93141a9e0bb60b25a20183c9ac7e509
Daniel Klothe <dklothe@skylineinnovations.com>
2012-06-20 19:27:46 -0400

    31229223 fixed 006_issued.sql script

--- upgrade_scripts/8/006_issued.sql
+++ upgrade_scripts/8/006_issued.sql
@@ -1,8 +1,4 @@
-/* makes all bills "issued" in mysql whose successor is issued */
-drop table t;
-
-create table t as select customer_id, sequence from rebill where issued = 1;
-create table u as select customer_id, sequence from rebill where issued = 1;
-update rebill set issued = 1 where rebill.id in (select id from u, t where u.customer_id = t.customer_id and u.sequence + 1 = t.sequence);
-
-drop table t;
+/* makes all bills "issued" in mysql where any bill for the same account but with higher sequence is issued */
+create temporary table t select * from rebill;
+create temporary table u select * from rebill;
+update rebill set issued = 1 where id in (select u.id from t, u where t.customer_id = u.customer_id and t.issued = 1 and t.sequence > u.sequence);
