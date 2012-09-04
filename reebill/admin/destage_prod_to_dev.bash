327395be06574367e0ad4772c2bcf19c99c351ae
Daniel Klothe <dklothe@skylineinnovations.com>
2012-09-03 20:19:56 -0400

    updated destage_prod_to_dev script to use tyrell-prod

--- reebill/admin/destage_prod_to_dev.bash
+++ reebill/admin/destage_prod_to_dev.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Script to restore development MySQL, Mongo collections, and filesystem from
-# backups on tyrell. Based on billing/reebill/admin/destage.bash.
+# backups on tyrell-prod. Based on billing/reebill/admin/destage.bash.
 now=`date +"%Y%m%d"`
 tarball=${now}reebill-prod.tar.z
+ssh_key=$HOME/Dropbox/IT/ec2keys/tyrell-prod.pem
 
 cd /tmp
 
-scp `whoami`@tyrell:/tmp/$tarball .
-tar xvzf $tarball
+scp -i $ssh_key ec2-user@tyrell-prod.skylineinnovations.net:/tmp/$tarball .
+tar xzf $tarball
 # apparently only root can restore the database?
 # "Access denied; you need the SUPER privilege for this operation"
 mysql -uroot -proot -D skyline_dev < ${now}billing_mysql.dmp
@@ -18,6 +19,6 @@ mongorestore --drop --db skyline --collection reebills ${now}reebills_mongo/skyl
 mongorestore --drop --db skyline --collection journal ${now}journal_mongo/skyline-prod/journal.bson
 mongorestore --drop --db skyline --collection users ${now}users_mongo/skyline-prod/users.bson
 
-# delete local bill files and replace with a copy from staging on tyrell
+# delete local bill files and replace with a copy from staging on tyrell-prod
 rm -fr /db-dev/*
 cp -r db-prod/* /db-dev
