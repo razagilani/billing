1c5dcbc77b2761f98b99cf9170a7941dc163176d
Rich Andrews <stuart.r.andrews@gmail.com>
2012-09-06 17:29:32 -0400

    branch merge

--- test/fake_skyliner.py
+++ test/fake_skyliner.py
@@ -3,14 +3,14 @@ from datetime import date, datetime, timedelta
 from decimal import Decimal
 from skyliner.sky_handlers import cross_range
 
-def one_hour_of_energy():
+def random_hour_of_energy():
     '''In BTU.'''
     return 50000 * (1 + random())
 
 class FakeSplinter(object):
     def __init__(self, random=True):
         self.random = random
-        self._guru = FakeMonguru()
+        self._guru = FakeMonguru(random)
 
     def get_install_obj_for(self, olap_id):
         return FakeSkyInstall(random=self.random)
@@ -22,12 +22,13 @@ class FakeSplinter(object):
 class FakeSkyInstall(object):
     def __init__(self, random=True, *args, **kwargs):
         self.random = random
+        self.name = 'Fake SkyInstall'
 
     #def get_billable_energy(self, day, hour_range=(0,24), places=5):
         #hours = hour_range[1] - hour_range[0]
         #if self.random:
             ## NOTE you can't pass a float into Decimal() in 2.6, only 2.7
-            #energy = Decimal(str(one_hour_of_energy())) * hours
+            #energy = Decimal(str(random_hour_of_energy())) * hours
         #else:
             #energy = Decimal('100000') * hours
         #return energy.quantize(Decimal('1.'+'0'*places))
@@ -35,7 +36,7 @@ class FakeSkyInstall(object):
     def get_billable_energy_timeseries(self, start, end, places=None):
         if self.random:
             # NOTE you can't pass a float into Decimal() in 2.6, only 2.7
-            return [(hour, Decimal(str(one_hour_of_energy()))) for hour in cross_range(start, end)]
+            return [(hour, Decimal(str(random_hour_of_energy()))) for hour in cross_range(start, end)]
         return [(hour, 100000) for hour in cross_range(start, end)]
             
     @property
@@ -51,15 +52,30 @@ class FakeCubeDocument(object):
         self.energy_sold = energy_sold
 
 class FakeMonguru(object):
+    def __init__(self, random=True):
+        self.random = random
+
     def get_data_for_hour(self, install, day, hour):
-        return FakeCubeDocument(one_hour_of_energy())
+        if self.random:
+            energy = random_hour_of_energy()
+        else:
+            energy = 100000
+        return FakeCubeDocument(energy)
 
     def get_data_for_day(self, install, day):
-        return FakeCubeDocument(one_hour_of_energy() * 24)
+        if self.random:
+            energy = random_hour_of_energy() * 24
+        else:
+            energy = 100000 * 24
+        return FakeCubeDocument(energy * 24)
 
     def get_data_for_week(self, install, year, week):
         raise NotImplementedError()
 
     def get_data_for_month(self, install, year, month):
-        return FakeCubeDocument(one_hour_of_energy() * 24 * 30)
+        if self.random:
+            energy = random_hour_of_energy() * 24
+        else:
+            energy = 100000 * 24
+        return FakeCubeDocument(energy * 24 * 30)
 
