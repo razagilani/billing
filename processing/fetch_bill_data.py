9a42720b37af6fc5d2029ea1f47f2b843426bdc3
Daniel Klothe <dklothe@skylineinnovations.com>
2012-09-06 14:00:26 -0400

    turned off verbose printing in fetch_bill_data by default, but added verbose=True argument in BTB, so we'll see output in the apache error log, but not when running unit tests

--- processing/fetch_bill_data.py
+++ processing/fetch_bill_data.py
@@ -24,7 +24,8 @@ from billing.dateutils import date_to_datetime, timedelta_in_hours
 from billing.exceptions import MissingDataError
 from decimal import Decimal
 
-def get_billable_energy_timeseries(splinter, install, start, end):
+def get_billable_energy_timeseries(splinter, install, start, end,
+        verbose=False):
     '''Returns a list of hourly billable-energy values during the datetime
     range [start, end) (endpoints must whole hours). Values during
     unbillable annotations are removed.''' 
@@ -50,13 +51,14 @@ def get_billable_energy_timeseries(splinter, install, start, end):
                 raise MissingDataError(("Couldn't get renewable energy data for %s: "
                         "OLAP documents lack energy_sold measure starting at "
                         "%s") % (install.name, hour))
-            print >> sys.stderr, "%s's OLAP energy_sold for %s: %s" % (
-                    install.name, hour, energy_sold)
+            if verbose:
+                print >> sys.stderr, "%s's OLAP energy_sold for %s: %s" % (
+                        install.name, hour, energy_sold)
             result.append(Decimal(energy_sold))
     return result
 
 # TODO 35345191 rename this function
-def fetch_oltp_data(splinter, olap_id, reebill, use_olap=True, verbose=True):
+def fetch_oltp_data(splinter, olap_id, reebill, use_olap=True, verbose=False):
     '''Update quantities of shadow registers in reebill with Skyline-generated
     energy. The OLAP database is the default source of energy-sold values; use
     use_olap=False to get them directly from OLTP.'''
@@ -70,7 +72,8 @@ def fetch_oltp_data(splinter, olap_id, reebill, use_olap=True, verbose=True):
     # get hourly "energy sold" values during this period
     if use_olap:
         timeseries = get_billable_energy_timeseries(splinter, install_obj,
-                date_to_datetime(start), date_to_datetime(end))
+                date_to_datetime(start), date_to_datetime(end),
+                verbose=verbose)
     else:
         # NOTE if install_obj.get_billable_energy_timeseries() uses
         # die_fast=False, this timeseries may be shorter than anticipated and
@@ -79,8 +82,6 @@ def fetch_oltp_data(splinter, olap_id, reebill, use_olap=True, verbose=True):
         timeseries = [Decimal(pair[1]) for pair in
                 install_obj.get_billable_energy_timeseries(
                 date_to_datetime(start), date_to_datetime(end))]
-
-    print "total REE should be: ", sum(timeseries), "BTU"
 
     # this function takes an hour and returns energy sold during that hour
     def energy_function(day, hourrange):
@@ -254,7 +255,7 @@ def get_shadow_register_data(reebill, meter_identifier=None):
 
 
 def usage_data_to_virtual_register(reebill, energy_function,
-        meter_identifier=None, verbose=True):
+        meter_identifier=None, verbose=False):
     '''Gets energy quantities from 'energy_function' and puts them in the total
     fields of the appropriate shadow registers in the MongoReebill object
     reebill. 'energy_function' should be a function mapping a date and an hour
