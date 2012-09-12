dee2e6c0c6f48b0c576e8d055c33eae4330b9f61
Daniel Klothe <dklothe@skylineinnovations.com>
2012-09-12 11:16:45 -0400

    35925731 added 1-minute timeout to prevent error message when rendering large utility bill images

---
+++ exceptions.py
@@ -0,0 +1,43 @@
+class Unauthenticated(Exception):
+    pass
+
+class MissingDataError(Exception):
+    '''Data from an external source (e.g. Skyline OLAP or OLTP) were expected
+    but not found.'''
+    pass
+
+# TODO rename to NoSuchBillException
+class NoSuchBillException(Exception):
+    pass
+
+class NoRateStructureError(Exception):
+    pass
+
+class NoUtilityNameError(Exception):
+    pass
+
+class IssuedBillError(Exception):
+    '''Exception for trying to modify a bill that has been issued.'''
+    pass
+
+class RSIError(Exception):
+    """Base class for exceptions in this module."""
+    def __init__(self, descriptor, msg):
+        self.descriptor = descriptor
+        self.msg = msg
+    def __str__(self):
+        return "%s %s" % (self.descriptor, self.msg)
+
+# errors that occur during evaluation of rate structure "quantity" expressions
+
+class RecursionError(RSIError):
+    pass
+
+class NoPropertyError(RSIError):
+    pass
+
+class NoSuchRSIError(RSIError):
+    pass
+
+class BadExpressionError(RSIError):
+    pass
