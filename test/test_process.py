b0d67347572ea697b13dbf6650bcbce6e938209e
Daniel Klothe <dklothe@skylineinnovations.com>
2012-09-21 19:12:56 -0400

    36281461 test_adjustment passes

--- test/test_process.py
+++ test/test_process.py
@@ -1037,9 +1037,14 @@ port = 27017
 
             # issue reebill #1 and correct it with an adjustment of 100
             self.process.issue(session, acc, 1)
-            corrected_version = self.process.new_version(session, acc, 1)
-            corrected_version.ree_charges = one.ree_charges + 100
+            one_corrected = self.process.new_version(session, acc, 1)
+            one_corrected.ree_charges = one.ree_charges + 100
+            # this change must be saved in Mongo, because compute_bill() ->
+            # get_unissued_corrections() loads the original and corrected bills
+            # from Mongo and compares them to calculate the adjustment
+            self.reebill_dao.save_reebill(one_corrected)
 
+            #import ipdb; ipdb.set_trace()
             self.process.compute_bill(session, one, two)
             self.process.compute_bill(session, two, three)
 
