29a0100b23bf1d894c9362e69a2d5d5ac6e45f89
Daniel Klothe <dklothe@skylineinnovations.com>
2012-10-04 19:28:11 -0400

    fixes in upgrade script related to 37217567 (issued reebill lacked issue_date key)

--- upgrade_scripts/11/01_separate_utilbills.py
+++ upgrade_scripts/11/01_separate_utilbills.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from datetime import datetime
 import pymongo
 import pprint
 from billing.dictutils import dict_merge, subdict
@@ -48,16 +49,15 @@ def get_external_utilbills(reebill):
                 }, overwrite=True)
             for meter in utilbill['meters']],
         }
-        utilbills.append(new_utilbill)
 
         # issued reebills also have frozen copy of each utility bill with a
         # "sequence" and "version" in it
-        if 'issue_date' in reebill:
+        if reebill['issue_date'] is not None:
             new_utilbill['_id'].update({
                 'sequence': reebill['_id']['sequence'],
                 'version': reebill['_id']['version']
             })
-            utilbills.append(new_utilbill)
+        utilbills.append(new_utilbill)
 
     return utilbills
 
@@ -88,14 +88,25 @@ def get_internal_utilbills(reebill):
     return utilbills
 
 for reebill in reebills_col.find():#{'_id.account':'10023', '_id.sequence':5}):
+    # data fix; TODO remove
+    if reebill['_id']['account'] == '10001' \
+            and reebill['_id']['sequence'] == 25 \
+            and reebill['_id']['version'] == 0:
+        reebill['issue_date'] = datetime(2012,5,24)
+        print '************** fixed 10001-25-0'
+
     try:
         # create external utilbills
         for ub in get_external_utilbills(reebill):
-            utilbills_col.save(ub)
+            utilbills_col.save(ub, safe=True)
+            if utilbills_col.get_lasterror_options() != {}:
+                raise ValueError(utilbills_col.get_lasterror_options())
 
         # replace utilbills list in reebill with list of internal ones
         reebill['utilbills'] = get_internal_utilbills(reebill)
-        reebills_col.save(reebill)
+        reebills_col.save(reebill, safe=True)
+        if reebills_col.get_lasterror_options() != {}:
+            raise ValueError
     except KeyError as e:
         print >> sys.stderr, reebill['_id']['account'], \
                 reebill['_id']['sequence'], reebill['_id']['version'], \
@@ -105,8 +116,9 @@ for reebill in reebills_col.find():#{'_id.account':'10023', '_id.sequence':5}):
                 reebill['_id']['sequence'], reebill['_id']['version'], \
                 'ERROR:', traceback.format_exc()
     else:
-        # no news is good news
-        pass
+        ## no news is good news
+        #pass
+        print "upgraded", reebill['_id']
 
 
 # check that all utility bills can be loaded for each reebill
@@ -128,8 +140,9 @@ with DBSession(sdb) as session:
     for acc in sdb.listAccounts(session):
         for seq in sdb.listSequences(session, acc):
             for version in range(sdb.max_version(session, acc, seq)):
-                try:
-                    dao.load_reebill(acc, seq)
-                except:
+                #try:
+                    print "loading", acc, seq, version
+                    dao.load_reebill(acc, seq, version)
+                #except:
                     #import ipdb; ipdb.set_trace()
-                    dao.load_reebill(acc, seq)
+                    #dao.load_reebill(acc, seq)
