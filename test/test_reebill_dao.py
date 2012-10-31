5aa842f65803d23a61cf67868ef996e9550c2c79
Daniel Klothe <dklothe@skylineinnovations.com>
2012-10-31 17:16:40 -0400

    fixed tests

--- test/test_reebill_dao.py
+++ test/test_reebill_dao.py
@@ -10,6 +10,7 @@ from billing.processing.state import StateDB
 from billing.processing.db_objects import ReeBill, Customer, UtilBill
 import MySQLdb
 from billing.test import example_data
+from billing.test.setup_teardown import TestCaseWithSetup
 from billing.processing.mongo import NoSuchBillException, IssuedBillError, NotUniqueException
 from billing.processing.session_contextmanager import DBSession
 
