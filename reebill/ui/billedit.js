f4d6abdf82f5afe188297a8491198f8c9b69b2cc
Daniel Klothe <dklothe@skylineinnovations.com>
2012-11-14 12:48:13 -0500

    merge

--- reebill/ui/billedit.js
+++ reebill/ui/billedit.js
@@ -998,7 +998,7 @@ function reeBillReady() {
                     // TODO: have other widgets pull when this selection is made
                     loadReeBillUIForSequence(selected_account, record.data.sequence);
                 },
-            }
+            },
         }),
         store: reeBillStore,
         enableColumnMove: false,
@@ -3819,6 +3819,7 @@ function reeBillReady() {
     });
 
     paymentStore.on('beforeload', function() {
+        paymentGrid.setDisabled(true);
         paymentStore.setBaseParam("account", selected_account);
         paymentStore.setBaseParam("service", Ext.getCmp('service_for_charges').getValue());
     });
@@ -3932,7 +3933,17 @@ function reeBillReady() {
     var paymentGrid = new Ext.grid.EditorGridPanel({
         tbar: paymentToolbar,
         colModel: paymentColModel,
-        selModel: new Ext.grid.RowSelectionModel({singleSelect: true}),
+        selModel: new Ext.grid.RowSelectionModel({
+            singleSelect: true,
+            listeners: {
+                rowdeselect: function(selModel, index, record) {
+                    paymentToolbar.find('id','paymentRemoveBtn')[0].setDisabled(true);
+                },
+                rowselect: function(selModel, index, record) {
+                    paymentToolbar.find('id','paymentRemoveBtn')[0].setDisabled(!record.data.editable);
+                },
+            },
+        }),
         store: paymentStore,
         enableColumnMove: false,
         frame: true,
@@ -3947,10 +3958,10 @@ function reeBillReady() {
         clicksToEdit: 1
     });
 
-    paymentGrid.getSelectionModel().on('selectionchange', function(sm){
+/*    paymentGrid.getSelectionModel().on('selectionchange', function(sm){
         //paymentGrid.getTopToolbar().findById('paymentInsertBtn').setDisabled(sm.getCount() <1);
         paymentGrid.getTopToolbar().findById('paymentRemoveBtn').setDisabled(sm.getCount() <1);
-    });
+    });*/
 
     // for bid editing of payments that the server says are not "editable"
     paymentGrid.on('beforeedit', function(e) {
@@ -4309,9 +4320,17 @@ function reeBillReady() {
         ],
         // looks to be initial order for load
         sortInfo: {
-            field: 'dayssince',
+            field: 'account',
             direction: 'DESC'
         },
+    });
+
+    accountStore.on('beforeload', function(store, options) {
+        accountGrid.setDisabled(true);
+    });
+
+    accountStore.on('load', function(store, options) {
+        accountGrid.setDisabled(false);
     });
 
     /* This function controls the style of cells in the account grid. */
@@ -4378,18 +4397,19 @@ function reeBillReady() {
 
     // this grid tracks the state of the currently selected account
     var accountGrid = new Ext.grid.EditorGridPanel({
+        id: 'accountGrid',
         colModel: accountColModel,
         selModel: new Ext.grid.RowSelectionModel({
             singleSelect: true,
-        listeners: {
-            rowselect: function (selModel, index, record) {
-               loadReeBillUIForAccount(record.data.account);
-               return false;
-           },
-        rowdeselect: function(selModel, index, record) {
-             loadReeBillUIForAccount(null);
-         }
-        },
+            listeners: {
+                rowselect: function (selModel, index, record) {
+                    loadReeBillUIForAccount(record.data.account);
+                    return false;
+                },
+                rowdeselect: function(selModel, index, record) {
+                    loadReeBillUIForAccount(null);
+                }
+            },
         }),
         store: accountStore,
         enableColumnMove: false,
@@ -4549,6 +4569,7 @@ function reeBillReady() {
     // this grid tracks the state of the currently selected account
 
     var accountReeValueGrid = new Ext.grid.GridPanel({
+        id: 'accountReeValueGrid',
         colModel: accountReeValueColModel,
         selModel: new Ext.grid.RowSelectionModel({
             singleSelect: true,
@@ -4701,6 +4722,7 @@ function reeBillReady() {
     newAccountDataConn.autoAbort = true;
     newAccountDataConn.disableCaching = true;
     var newAccountFormPanel = new Ext.FormPanel({
+        id: 'newAccountFormPanel',
         url: 'http://'+location.host+'/reebill/new_account',
         labelWidth: 120, // label settings here cascade unless overridden
         frame: true,
@@ -4831,7 +4853,13 @@ function reeBillReady() {
                                     Ext.Msg.alert('Success', "New account created");
                                     // update next account number shown in field
                                     newAccountField.setValue(nextAccount);
-
+                                    accountsPanel.getLayout().setActiveItem('accountGrid');
+                                    accountGrid.getSelectionModel().clearSelections();
+                                    accountStore.setDefaultSort('account','DESC');
+                                    pageSize = accountGrid.getBottomToolbar().pageSize;
+                                    accountStore.load({params: {start: 0, limit: pageSize}, callback: function() {
+                                        accountGrid.getSelectionModel().selectFirstRow();
+                                    }});
                                     // reload grid to show new account
                                     // TODO "load" gets no records, "reload" gets records, but neither one causes the grid to update
                                     reeBillStore.reload({
@@ -5645,7 +5673,7 @@ function reeBillReady() {
     {
 
         var sb = Ext.getCmp('statusbar');
-        var selStatus;
+        var selStatus = "No REE Bill Selected";
         if (account != null && sequence != null && branch != null)
             selStatus = account + "-" + sequence + "-" + branch;
         else if (account != null && sequence != null)
@@ -5688,6 +5716,7 @@ function reeBillReady() {
         if (account == null) {
             /* no account selected */
             journalPanel.setDisabled(true);
+            updateStatusbar(null, null, null)
             return;
         }
 
@@ -5829,7 +5858,7 @@ function reeBillReady() {
             ubMeasuredUsagesPanel.setDisabled(true);
             rateStructurePanel.setDisabled(true);
             chargeItemsPanel.setDisabled(true);
-            updateStatusbar(selected_account, null);
+            updateStatusbar(selected_account, null, null);
             deleteButton.setDisabled(true);
             return;
         }
