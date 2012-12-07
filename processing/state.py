a7f29290c783d186c18708a1a5ff55cab6ef0a6c
Rich Andrews <stuart.r.andrews@gmail.com>
2012-12-07 13:54:22 -0500

    merged up stable bugfix to default

--- processing/state.py
+++ processing/state.py
@@ -448,7 +448,8 @@ class StateDB:
     def listAccounts(self, session):
         '''List of all customer accounts (ordered).'''    
         # SQLAlchemy returns a list of tuples, so convert it into a plain list
-        result = map((lambda x: x[0]), session.query(Customer.account).all())
+        result = map((lambda x: x[0]),
+                session.query(Customer.account).order_by(Customer.account).all())
         return result
 
     def list_accounts(self, session, start, limit):
@@ -505,7 +506,8 @@ class StateDB:
         return slice, count
 
     def reebills(self, session, include_unissued=True):
-        '''Generates (account, sequence) tuples for all reebills in MySQL.'''
+        '''Generates (account, sequence, max version) tuples for all reebills
+        in MySQL.'''
         for account in self.listAccounts(session):
             for sequence in self.listSequences(session, account):
                 reebill = self.get_reebill(session, account, sequence)
@@ -587,8 +589,10 @@ class StateDB:
         for service, period_end in service_iter:
             # First, query to find the next unattached utilbill on this account for this customer and this service
             try:
-                utilbill = session.query(UtilBill).filter(UtilBill.customer==customer, UtilBill.service==service, UtilBill.period_start>=period_end, UtilBill.rebill_id == None)\
-                    .order_by(asc(UtilBill.period_start)).first()
+                utilbill = session.query(UtilBill).filter(
+                        UtilBill.customer==customer, UtilBill.service==service,
+                        UtilBill.period_start>=period_end, UtilBill.rebill_id
+                        == None).order_by(asc(UtilBill.period_start)).first()
             except NoResultFound:
                 # If the utilbill is not found, then the rolling process can't proceed
                 raise Exception('No new %s utility bill found' % service)
