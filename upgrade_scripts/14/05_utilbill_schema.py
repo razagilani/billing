2fe24d1956aae9794cc0c7c4bd7ed3e4af2fc0d3
Daniel Klothe <dklothe@skylineinnovations.com>
2012-12-19 13:02:39 -0500

    05_utilbill_schema.py may be working

--- upgrade_scripts/14/05_utilbill_schema.py
+++ upgrade_scripts/14/05_utilbill_schema.py
@@ -1,5 +1,6 @@
 import MySQLdb
 import pymongo
+from billing.util.dateutils import date_to_datetime
 
 con = MySQLdb.Connection('localhost', 'dev', 'dev', 'skyline_dev')
 db = pymongo.Connection('localhost')['skyline-dev']
@@ -29,7 +30,7 @@ cur.execute("alter table utilbill_version add column utility varchar(45) not nul
 #cur.execute("insert into utilbill_version select (rebill_id, period_start, period_end, service, total_charges) from utilbill")
 cur.execute("select customer.account, customer_id, rebill_id, period_start, period_end, service, total_charges from utilbill join customer where customer_id = customer.id order by customer.account, utilbill.period_start")
 for account, customer_id, rebill_id, start, end, service, total_charges in cur.fetchall():
-    mongo_docs = db.findOne({'account': account, 'start': start, 'end': end, 'service': service})
+    mongo_docs = db.utilbill.find({'account': account, 'start': date_to_datetime(start), 'end': date_to_datetime(end), 'service': service})
 
     # create one row in utilbill_version for each mongo document, and put the row's id in the document
     for doc in mongo_docs:
@@ -39,13 +40,20 @@ for account, customer_id, rebill_id, start, end, service, total_charges in cur.f
         doc['mysql_id'] = cur.lastrowid
 
 # add constraint for uniqueness on {account, service, utility, start, end} (which we already enforce for mongo documents in rebillDAO)
-cur.execute("alter table utilbill add constraint utilbill_version_unique unique (customer_id, service, utility, period_start, period_end)")
+cur.execute("alter table utilbill_version add constraint utilbill_version_unique unique (customer_id, service, utility, period_start, period_end)")
 
 ###############################################################################
 # utilbill
 
+#cur.execute("alter table utilbill drop foreign key fk_utilbill_rebill")
+cur.execute("drop index fk_utilbill_rebill on utilbill;")
+
 # remove all data that have been moved to the utilbill_version table
-cur.execute("alter table utilbill drop column rebill_id, period_start, period_end, service, total_charges")
+cur.execute("alter table utilbill drop column rebill_id")
+cur.execute("alter table utilbill drop column period_start")
+cur.execute("alter table utilbill drop column period_end")
+cur.execute("alter table utilbill drop column service")
+cur.execute("alter table utilbill drop column total_charges")
 
 con.commit()
 
