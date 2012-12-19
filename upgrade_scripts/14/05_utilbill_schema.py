dd08301226765b9fc5fcbdc8bf38c27004dab863
Daniel Klothe <dklothe@skylineinnovations.com>
2012-12-19 10:53:23 -0500

    05_utilbill_schema.py correctly converts primary key "id" column to foreign key "utilbill_id" column

--- upgrade_scripts/14/05_utilbill_schema.py
+++ upgrade_scripts/14/05_utilbill_schema.py
@@ -14,9 +14,10 @@ cur.execute("create table utilbill_version like utilbill")
 
 # convert promary key column utilbill_version.id into foreign key column "utilbill_id"
 # TODO is there no way to add the foreign key constraint and remove the primary key constraint at the same time as the column is redefined, like other constraints?
+cur.execute("alter table utilbill_version modify id int not null")
+cur.execute("alter table utilbill_version drop primary key")
 cur.execute("alter table utilbill_version change column id utilbill_id int(11) not null")
 cur.execute("alter table utilbill_version add constraint foreign key (utilbill_id) references utilbill (id)")
-cur.execute("alter table utilbill_version drop primary key")
 
 # add id column for utilbill_version itself
 cur.execute("alter table utilbill_version add column id int(11) not null auto_increment primary key")
@@ -28,7 +29,7 @@ cur.execute("alter table utilbill_version add column utility varchar(45) not nul
 #cur.execute("insert into utilbill_version select (rebill_id, period_start, period_end, service, total_charges) from utilbill")
 cur.execute("select customer.account, customer_id, rebill_id, period_start, period_end, service, total_charges from utilbill join customer where customer_id = customer.id order by customer.account, utilbill.period_start")
 for account, customer_id, rebill_id, start, end, service, total_charges in cur.fetchall():
-    mongo_docs = db.findOne({'account': account, 'start': period_start, 'end': period_end, 'service': service})
+    mongo_docs = db.findOne({'account': account, 'start': start, 'end': end, 'service': service})
 
     # create one row in utilbill_version for each mongo document, and put the row's id in the document
     for doc in mongo_docs:
