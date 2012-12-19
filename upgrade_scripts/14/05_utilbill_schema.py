16acb98cacd3073ef3d909ecf14389670a76c431
Daniel Klothe <dklothe@skylineinnovations.com>
2012-12-19 10:23:20 -0500

    changes to utilbill upgrade script; made it not depend on 04_reebill_schema.py

--- upgrade_scripts/14/05_utilbill_schema.py
+++ upgrade_scripts/14/05_utilbill_schema.py
@@ -12,10 +12,11 @@ cur = con.cursor()
 
 cur.execute("create table utilbill_version like utilbill")
 
-# convert utilbill_version.id into foreign key column "utilbill_id"
-# TODO is there no way to add the foreign key constraint at the same time as the column is redefined, like other constraints?
+# convert promary key column utilbill_version.id into foreign key column "utilbill_id"
+# TODO is there no way to add the foreign key constraint and remove the primary key constraint at the same time as the column is redefined, like other constraints?
 cur.execute("alter table utilbill_version change column id utilbill_id int(11) not null")
 cur.execute("alter table utilbill_version add constraint foreign key (utilbill_id) references utilbill (id)")
+cur.execute("alter table utilbill_version drop primary key")
 
 # add id column for utilbill_version itself
 cur.execute("alter table utilbill_version add column id int(11) not null auto_increment primary key")
@@ -26,17 +27,17 @@ cur.execute("alter table utilbill_version add column utility varchar(45) not nul
 # fill utilbill_version with data from mongo (excluding customer_id, processed, state, date_received)
 #cur.execute("insert into utilbill_version select (rebill_id, period_start, period_end, service, total_charges) from utilbill")
 cur.execute("select customer.account, customer_id, rebill_id, period_start, period_end, service, total_charges from utilbill join customer where customer_id = customer.id order by customer.account, utilbill.period_start")
-for account, customer_id, reebill_id, start, end, service, total_charges in cur.fetchall():
+for account, customer_id, rebill_id, start, end, service, total_charges in cur.fetchall():
     mongo_docs = db.findOne({'account': account, 'start': period_start, 'end': period_end, 'service': service})
 
     # create one row in utilbill_version for each mongo document, and put the row's id in the document
     for doc in mongo_docs:
         utility_name = doc['utility']
-        cur.execute("insert into utilbill_version (customer_id, reebill_id, period_start, period_end, service, total_charges, utility) values (%s, %s, %s, %s, %s, %s, %s)",
-                (reebill_id, start, end, service, total_charges, utility))
+        cur.execute("insert into utilbill_version (customer_id, rebill_id, period_start, period_end, service, total_charges, utility) values (%s, %s, %s, %s, %s, %s, %s)",
+                (rebill_id, start, end, service, total_charges, utility))
         doc['mysql_id'] = cur.lastrowid
 
-# add constraint for uniqueness on {account, service, utility, start, end} (which we already enforce for mongo documents in ReeBillDAO)
+# add constraint for uniqueness on {account, service, utility, start, end} (which we already enforce for mongo documents in rebillDAO)
 cur.execute("alter table utilbill add constraint utilbill_version_unique unique (customer_id, service, utility, period_start, period_end)")
 
 ###############################################################################
