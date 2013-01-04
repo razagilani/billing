2f6a9ccf8320b3056d441a4e78229e8c053f8ffe
Daniel Klothe <dklothe@skylineinnovations.com>
2013-01-04 18:29:12 -0500

    removed prints from 04_reebill_schema.py

--- upgrade_scripts/14/04_reebill_schema.py
+++ upgrade_scripts/14/04_reebill_schema.py
@@ -5,7 +5,7 @@ cur = con.cursor()
 
 cur.execute('''select count(*), sum(max_version) from rebill''')
 initial_count, new_rows = cur.fetchall()[0]
-print '%s rows exist; %s new rows should be added' % (initial_count, new_rows)
+print 'rebill table: %s rows exist; %s new rows should be added' % (initial_count, new_rows)
 
 # remove constraint that enforces uniqueness of {customer_id, sequence}
 # (in "show index from rebill" this appears as 2 separate indices, one on customer_id and another on sequence)
@@ -14,11 +14,11 @@ cur.execute('''drop index unique_constraint on rebill;''')
 # add another row to rebill for each version from 0 up to max_version
 cur.execute('''select max_version, sequence, customer_id, issued from rebill''')
 for row in cur.fetchall():
-    print row
+    #print row
     for v in range(row[0]):
         statement = '''insert into rebill (max_version, sequence,
                 customer_id, issued) values (%s, %s, %s, %s)'''
-        print statement % ((v,) + row[1:])
+        #print statement % ((v,) + row[1:])
         cur.execute(statement, (v,) + row[1:])
 
 # make sure the right number of rows was added
@@ -34,3 +34,4 @@ cur.execute('''alter table rebill change max_version version int(11)''')
 #cur.execute('''rename table rebill to reebill''')
 
 con.commit()
+print 'rebill upgrade done'
