85a74df2cb1f69094e9ebd5a48ef3bae2b27a22d
Daniel Stiles <dstiles@skylineinnovations.com>
2013-01-31 14:17:45 -0500

    43590609 fix unable to render unissued reebills

--- processing/render.py
+++ processing/render.py
@@ -382,7 +382,7 @@ class ReebillRenderer:
         # populate account number, bill id & issue date
         accountNumber = [
             [Paragraph("Account Number", styles['BillLabelRight']),Paragraph(reebill.account + " " + str(reebill.sequence),styles['BillField'])], 
-            [Paragraph("Issue Date", styles['BillLabelRight']), Paragraph(reebill.issue_date.strftime('%m-%d-%Y'), styles['BillField'])]
+            [Paragraph("Issue Date", styles['BillLabelRight']), Paragraph(reebill.issue_date.strftime('%m-%d-%Y') if reebill.issue_date is not None else 'None', styles['BillField'])]
         ]
 
         t = Table(accountNumber, [135,85])
@@ -393,7 +393,7 @@ class ReebillRenderer:
 
         # populate due date and amount
         dueDateAndAmount = [
-            [Paragraph("Due Date", styles['BillLabelRight']), Paragraph(reebill.due_date.strftime('%m-%d-%Y'), styles['BillFieldRight'])], 
+            [Paragraph("Due Date", styles['BillLabelRight']), Paragraph(reebill.due_date.strftime('%m-%d-%Y') if reebill.due_date is not None else 'None', styles['BillFieldRight'])], 
             [Paragraph("Balance Due", styles['BillLabelRight']), Paragraph(str(reebill.balance_due.quantize(Decimal("0.00"))), styles['BillFieldRight'])]
         ]
         
