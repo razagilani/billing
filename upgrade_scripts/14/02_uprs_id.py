b0425b45a0404d0edfaa8083485467fe33e05c5f
Daniel Stiles <dstiles@skylineinnovations.com>
2013-02-05 17:18:55 -0500

    Update upgrade script for solution of 43874711 by removing effective and expires from uprs id

--- upgrade_scripts/14/02_uprs_id.py
+++ upgrade_scripts/14/02_uprs_id.py
@@ -58,8 +58,8 @@ for reebill in db.reebills.find():
     new_uprs['_id']['account'] = reebill['_id']['account']
     new_uprs['_id']['sequence'] = reebill['_id']['sequence']
     new_uprs['_id']['version'] = reebill['_id']['version']
-    assert 'effective' in new_uprs['_id']
-    assert 'expires' in new_uprs['_id']
+    del new_uprs['_id']['effective']
+    del new_uprs['_id']['expires']
 
     uprs_ids_to_remove.append(original_uprs['_id'])
     db.ratestructure.save(new_uprs)
