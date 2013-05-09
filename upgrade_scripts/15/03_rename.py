76b2b58c2423a22102322e7a6b502d0659959094
Daniel Klothe <dklothe@skylineinnovations.com>
2013-05-09 12:29:23 -0400

    49554839 don't re-add constraints until after table/column renaming is done

--- upgrade_scripts/15/03_rename.py
+++ upgrade_scripts/15/03_rename.py
@@ -7,3 +7,7 @@ cur = con.cursor()
 cur.execute('''rename table rebill to reebill''')
 cur.execute('''alter table utilbill_version change column rebill_id reebill_id int(11)''')
 
+# re-add constraints that were removed when utilbill_version was created (this has to come after renaming)
+cur.execute("alter table utilbill_version add constraint fk_utilbill_version_customer foreign key (customer_id) references customer (id)")
+cur.execute("alter table utilbill_version add constraint fk_utilbill_version_utilbill foreign key (utilbill_id) references utilbill (id)")
+cur.execute("alter table utilbill_version add constraint fk_utilbill_version_reebill foreign key (reebill_id) references reebill (id)")
