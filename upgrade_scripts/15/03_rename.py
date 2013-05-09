cdc56ed25e392dc20ce8969a577a46cd81f72700
Daniel Klothe <dklothe@skylineinnovations.com>
2013-05-09 13:22:16 -0400

    49554839 upgrade scripts change "key indices" or whatever they're called so the names match the foreign key constraint names; hopefully this makes SQLAlchemy recognize the relationships between tables

--- upgrade_scripts/15/03_rename.py
+++ upgrade_scripts/15/03_rename.py
@@ -7,7 +7,14 @@ cur = con.cursor()
 cur.execute('''rename table rebill to reebill''')
 cur.execute('''alter table utilbill_version change column rebill_id reebill_id int(11)''')
 
+# delete "key indices" (i.e. those lines starting with "KEY" in the "show
+# create table" output) remaining from when the utilbill_version table was the
+# utilbill table, in preparation for adding new constraints below.
+cur.execute("alter table utilbill_version drop key fk_utilbill_customer")
+cur.execute("alter table utilbill_version drop key fk_utilbill_rebill")
+
 # re-add constraints that were removed when utilbill_version was created (this has to come after renaming)
-cur.execute("alter table utilbill_version add constraint fk_utilbill_version_customer foreign key (customer_id) references customer (id)")
-cur.execute("alter table utilbill_version add constraint fk_utilbill_version_utilbill foreign key (utilbill_id) references utilbill (id)")
-cur.execute("alter table utilbill_version add constraint fk_utilbill_version_reebill foreign key (reebill_id) references reebill (id)")
+cur.execute("alter table utilbill_version add constraint fk_utilbill_version_customer foreign key (customer_id) references customer (id) on delete no action on update no action")
+cur.execute("alter table utilbill_version add constraint fk_utilbill_version_reebill foreign key (reebill_id) references reebill (id) on delete no action on update no action")
+
+con.commit()
