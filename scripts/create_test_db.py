232864980a60a05eac4292f0af97e647bc92fda3
Daniel Klothe <dklothe@skylineinnovations.com>
2013-05-13 15:31:33 -0400

    comments/formatting

--- scripts/create_test_db.py
+++ scripts/create_test_db.py
@@ -3,22 +3,24 @@
 database, but empty. Any existing database of the same name is replaced.'''
 from MySQLdb import Connection
 
-USER, PASSWORD = 'root', 'root'
+USER, PASSWORD = 'root', 'root' # use root because only root or the "definer" can create a view
 DEV_DB, TEST_DB = 'skyline_dev', 'test'
 
 con = Connection('localhost', USER, PASSWORD)
 cur = con.cursor()
 
-# remove test database and re-create it, so it's guaranteed to exist and has no
-# tables
+# remove test database and re-create it, so it's guaranteed to exist and have
+# no tables
 cur.execute("drop database if exists %s" % TEST_DB)
 cur.execute("create database %s" % TEST_DB)
 cur.execute("use %s" % TEST_DB)
 
 # get table/view names from dev db
-cur.execute("select table_name from information_schema.tables where table_schema = '%s' and table_type = 'BASE TABLE'" % DEV_DB);
+cur.execute('''select table_name from information_schema.tables where
+        table_schema = '%s' and table_type = "BASE TABLE"''' % DEV_DB);
 dev_tables = [x[0] for x in cur.fetchall()]
-cur.execute("select table_name from information_schema.tables where table_schema = '%s' and table_type = 'VIEW'" % DEV_DB);
+cur.execute('''select table_name from information_schema.tables where
+        table_schema = '%s' and table_type = "VIEW"''' % DEV_DB);
 dev_views = [x[0] for x in cur.fetchall()]
 
 # copy tables from dev db to test db
