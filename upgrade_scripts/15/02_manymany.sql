dbf58a55d4c413e290e0df616776af0434ac042c
Daniel Klothe <dklothe@skylineinnovations.com>
2013-05-14 13:02:29 -0400

    added deletion cascade to new utilbill_reebill table

--- upgrade_scripts/15/02_manymany.sql
+++ upgrade_scripts/15/02_manymany.sql
@@ -1,11 +1,13 @@
 use skyline_dev;
 
 -- create junction table for utilbill/reebill relationship
+-- note the cascade: rows from this table automatically get deleted when either
+-- the utilbill or the reebill is deleted
 create table utilbill_reebill (
     utilbill_id int(11) not null,
     reebill_id int(11) not null,
-    foreign key (utilbill_id) references utilbill (id),
-    foreign key (reebill_id) references rebill (id),
+    foreign key (utilbill_id) references utilbill (id) on delete cascade,
+    foreign key (reebill_id) references rebill (id) on delete cascade,
     primary key (utilbill_id, reebill_id)
 );
 -- copy data from utilbill.rebill_id into the new table
