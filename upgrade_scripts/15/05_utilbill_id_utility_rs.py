40459ac2dacdee686e094ab4579f1c0c288d0b4b
? <dklothe@skylineinnovations.com>
2013-06-24 16:46:02 -0400

    code cleanup/comments in 05_utilbill_id_utility_rs.py

--- upgrade_scripts/15/05_utilbill_id_utility_rs.py
+++ upgrade_scripts/15/05_utilbill_id_utility_rs.py
@@ -42,8 +42,6 @@ for mysql_id, account, service, start, end in cur.fetchall():
     # put mongo document id in MySQL table
     cur.execute("update utilbill set utility = '%s', rate_class = '%s', document_id = '%s' where id = %s" % (mongo_doc['utility'], mongo_doc['rate_structure_binding'], mongo_doc['_id'], mysql_id))
 
-    print "%s: success" % mysql_id
-
 # check for success; abort if any rows did not get a document id
 cur.execute("select count(*) from utilbill where document_id = ''")
 count = cur.fetchone()[0]
@@ -54,44 +52,7 @@ if count > 0:
 # NOTE currently about 1/3 of utility bill rows did not match a document
 
 
-#ids = set()
-#
-#cur = con.cursor()
-#for doc in db.utilbills.find({'sequence': {'$exists': False}}):
-#    # find MySQL id
-#    query = '''select utilbill.id from utilbill join customer where utilbill.customer_id = customer.id and account = "%s" and service = "%s" and period_start = "%s" and period_end = "%s"''' % (doc['account'], doc['service'], doc['start'], doc['end'])
-#    cur.execute(query)
-#    result = cur.fetchone()
-#    if result is None:
-#        print "Couldn't find utilbill document for query %s" % query
-#        continue
-#    mysql_id = result[0]
-#
-#    print doc['_id']
-#
-#    # find reebill document
-#    reebills = db.reebills.find({'utilbills.id': doc['_id']})
-#    if reebills.count() == 0:
-#        print "    No reebills with utility bill id %s" % doc['_id']
-#    elif reebills.count() > 1:
-#        print "    Multiple reebills (%s) with utility bill id %s" % (reebills.count(), doc['_id'])
-#
-#    # update reebill document if necessary
-#    for reebill in reebills: # handle multiple reebills even though there should never be more than 1
-#        for utilbill_subdoc in reebill['utilbills']:
-#            if utilbill_subdoc['id'] == doc['_id']:
-#                utilbill_subdoc['id'] = mysql_id
-#                print "    Updated reebill ", reebill['_id']
-#        #db.reebills.save(reebill)
-#
-#    # update utilbill document
-#    doc['_id'] = mysql_id
-#
-#    #db.utilbills.save(doc)
-#
-## TODO what id goes in attached/non-editable utility bills? arbitrary ObjectID or some combination of original MySQL id, account, sequence?
-
-
+# insert mongo document ids in document_id column of utilbill_reebill to represent frozen utility bill documents
 cur.execute("select reebill.id, customer.account, reebill.sequence, reebill.version from reebill join customer where reebill.customer_id = customer.id")
 for mysql_id, account, sequence, version, in cur.fetchall():
     # get mongo id of editable utility bill document
