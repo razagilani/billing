a78a14890d03aaa68b3e2afef875383ecac48cfb
? <dklothe@skylineinnovations.com>
2013-07-15 16:45:47 -0400

    00_addresses.py converts reebill adddress format before copying to utility bill document

--- upgrade_scripts/15/00_addresses.py
+++ upgrade_scripts/15/00_addresses.py
@@ -1,9 +1,20 @@
 '''Replace utility bill "billing_address" and "service_address" fields with "billing_address" and "service_address" from reebill documents, using the same schema as in reebills.'''
 from pymongo import Connection
 from sys import stderr
+from billing.processing.mongo import utilbill_billing_address_to_reebill_address, utilbill_service_address_to_reebill_address, reebill_address_to_utilbill_address
 db = Connection('localhost')['skyline-dev']
 
 for reebill in db.reebills.find():
+    # convert address schema
+    try:
+        billing_address = reebill_address_to_utilbill_address(reebill['billing_address'])
+        service_address = reebill_address_to_utilbill_address(reebill['service_address'])
+    except KeyError:
+        print >> stderr, 'bad address schema in', reebill['_id']
+        print >> stderr, reebill['service_address']
+        print >> stderr, reebill['billing_address']
+        continue
+
     utilbill_ids = [u['id'] for u in reebill['utilbills']]
     utilbills = db.utilbills.find({'_id': {'$in': utilbill_ids}})
 
