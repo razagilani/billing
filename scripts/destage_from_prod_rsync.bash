b0029da765cf150b9297139626e1bdbd43975077
Josh Galicic <jgalicic@skylineinnovations.com>
2013-07-17 11:40:19 -0400

    53507715 added arg to destage_from_prod_rsync to not download files.

--- scripts/destage_from_prod_rsync.bash
+++ scripts/destage_from_prod_rsync.bash
@@ -1,45 +1,44 @@
 #!/bin/bash -v
 
 USAGE="
-Usage: $0 PRODHOST TOENV MYSQLPASSWORD
+Usage: $0 PRODHOST TOENV MYSQLPASSWORD -n 
      De-stages production ReeBill data to the specified environment.
      PRODHOST -- parameter specifying the hostname containing production data (e.g. tyrell-prod).
      TOENV -- parameter specifying the environment to be targeted by the de-stage (e.g. stage, dev).
      MYSQLPASSWORD -- local mysql admin password
+     -n -- optional, use already downloaded files, skipping rsync 
+             
      "
-
 # All SaaS environments (yourhost.yourdomain.com) are additionally named within the *.skylineinnovations.net DNS namespace
 # therefore, hosts do not need to be qualified with skylineinnovations.net
 
 : ${1?"$USAGE"} 
 
-if [ $# -ne 3 ]; then
+if [ $# -lt 3 ]; then
     echo "Specify args."
     exit 1
 fi
 
-MYSQLPASSWORD=$3
 PRODHOST=$1 
 TOENV=$2
+MYSQLPASSWORD=$3
+RSYNC=$4
 
-# Script to restore development MySQL, Mongo collections, and filesystem from
-# backups on tyrell-prod. Based on billing/reebill/admin/destage.bash.
-now=`date +"%Y%m%d"`
 # need to more uniquely name backup file
+now=`date +"%Y%m%d"`
 destage_dir=${now}reebill-prod
 ssh_key=$HOME/Dropbox/IT/ec2keys/$PRODHOST.pem
 # Save current directory to CD back to it
 current_dir="$( cd "$( dirname "$0" )" && pwd)"
 
 cd /tmp
-
-#rsync -ahz --exclude 'db_prod' --progress -e "ssh -i ${ssh_key}" ec2-user@$PRODHOST.skylineinnovations.net:/tmp/$destage_dir . 
-rsync -ahz --progress -e "ssh -i ${ssh_key}" ec2-user@$PRODHOST.skylineinnovations.net:/tmp/$destage_dir/${now}billing_mysql.dmp . 
-rsync -ahz --progress -e "ssh -i ${ssh_key}" ec2-user@$PRODHOST.skylineinnovations.net:/tmp/$destage_dir/${now}ratestructure_mongo . 
-rsync -ahz --progress -e "ssh -i ${ssh_key}" ec2-user@$PRODHOST.skylineinnovations.net:/tmp/$destage_dir/${now}reebills_mongo . 
-rsync -ahz --progress -e "ssh -i ${ssh_key}" ec2-user@$PRODHOST.skylineinnovations.net:/tmp/$destage_dir/${now}journal_mongo . 
-rsync -ahz --progress -e "ssh -i ${ssh_key}" ec2-user@$PRODHOST.skylineinnovations.net:/tmp/$destage_dir/${now}users_mongo . 
-rsync -ahz --progress -e "ssh -i ${ssh_key}" ec2-user@$PRODHOST.skylineinnovations.net:/tmp/$destage_dir/${now}utilbills_mongo . 
+if [ -z $RSYNC ] || [ $RSYNC != '-n' ]
+then
+    rsync -ahz --exclude 'db-prod' --progress -e "ssh -i ${ssh_key}" ec2-user@$PRODHOST.skylineinnovations.net:/tmp/$destage_dir . 
+else
+    echo "Skipping file download due to -n flag"
+fi
+cd ${now}reebill-prod
 
 # apparently only root can restore the database
 mysql -uroot -p$MYSQLPASSWORD -e "create database if not exists skyline_${TOENV};"
