6aa2ad3d3aec240f41118d1ac57cf068c2ef0714
? <dklothe@skylineinnovations.com>
2013-08-29 17:25:08 -0400

    05_unfreeze.py does not update document_id in utilbill table (which doesn't exist yet)

--- upgrade_scripts/17/05_unfreeze.py
+++ upgrade_scripts/17/05_unfreeze.py
@@ -12,7 +12,11 @@ db = pymongo.Connection('localhost')['skyline-dev']
 con = MySQLdb.Connection('localhost', 'dev', 'dev', 'skyline_dev')
 con.autocommit(False)
 
-# iterate through all unissued version-0 reebill documents
+# remove all editable utility bill documents so that better ones can be
+# recreated from the frozen ones
+db.utilbills.remove({'sequence': {'$exists': False}})
+
+# iterate through all unissued version-0 reebills
 cur = con.cursor()
 cur.execute('select distinct customer.account, sequence, utilbill_id from reebill, customer, utilbill_reebill where customer_id = customer.id and reebill_id = reebill.id and version = 0 and issued != 1 order by account, sequence')
 for account, sequence, utilbill_id in cur.fetchall():
@@ -21,7 +25,7 @@ for account, sequence, utilbill_id in cur.fetchall():
     for utilbill_subdoc in reebill['utilbills']:
         utilbill = db.utilbills.find_one({'_id': utilbill_subdoc['id']})
         if utilbill is None:
-            print >> stderr, 'No utility bill for reebill %s-%s-%s, id %s' % (reebill['_id']['account'], reebill['_id']['sequence'], reebill['_id']['version'], utilbill_subdoc['id'])
+            print >> stderr, 'No utility bill document for reebill %s-%s-%s, id %s' % (reebill['_id']['account'], reebill['_id']['sequence'], reebill['_id']['version'], utilbill_subdoc['id'])
             continue
 
         # unissued version-0 reebills always have "sequence" and "version" keys in their utility bill. if not, something is wrong.
@@ -52,6 +56,8 @@ for account, sequence, utilbill_id in cur.fetchall():
         db.utilbills.save(utilbill)
         db.utilbills.remove({'_id': existing_editable_utility_bills[0]['_id']}, True)
 
-        # update MySQL "utilbill" table with utilbill id
-        cur.execute("update utilbill set document_id = '%s' where id = %s" % (utilbill_subdoc['id'], utilbill_id))
+        # NOTE that a new document is not being created, so the document can
+        # keep the same _id and the reebill subdocument "id" field does not
+        # need to be updated
+
 con.commit()
