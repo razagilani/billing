3fecb0079e4dd56af831de79ff4e5552803fdd0c
? <dklothe@skylineinnovations.com>
2013-09-27 18:38:30 -0400

    57185468 fixed "syntax error" in which mongo shell expects semicolon at start of file

--- upgrade_scripts/17/rs_mongoengine.js
+++ upgrade_scripts/17/rs_mongoengine.js
@@ -1,8 +1,9 @@
 /* Add keys to rate structure documents to enable MoneoEngine. */
+;
 use skyline-dev;
 
 // add "_cls" key to UPRS/CPRS documents (class RateStructure)
-db.ratestructure.update({type: {$ne: 'URS'}}, {$set: {_cls: 'RateStructure'}}, {multi: true})
+db.ratestructure.update({type: {$ne: 'URS'}}, {$set: {_cls: 'RateStructure'}}, {multi: true});
 
 // add "_cls" key to URS documents (class URS)
-db.ratestructure.update({$or: [{type: 'URS'}, {'_id.type': 'URS'}]}, {$set: {_cls: 'RateStructure.URS'}}, {multi: true})
+db.ratestructure.update({$or: [{type: 'URS'}, {'_id.type': 'URS'}]}, {$set: {_cls: 'RateStructure.URS'}}, {multi: true});
