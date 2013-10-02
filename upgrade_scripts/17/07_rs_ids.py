cc3c91b48a2efe5b31ae4f4033d269c8bb91ec3c
? <dklothe@skylineinnovations.com>
2013-10-02 18:17:09 -0400

    58115570 07_rs_ids.py handles cases where reebills have multiple utility bills by delaying all RS document deletions until after insertions. also verifies that the increase in RS document count was due only to these cases.

--- upgrade_scripts/17/07_rs_ids.py
+++ upgrade_scripts/17/07_rs_ids.py
@@ -23,9 +23,39 @@ cur.execute("""alter table utilbill_reebill add column cprs_document_id varchar(
 
 cur.execute("begin")
 
-cur.execute("select reebill.id, customer.account, reebill.sequence, reebill.version, reebill.issued, utilbill_id from reebill join customer join utilbill_reebill where reebill.customer_id = customer.id and reebill.id = utilbill_reebill.reebill_id")
+#reebill_query = '''
+#select reebill.id, customer.account, reebill.sequence, reebill.version, reebill.issued, utilbill_id
+#from reebill join customer join utilbill_reebill
+#where reebill.customer_id = customer.id
+#and reebill.id = utilbill_reebill.reebill_id'''
+reebill_query = '''
+select reebill.id, customer.account, reebill.sequence, reebill.version, reebill.issued, utilbill_id
+from reebill join customer join utilbill_reebill
+where reebill.customer_id = customer.id
+and reebill.id = utilbill_reebill.reebill_id'''
+cur.execute(reebill_query)
 initial_count = db.ratestructure.count()
+
+# this set tracks queries for CPRS documents to delete. because some reebills
+# have multiple utility bills (in MySQL) and CPRS documents are identified by
+# reebill sequence/version, there are effectively multiple utility bills
+# sharing the same CPRS documents. therefore CPRS documents can't be deleted
+# until all utilbill_reebill rows that involve them have been processed. note
+# that in the case of a reebill having multiple utility bills, the number of
+# CPRS documents will increase by [number of utility bills] - 1. a set is used
+# because duplicate queries will result in deletion failure after the first.
+# see https://www.pivotaltracker.com/story/show/58115570
+deletes = set()
+
+# count duplicate occurrences of the same reebill in the loop. whenever a
+# reebill occurs more than once, it has multiple utility bills all sharing the
+# same RS documents, and 2 new RS documents will be added. the total number of
+# RS documents should only have increases by the number of these duplicates.
+duplicate_count = 0
+last_reebill_id = None
+
 for reebill_id, account, sequence, version, issued, utilbill_id in cur.fetchall():
+
     # find CPRS and UPRS docs
     cprs_query = {
         '_id.type': 'CPRS', 
@@ -56,10 +86,11 @@ for reebill_id, account, sequence, version, issued, utilbill_id in cur.fetchall(
         continue
     uprs = uprss[0]
 
-    # delete CPRS and UPRS docs from collection
-    # TODO do this after saving replacement, not before
-    check_error(db.ratestructure.remove(cprs_query, safe=True))
-    check_error(db.ratestructure.remove(uprs_query, safe=True))
+    # deletion of old CPRS and UPRS docs is deferred until the end of the loop.
+    # query dict is "encoded" as frozenset of key-value pairs because only
+    # hashable (i.e. immutable) objects can be stored in a set
+    deletes.add(frozenset(uprs_query.items()))
+    deletes.add(frozenset(cprs_query.items()))
 
     # replace ids with new ones, and add "type" field to the body of the document
     cprs['_id'] = ObjectId()
@@ -80,9 +111,23 @@ for reebill_id, account, sequence, version, issued, utilbill_id in cur.fetchall(
     db.ratestructure.insert(cprs)
     db.ratestructure.insert(uprs)
 
-    # total number of rate structure documents should not have changed
-    assert db.ratestructure.count() == initial_count
+    if reebill_id == last_reebill_id:
+        duplicate_count += 1
+    last_reebill_id = reebill_id
 
+# delete all documents in 'deletes'
+for kvp_set in deletes:
+    # frozenset of key-value pairs needs to be "decoded" into a dict
+    query = dict(kvp_set)
+    check_error(db.ratestructure.remove(query, safe=True))
+
+# total number of rate structure documents should not have changed, except when
+# there were reebills sharing the same RS documents, in which case one CPRS and
+# one UPRS is added
+try:
+    assert db.ratestructure.count() == initial_count + 2 * duplicate_count
+except AssertionError:
+    import ipdb; ipdb.set_trace()
 
 # put ids of "template" utility bills in MySQL customer table
 cur.execute("select account, customer.id from customer order by account")
