19fd2adb4e823df09312e5bbed32b43be8471650
Daniel Stiles <dstiles@skylineinnovations.com>
2013-10-10 13:32:28 -0400

    Added cleanup script for account 10008

---
+++ upgrade_scripts/17/cleanup/fix_10008.py
@@ -0,0 +1,11 @@
+import MySQLdb
+
+mysql_host = 'localhost'
+mysql_user = 'root'
+mysql_pass = 'root'
+mysql_db = 'skyline_dev'
+
+mysql_con = MySQLdb.Connection(mysql_host, mysql_user, mysql_pass, mysql_db)
+mysql_cur = mysql_con.cursor()
+
+mysql_cur.execute('''delete from utilbill where id=486;''')
