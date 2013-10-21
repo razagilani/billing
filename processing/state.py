d4fd6fa30501a505ed386032b04a1db454f040f5
? <dklothe@skylineinnovations.com>
2013-10-21 18:32:10 -0400

    fixed bug 59288446 in which issue_date did not get set in MySQL when bills were issued

--- processing/state.py
+++ processing/state.py
@@ -666,6 +666,7 @@ class StateDB(object):
             raise IssuedBillError(("Can't issue reebill %s-%s-%s because it's "
                     "already issued") % (account, sequence, reebill.version))
         reebill.issued = 1
+        reebill.issue_date = datetime.utcnow().date()
 
     def is_issued(self, session, account, sequence, version='max',
             nonexistent=None):
