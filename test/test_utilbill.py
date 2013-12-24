51fe6141e3a4931e52aeee4d6ad81ac6a9a47a78
Thomas Hoffmann <thoffmann@skylineinnovations.com>
2013-12-24 13:38:41 -0500

    62927706 test_utilbill.test_get_service_address directly accesses example_data.example_utilbill

--- test/test_utilbill.py
+++ test/test_utilbill.py
@@ -310,7 +310,7 @@ class UtilBillTest(utils.TestCase):
                 'METER', 'Insert register ID here', register_id='REGISTER')
 
     def test_get_service_address(self):
-        utilbill_doc = example_data._example_utilbill
+        utilbill_doc = example_data.get_utilbill_dict('10003')
         address = mongo.get_service_address(utilbill_doc)
         self.assertEqual(address['postal_code'],'20010')
         self.assertEqual(address['city'],'Washington')
