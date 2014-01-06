a50b05a3557c0d34a453c6e0e6887f02cc8e6dd1
? <dklothe@skylineinnovations.com>
2014-01-06 17:02:47 -0500

    61777944 added test_a.py and test_b.py for investigating nosetests mystery

---
+++ test/test_b.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestTheThing(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_that_fails(self):
+        raise Exception
