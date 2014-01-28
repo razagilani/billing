74db5f12d330b905cd24fb83ee5071f8522c39c6
? <dklothe@skylineinnovations.com>
2014-01-28 18:11:48 -0500

    63030292 charge "descriptions" are non-editable and copied from corresponding RSIs

--- reebill/ui/billedit.js
+++ reebill/ui/billedit.js
@@ -2563,6 +2563,7 @@ function reeBillReady() {
                 header: 'Description',
                 width: 75,
                 sortable: true,
+                editable: false,
                 dataIndex: 'description',
                 editor: new Ext.form.TextField({allowBlank: false})
             },{
