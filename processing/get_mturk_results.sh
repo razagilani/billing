d1bc141d5dd3642c910ab2805e7730f2a8bb4424
Josh Galicic <jgalicic@skylineinnovations.com>
2014-03-06 13:55:14 -0500

    slices go into Dropbox public folder for turk access

--- processing/get_mturk_results.sh
+++ processing/get_mturk_results.sh
@@ -2,4 +2,4 @@
 
 export JAVA_HOME=/usr/local/java/jre1.7.0_40
 cd /tmp/aws-mturk-clt-1.3.1/bin
-./getResults.sh -sandbox -outputfile /tmp/slices/$1.results -successfile /tmp/slices/$1.success
+./getResults.sh -outputfile /tmp/slices/$1.results -successfile /tmp/slices/$1.success
