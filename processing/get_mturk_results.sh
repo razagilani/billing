0550ad32f873d9b9ff28ff1144b2079707b02292
Josh Galicic <jgalicic@skylineinnovations.com>
2014-03-13 11:04:19 -0400

    Switch to the sandbox turk environment, also configured correct java home configuration

--- processing/get_mturk_results.sh
+++ processing/get_mturk_results.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+export JAVA_HOME=/usr/lib/jvm/jre
 cd /tmp/aws-mturk-clt-1.3.1/bin
-./getResults.sh -outputfile /tmp/slices/$1.results -successfile /tmp/slices/$1.success
+./getResults.sh -sandbox -outputfile /tmp/slices/$1.results -successfile /tmp/slices/$1.success
--- processing/load_mturk_hit.sh
+++ processing/load_mturk_hit.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
+export JAVA_HOME=/usr/lib/jvm/jre
 cd /tmp/aws-mturk-clt-1.3.1/bin
-echo $1
-echo $2
-./loadHITs.sh -properties $2 -input /tmp/slices/$1.input -question /tmp/slices/$1.question -label /tmp/slices/$1
+./loadHITs.sh -sandbox -properties $2 -input /tmp/slices/$1.input -question /tmp/slices/$1.question -label /tmp/slices/$1
