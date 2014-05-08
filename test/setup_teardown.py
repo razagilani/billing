cfa4609d1f69eeceac4a557fc57fd83e99134056
? <dklothe@skylineinnovations.com>
2014-05-08 16:47:38 -0400

    removed dead code

--- test/setup_teardown.py
+++ test/setup_teardown.py
@@ -3,22 +3,19 @@ import unittest
 from StringIO import StringIO
 import ConfigParser
 import logging
-from mock import Mock
 import pymongo
 from bson import ObjectId
 import mongoengine
 import MySQLdb
 from datetime import date, datetime, timedelta
-from billing.util import dateutils
 from billing.processing import mongo
 from billing.processing import rate_structure2
-from billing.processing.process import Process, IssuedBillError
-from billing.processing.state import StateDB, ReeBill, Customer, UtilBill
+from billing.processing.process import Process
+from billing.processing.state import StateDB, Customer
 from billing.processing.billupload import BillUpload
 from billing.processing.bill_mailer import Mailer
 from billing.processing.render import ReebillRenderer
 from billing.processing.fetch_bill_data import RenewableEnergyGetter
-from billing.util.dictutils import deep_map
 from billing.test import example_data
 from nexusapi.nexus_util import MockNexusUtil
 from skyliner.mock_skyliner import MockSplinter, MockSkyInstall
@@ -33,9 +30,6 @@ class TestCaseWithSetup(unittest.TestCase):
         NexusUtil.'''
         # show long diffs for failed dict equality assertions
         self.maxDiff = None
-
-        # clear SQLAlchemy mappers so StateDB can be instantiated again
-        #sqlalchemy.orm.clear_mappers()
 
         # everything needed to create a Process object
         config_file = StringIO('''[runtime]
@@ -59,12 +53,6 @@ port = 27017
         mock_install_2 = MockSkyInstall(name='example-2')
         self.splinter = MockSplinter(deterministic=True,
                 installs=[mock_install_1, mock_install_2])
-        
-        # temporary hack to get a bill that's always the same
-        # this bill came straight out of mongo (except for .date() applied to
-        # datetimes)
-        ISODate = lambda s: datetime.strptime(s, dateutils.ISO_8601_DATETIME)
-        true, false = True, False
 
         # customer database ("test" database has already been created with
         # empty customer table)
@@ -136,7 +124,7 @@ port = 27017
                 'olap': 'example-2',
                 'casualname': 'Example 2',
                 'primus': '1786 Massachusetts Ave.',
-                },
+            },
         ])
 
         bill_mailer = Mailer({
