76607b648dbf356c463356ae47561336ff63819e
Michael Naber <mickeybob@gmail.com>
2014-05-14 14:01:46 -0400

    Added Alembic and created the Charges table.

--- __init__.py
+++ __init__.py
@@ -1,19 +1,14 @@
-  
+import sys
+import os.path as path
+
+__version__ = '21'
+
 __all__ = ['util', 'processing', 'init_logging', 'init_config', 'init_model',
-           'initialize', 'config']
+           'initialize', 'config', 'update_path']
 
 log = None
 config = None
 
-def init_logging(path='settings.cfg'):
-    """Initializes logging"""
-    import logging, logging.config
-    global log
-    
-    logging.config.fileConfig(path)
-    log = logging.getLogger(__name__)
-    log.debug('Initialized logging')
-    
 def init_config(path='settings.cfg'):
     """Sets `billing.config` to an instance of 
     :class:`billing.lib.config.ValidatedConfigParser`.
@@ -32,21 +27,30 @@ def init_config(path='settings.cfg'):
         config.add_section('main')
     config.set('main', 'appdir', "/".join(dirname(__file__).split("/")[:-1]))
     log.debug('Initialized configuration')
+
+def init_logging(path='settings.cfg'):
+    """Initializes logging"""
+    import logging, logging.config
+    global log
+    
+    logging.config.fileConfig(path)
+    log = logging.getLogger(__name__)
+    log.debug('Initialized logging')
     
 def init_model(uri=None):
-    """Initializes the data model
+    """Initializes the sqlalchemy data model. 
     """
-    from billing.processing.state import Session, Base
+    from billing.processing.state import Session, Base, check_schema_revision
     from sqlalchemy import create_engine
     uri = uri if uri else config.get('statedb', 'uri')
-    log.debug('Intializing model with uri %s' % uri)
+    log.debug('Intializing sqlalchemy model with uri %s' % uri)
     engine = create_engine(uri)
     Session.configure(bind=engine)
     Base.metadata.bind = engine
-    log.debug('Initialized model')
+    check_schema_revision()
+    log.debug('Initialized sqlalchemy model')
 
 def initialize():
     init_logging()
     init_config()
     init_model()
-
--- upgrade_scripts/v21/__init__.py
+++ upgrade_scripts/v21/__init__.py
@@ -1,7 +1,24 @@
-from billing.upgrade_scripts.v21.migrate_utilbill_charges_from_mongodb import migrate_utilbill_charges_from_mongodb
-from billing.upgrade_scripts import UpgradeRunner
+"""Upgrade script for version 21. 
 
-class Upgrade(UpgradeRunner):    
-    version = '21'
-    upgrades = [upgrade_alembic_schema, 
-                migrate_utilbill_charges_from_mongodb]
+Script must define `upgrade`, the function for upgrading.
+
+Important: For the purpose of allowing schema migration, this module will be 
+imported with the data model uninitialized! Therefore this module should not 
+import any other code that that expects an initialized data model without first
+calling :func:`.billing.init_model`.  
+"""
+from upgrade_scripts import alembic_upgrade
+import logging
+from billing import init_model
+
+log = logging.getLogger(__name__)
+
+def upgrade():
+    log.info('Beginning upgrade to version 21')
+    alembic_upgrade('55e7e5ebdd29')
+    init_model()
+    
+    ##Code to copy data from mongo goes here
+
+    
+    log.info('Upgrade to version 21 complete')
