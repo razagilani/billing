a9137196fa351cd1044f2e076bd70665a795bcc8
Michael Naber <mickeybob@gmail.com>
2014-05-15 12:02:57 -0400

    Completed migration script to copy data to charge table from mongodb

--- upgrade_scripts/v21/__init__.py
+++ upgrade_scripts/v21/__init__.py
@@ -9,16 +9,44 @@ calling :func:`.billing.init_model`.
 """
 from upgrade_scripts import alembic_upgrade
 import logging
-from billing import init_model
+from pymongo import MongoClient
+from billing import config, init_model
+from billing.processing.state import Session
+from processing.state import Charge, UtilBill
+from bson.objectid import ObjectId
 
 log = logging.getLogger(__name__)
+
+
+def copy_charges_from_mongo():
+
+    client = MongoClient(config.get('billdb', 'host'),
+                         config.get('billdb', 'port'))
+    db = client[config.get('billdb', 'database')]
+    s = Session()
+    assert s.query(Charge).all() == [], "Charges table is not empty"
+    
+    for ub in s.query(UtilBill).all():
+        mongo_ub = db.utilbills.find_one({"_id": ObjectId(ub.document_id)})
+        if mongo_ub is None:
+            log.error("No mongo utility bill found for utilbill"
+                      "   id %s document_id %s" % (ub.id, ub.document_id))
+            continue
+        for mongo_charge in mongo_ub['charges']:
+            log.debug('Adding charge for utilbill id %s' % ub.id)
+            s.add(Charge(ub,
+                         mongo_charge.get('description', ""),
+                         mongo_charge.get('quantity', 0),
+                         mongo_charge.get('quantity_units', ""),
+                         mongo_charge.get('rate', 0),
+                         mongo_charge.get('rsi_binding', ""),
+                         mongo_charge.get('total', 0)))
+    log.info('Commiting to database')
+    s.commit()
 
 def upgrade():
     log.info('Beginning upgrade to version 21')
     alembic_upgrade('55e7e5ebdd29')
     init_model()
-    
-    ##Code to copy data from mongo goes here
-
-    
+    copy_charges_from_mongo()
     log.info('Upgrade to version 21 complete')
