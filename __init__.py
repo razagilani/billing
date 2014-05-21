e498d1b6e1d8182da58638502e9971fe6f278ae5
Michael Naber <michael@skylineinnovations.com>
2014-05-21 14:26:09 -0400

    Updated the TestCaseWithSetup class to use common initialization methods; separated out the setup method into initialization of (1) logging, (2) sqlalchemy model / other service and database dependencies, and (3) insertion of test data

--- __init__.py
+++ __init__.py
@@ -6,42 +6,53 @@ __version__ = '21'
 __all__ = ['util', 'processing', 'init_logging', 'init_config', 'init_model',
            'initialize', 'config', 'update_path']
 
-log = None
 config = None
 
-def init_config(path='settings.cfg'):
+
+def init_config(filename='settings.cfg', fp=None):
     """Sets `billing.config` to an instance of 
     :class:`billing.lib.config.ValidatedConfigParser`.
     
-    :param path: The configuration file path; default `settings.cfg`.
+    :param filename: The configuration file path; default `settings.cfg`.
+    :param fp: A configuration file pointer to be used in place of filename
     """
     from billing.data.validation import configuration as vns
     from billing.lib.config import ValidatedConfigParser
     from os.path import dirname
+    import logging
+
+    log = logging.getLogger(__name__)
     
     global config
     config = ValidatedConfigParser(vns)
-    log.debug('Reading configuration file %s' % path)
-    config.read(path)
+    if fp:
+        log.debug('Reading configuration fp')
+        config.readfp(fp)
+    else:
+        log.debug('Reading configuration file %s' % filename)
+        config.read(filename)
     if not config.has_section('main'):
         config.add_section('main')
     config.set('main', 'appdir', "/".join(dirname(__file__).split("/")[:-1]))
     log.debug('Initialized configuration')
 
+
 def init_logging(path='settings.cfg'):
     """Initializes logging"""
     import logging, logging.config
-    global log
-    
     logging.config.fileConfig(path)
     log = logging.getLogger(__name__)
     log.debug('Initialized logging')
-    
+
+
 def init_model(uri=None):
     """Initializes the sqlalchemy data model. 
     """
     from billing.processing.state import Session, Base, check_schema_revision
     from sqlalchemy import create_engine
+    import logging
+    log = logging.getLogger(__name__)
+
     uri = uri if uri else config.get('statedb', 'uri')
     log.debug('Intializing sqlalchemy model with uri %s' % uri)
     engine = create_engine(uri)
