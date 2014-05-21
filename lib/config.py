dda0965f4d565998ceaaa24919bc10ab4118cb70
Michael Naber <michael@skylineinnovations.com>
2014-05-21 14:26:09 -0400

    Updated the TestCaseWithSetup class to use common initialization methods; separated out the setup method into initialization of (1) logging, (2) sqlalchemy model / other service and database dependencies, and (3) insertion of test data

--- lib/config.py
+++ lib/config.py
@@ -13,15 +13,26 @@ class ValidatedConfigParser(RawConfigParser):
         """
         self._vns = vns
         RawConfigParser.__init__(self, **kwargs)
-    
-    def read(self, filenames):
-        """Reads the configuration file using :meth:`ConfigParser.read` and 
-        runs formencode validators on each configuration section. 
-        """
-        RawConfigParser.read(self, filenames)
+
+    def _validate(self):
+        """Runs formencode validators on each configuration section."""
         for section in self.sections():
             validator = getattr(self._vns, section, None)
             if not validator: continue
             raw_section_vals = dict(self.items(section))
             for k, v in validator.to_python(raw_section_vals).iteritems():
                 self.set(section, k, v)
+
+    def readfp(self, fp, filename=None):
+        """Reads the configuration file using :meth:`ConfigParser.readfp` and
+        runs formencode validators on each configuration section.
+        """
+        RawConfigParser.readfp(self, fp, filename)
+        self._validate()
+
+    def read(self, filenames):
+        """Reads the configuration file using :meth:`ConfigParser.read` and 
+        runs formencode validators on each configuration section. 
+        """
+        RawConfigParser.read(self, filenames)
+        self._validate()
--- upgrade_scripts/__init__.py
+++ upgrade_scripts/__init__.py
@@ -7,61 +7,21 @@ migration, data migration etc. for the version upgrade.
 
 import logging
 from alembic.config import Config
-from billing import config
 from alembic.command import upgrade
-from billing import init_model
 import importlib
 
 log = logging.getLogger(__name__)
+
 
 def run_upgrade(version):
     """Upgrade to a specified version
     
     :param version: the version number of the upgrade
     """
-    upmod = importlib.import_module('billing.upgrade_scripts.v%s' % version)
-    upmod.upgrade()
+    module = importlib.import_module('billing.upgrade_scripts.v%s' % version)
+    module.upgrade()
+
 
 def alembic_upgrade(revision_to):
-    almcfg = Config("alembic.ini")
-    upgrade(almcfg, revision_to)
-
-
-
-'''
-class UpgradeRunner(object):
-    """Encapsulates actions for deployment of a software upgrade.  
-    """
-
-    @staticmethod
-    def alembic_upgrade(revision):
-        """Calls `alembic.command.upgrade` to upgrade to the specified revision.
-        
-        :param revision: the alembic revision to upgrade to
-        """
-        log.info('Upgrading to Alembic revision %s' % revision)
-        almcfg = Config("alembic.ini")
-        upgrade(almcfg, revision)
-    
-    def __init__(self, version,
-                 alembic_revision_from=None,
-                 alembic_revision_to=None):
-        """Construct a new :class:`.UpgradeRunner`.
-        
-        :param version: the version number of the upgrade
-        :param 
-        """
-        self.version = version
-        self.alembic_revision_from = alembic_revision_from
-        self.alembic_revision_to = alembic_revision_to
-
-    def upgrade(self):
-        """Runs the software upgrade: First, an Alembic schema revision update
-        to `self.alembic_revision`, and then each upgrade function within 
-        `self.upgrade_functions`.
-        """
-        if self.alembic_revision_from and self.alembic_revision_to:
-            init_model(schema_revision=self.alembic_revision_from)
-            self.alembic_upgrade(self.alembic_revision_to)
-            init_model(schema_revision=self.alembic_revision_to)
-'''
+    config = Config("alembic.ini")
+    upgrade(config, revision_to)
