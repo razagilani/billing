8251bec9b743a5e8c66714bcc2170ece11f483bc
Michael Naber <michael@skylineinnovations.com>
2014-05-21 14:26:09 -0400

    Updated the TestCaseWithSetup class to use common initialization methods; separated out the setup method into initialization of (1) logging, (2) sqlalchemy model / other service and database dependencies, and (3) insertion of test data

--- exc.py
+++ exc.py
@@ -3,9 +3,14 @@
 The base exception class is :exc:`.BillingError`.
 """
 
+
 class BillingError(Exception):
     """Generic error class."""
+
 
 class DatabaseError(BillingError):
     """Raised when we have a database-related problem"""
 
+
+class InvalidParameter(BillingError):
+    """Raised when function is called with invalid parameters"""
