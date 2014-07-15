b742e00832ed52e1d2de9d6f1dccc0619394a444
? <dklothe@skylineinnovations.com>
2014-07-15 16:33:13 -0400

    removed debugging code in test/test_utilbill.py

--- test/test_utilbill.py
+++ test/test_utilbill.py
@@ -14,7 +14,6 @@ from billing.processing.rate_structure2 import RateStructure, RateStructureItem
 from billing.processing import mongo
 from billing.test.setup_teardown import TestCaseWithSetup
 from billing.processing.exceptions import NoRSIError
-from billing.processing.state import UtilBill
 import example_data
 
 
@@ -418,7 +417,6 @@ class UtilBillTest(TestCaseWithSetup, utils.TestCase):
         }
 
         # simplified version of document with _id 52b455467eb49a52d23d105c
-        # (originally this was a CPRS with an empty UPRS)
         uprs =  RateStructure.from_json('''{
             "_cls" : "RateStructure",
             "type" : "UPRS",
@@ -589,8 +587,6 @@ class UtilBillTest(TestCaseWithSetup, utils.TestCase):
 
         with self.assertRaises(RSIError) as e:
             self.process.refresh_charges(session, utilbill['id'])
-        session.flush()
-        session.commit()
         charges = self.process.get_utilbill_charges_json(session,
                 utilbill['id'])
         self.assertEqual([{
