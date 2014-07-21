174fefc619cfa13aab8791e10fb697c3fe833d93
? <dklothe@skylineinnovations.com>
2014-07-21 19:11:56 -0400

    75402058 removed duplicate definition of ReeBill.update_readings_from_reebill

--- processing/state.py
+++ processing/state.py
@@ -421,22 +421,6 @@ class ReeBill(Base):
 
         return total_therms
 
-    def update_readings_from_reebill(self, reebill_readings, utilbill_doc):
-        '''Updates the set of Readings associated with this ReeBill to match
-        the list of registers in the given reebill_readings. Readings that do not
-        have a register binding that matches a register in 'utilbill_doc' are
-        ignored.
-        '''
-        session = Session.object_session(self)
-        for r in self.readings:
-            session.delete(r)
-        utilbill_register_bindings = {r['register_binding']
-                for r in chain.from_iterable(m['registers']
-                for m in utilbill_doc['meters'])}
-        self.readings = [Reading(r.register_binding, r.measure, 0,
-                0, r.aggregate_function, r.unit) for r in reebill_readings
-                if r.register_binding in utilbill_register_bindings]
-
     def get_renewable_energy_reading(self, register_binding):
         assert isinstance(register_binding, basestring)
         try:
