84eb5b94f7a657756271b2e5f9af0c40ffb520a7
Thomas Hoffmann <thoffmann@skylineinnovations.com>
2014-07-31 17:27:39 -0400

    76075114 Update the cell on updating the formula input

--- reebill/ui/static/app/store/RateStructures.js
+++ reebill/ui/static/app/store/RateStructures.js
@@ -4,18 +4,33 @@ Ext.define('ReeBill.store.RateStructures', {
     model: 'ReeBill.model.Charge',
 
     autoLoad: false,
-    disableCaching: true,
-      
+    autoSync: true,
+
 	proxy: {
-		type: 'ajax',
+		type: 'rest',
 
         pageParam: false,
 
         url: 'http://'+window.location.host+'/reebill/ratestructure',
+
 		reader: {
 			type: 'json',
 			root: 'rows',
 			totalProperty: 'results'
-		}
+		},
+
+        listeners:{
+            exception: function (proxy, response, operation) {
+                Ext.getStore('UtilityBills').rejectChanges();
+                Ext.MessageBox.show({
+                    title: "Server error - " + response.status + " - " + response.statusText,
+                    msg:  response.responseText,
+                    icon: Ext.MessageBox.ERROR,
+                    buttons: Ext.Msg.OK,
+                    cls: 'messageBoxOverflow'
+                });
+            },
+            scope: this
+        }
 	}
 });
