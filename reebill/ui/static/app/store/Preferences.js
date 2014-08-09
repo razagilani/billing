490fee6a5e89cd90048169ced9429f4223ae1564
Thomas Hoffmann <thoffmann@skylineinnovations.com>
2014-08-09 14:52:24 -0400

    75043772 implemented the difference threshold

--- reebill/ui/static/app/store/Preferences.js
+++ reebill/ui/static/app/store/Preferences.js
@@ -40,6 +40,36 @@ Ext.define('ReeBill.store.Preferences', {
 
 	},
 
+    /**
+     * Checks if Preference with key pref exists and retrieves it if it does
+     * If it doesn't, this function creates one and returns it
+     */
+    getOrCreate: function(pref, val){
+        var result = this.find('key', pref);
+        if(result === -1){
+            result = this.add({key: pref, value: val});
+        }else{
+            result = this.getAt(result);
+        }
+        return result;
+    },
+
+    /**
+     * Sets preference pref to val. If it doesn't exist, preferenece pref is
+     * created first
+     */
+    setOrCreate: function(pref, val){
+        var model = this.find('key', pref);
+        if(model === -1){
+            var result = this.add({key: pref, value: val});
+        }else{
+            model = this.getAt(model);
+            model.set('value', val);
+            var result = model;
+        }
+        return model;
+    },
+
     sorters: [{
         property: 'key',
         direction: 'DESC'
