a5145b0e6b2d95bfa0eded051e0cd76af34d3bac
Michael Naber <mnaber@skylineinnovations.com>
2014-08-12 13:37:02 -0400

    Fixed schema migration to properly set the fb columns on the customer table

--- upgrade_scripts/__init__.py
+++ upgrade_scripts/__init__.py
@@ -26,28 +26,3 @@ def alembic_upgrade(revision_to):
     config = Config("alembic.ini")
     upgrade(config, revision_to)
 
-# def alembic_upgrade(connection, revision, sql=False, tag=None):
-#     """Upgrade to a later version."""
-#     config = Config("alembic.ini")
-#     script = ScriptDirectory.from_config(config)
-#
-#     starting_rev = None
-#     if ":" in revision:
-#         if not sql:
-#             raise CommandError("Range revision not allowed")
-#         starting_rev, revision = revision.split(':', 2)
-#
-#     def upgrade(rev, context):
-#         return script._upgrade_revs(revision, rev)
-#
-#     with EnvironmentContext(
-#         config,
-#         script,
-#         fn=upgrade,
-#         as_sql=sql,
-#         starting_rev=starting_rev,
-#         destination_rev=revision,
-#         tag=tag
-#     ) as ec:
-#         ec.configure(connection=connection)
-#         script.run_env()
