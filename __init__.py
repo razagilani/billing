7c4d4659fe880209359664729929d3d8f2c839e0
? <dklothe@skylineinnovations.com>
2014-08-15 14:35:30 -0400

    merge default to release22

--- __init__.py
+++ __init__.py
@@ -18,13 +18,14 @@ def init_config(filename='settings.cfg', fp=None):
     """
     from billing.data.validation import configuration as vns
     from billing.lib.config import ValidatedConfigParser
+    from ConfigParser import ConfigParser
     from os.path import dirname, realpath
     import logging
 
     log = logging.getLogger(__name__)
     
     global config
-    config = ValidatedConfigParser(vns)
+    config = ConfigParser()
     if fp:
         log.debug('Reading configuration fp')
         config.readfp(fp)
