5e03effb60423e3d95612d22864eb2f8f4ff7f2d
Michael Naber <mnaber@skylineinnovations.com>
2014-08-15 13:26:35 -0400

    added a sha256_hexdigest column to the utilbill table; work on migration script

--- upgrade_scripts/v23/migrate_to_aws.py
+++ upgrade_scripts/v23/migrate_to_aws.py
@@ -6,6 +6,8 @@ import os
 import boto
 from boto.s3.connection import S3Connection
 from billing.processing.state import UtilBill
+import hashlib, logging
+from processing.billupload import BillUpload
 
 # connection = S3Connection('AKIAJVT5YEOCCNGKKE3Q',
 #                           'DFXNb7yeG3toqb9OFMs2wMwJ/76CVSyJz2ICl5Hc')
@@ -25,25 +27,39 @@ from billing.processing.state import UtilBill
 
 
 
-def upload_bill_to_aws(local_filename, type, bucket):
-    """
-    :param filename: the path to the utilbill or reebille
-    :param type: string either 'utilbill' or 'reebill'
-    :param bucket: a :class:`boto.s3.bucket.Bucket`
-    """
-    pass
+def get_hash(file_name):
+    with open(file_name) as f:
+        m = hashlib.sha256()
+        m.update(f.read())
+        res = m.hexdigest()
+    return res
 
+
+# def upload_utilbill_bill_to_aws(local_file_path, type, bucket):
+#     """
+#     :param local_file_path: the path to the utilbill or reebille
+#     :param type: string either 'utilbill' or 'reebill'
+#     :param bucket: a :class:`boto.s3.bucket.Bucket`
+#     """
+#     pass
+
+log = logging.getLogger(__name__)
 
 def upload_utilbills_to_aws(session):
 
-    connection = S3Connection(config.get('access_key_id'),
-                              config.get('access_key_secret'))
-    bucket = connection.get_bucket('reebill-dev')
+    # connection = S3Connection(config.get('aws_s3', 'access_key_id'),
+    #                           config.get('aws_s3', 'access_key_secret'))
+    # bucket = connection.get_bucket('reebill-dev')
+    # utilbill_path = config.get('billdb', 'utilitybillpath')
 
-
-    utilbill_path = config.get('billdb', 'utilitybillpath')
-
-    #Upload Utilbills
     #for utilbill in os.path.walk(utilbill_path)
+    #Upload Utilbills
+    bu = BillUpload(config, log)
     for utilbill in session.query(UtilBill).all():
-        print utilbill.id
+        try:
+            local_file_path = bu.get_utilbill_file_path(utilbill)
+        except IOError:
+            continue
+        #upload_bill_to_aws(local_file_path)
+
+
