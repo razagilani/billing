547c329550f2249273c27414d65fab986f113058
? <dklothe@skylineinnovations.com>
2014-09-04 18:54:09 -0400

    fixed error in command-line help text

---
+++ reebill/ui/static/app/view/reports/ExportReebill.js
@@ -0,0 +1,30 @@
+Ext.define('ReeBill.view.Exports', {
+    extend: 'Ext.panel.Panel',
+
+    title:'Exports',
+    header: false,
+    alias: 'widget.exports',
+
+    
+    dockedItems: [{
+        dock: 'top',
+        xtype: 'toolbar',
+        items: [{
+            xtype: 'button',
+            text: 'Export ReeBill XLS',
+            iconCls: 'silk-application-go',
+            action: 'exportRebill'
+        },{
+            xtype: 'button',
+            text: 'Export Utility Bills to XLS',
+            iconCls: 'silk-application-go',
+            action: 'exportUtilityBills'
+        },{
+            xtype: 'button',
+            text: '12 Month Estimated Revenue',
+            iconCls: 'silk-application-go',
+            action: 'export12MonthRevenue'
+        }]
+    }],
+
+});
