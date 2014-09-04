f182ebf236166959d22498c2adca1e8a1c4dc1cb
Michael Naber <mnaber@skylineinnovations.com>
2014-09-04 16:47:17 -0400

    getting rid of rate_formula

---
+++ reebill/ui/static/app/view/metersandregisters/UtilityBillRegisters.js
@@ -0,0 +1,117 @@
+Ext.define('ReeBill.view.UtilityBillRegisters', {
+    extend: 'Ext.grid.Panel',
+
+    title: 'Utility Bill Registers',
+    alias: 'widget.utilityBillRegisters',    
+    store: 'UtilityBillRegisters',
+    preventHeader: true,
+
+    plugins: [
+        Ext.create('Ext.grid.plugin.CellEditing', {
+            clicksToEdit: 2
+        })
+    ],
+
+    viewConfig: {
+        trackOver: false,
+        stripeRows: true,
+        getRowClass: function(record) {
+
+        }
+    },
+    
+    columns: [{
+        header: 'Service',
+        renderer: function(value){
+            console.log()
+            var selectedBill = ReeBill.getApplication().getController('UtilityBills')
+                .getUtilityBillsGrid().getSelectionModel().getSelection()[0];
+            return selectedBill.get('service');
+        },
+    },{
+        header: 'Meter ID',
+        dataIndex: 'meter_identifier',
+        sortable: false,
+        editor: {
+            xtype: 'textfield',
+        },
+        width: 150,
+    },{
+        header: 'Register ID',
+        dataIndex: 'identifier',
+        sortable: false,
+        editor: {
+            xtype: 'textfield',
+        },
+        width: 150,
+    },{
+        header: 'Type',
+        dataIndex: 'reg_type',
+        sortable: false,
+        editor: {
+            xtype: 'textfield',
+            allowBlank: false
+        },
+        width: 100,
+    },{
+        header: 'RSI Binding',
+        dataIndex: 'register_binding',
+        sortable: false,
+        editor: {
+            xtype: 'textfield',
+            allowBlank: false
+        },
+        width: 200,
+    },{
+        header: 'Quantity',
+        dataIndex: 'quantity',
+        sortable: false,
+        editor: {
+            xtype: 'textfield',
+            allowBlank: false
+        },
+        width: 150,
+    },{
+        header: 'Units',
+        dataIndex: 'quantity_units',
+        width: 100,
+        sortable: true,
+        editor: {
+            xtype: 'combo',
+            name: 'service',
+            store: 'Units',
+            triggerAction: 'all',
+            valueField: 'value',
+            displayField: 'name',
+            queryMode: 'local',
+            forceSelection: true,
+            selectOnFocus: true
+        }  
+    },{
+        header: 'Description',
+        dataIndex: 'description',
+        sortable: false,
+        editor: {
+            xtype: 'textfield',
+            allowBlank: false
+        },
+        flex: 1
+    }],
+
+    dockedItems: [{
+        dock: 'top',
+        xtype: 'toolbar',
+        items: [{
+            xtype: 'button',
+            text: 'New',
+            action: 'newUtilityBillRegister',
+            iconCls: 'silk-add'
+        },{        
+            xtype: 'button',
+            text: 'Remove',
+            action: 'removeUtilityBillRegister',
+            iconCls: 'silk-delete',
+            disabled: true
+        }]
+    }]
+});
