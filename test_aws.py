2cb79a47a3e88c225bdf69ce82b491dfdae1c0b4
Michael Naber <mnaber@skylineinnovations.com>
2014-09-09 13:53:16 -0400

    merge with default

--- test_aws.py
+++ test_aws.py
@@ -1,36 +1,14 @@
 import os
-import boto
 from boto.s3.connection import S3Connection, OrdinaryCallingFormat, ProtocolIndependentOrdinaryCallingFormat
-from boto.s3.key import Key
-
-# connection = S3Connection('AKIAJVT5YEOCCNGKKE3Q',
-#                           'DFXNb7yeG3toqb9OFMs2wMwJ/76CVSyJz2ICl5Hc')
-
-#print 'calling'
-# connection = S3Connection('AKIAJVT5YEOCCNGKKE3Q',
-#                           'DFXNb7yeG3toqb9OFMs2wMwJ/76CVSyJz2ICl5Hc',
-#                           host='127.0.0.1',
-#                           port=4567)
-
-#print 'done'
-#b0 = connection.get_bucket('reebill-dev')
 
 print 'connecting to s3'
-# connection = S3Connection('AKIAIOSFODNN7EXAMPLE',
-#                           'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
-#                           host='localhost', port=4567)
-
-# connection = S3Connection('',
-#                           '',
-#                           host='localhost', port=4567,
-#                           is_secure=False,
-#                           calling_format=OrdinaryCallingFormat())
 
 connection = S3Connection('',
                           '',
-                          host='localhost', port=9444,
+                          host='localhost', port=4567,
                           is_secure=False,
-                          calling_format=ProtocolIndependentOrdinaryCallingFormat())
+                          calling_format=OrdinaryCallingFormat())
+
 
 print 'connected'
 print 'making bucket'
@@ -50,5 +28,7 @@ full_key_name = os.path.join(bucket_directory, keyname)
 k = bucket.new_key(full_key_name)
 #k = Key(bucket)
 #k.set_contents_from_filename(file_to_upload)
-k.set_contents_from_string('whee')
-print 'all done'
+#k.set_contents_from_string('whee')
+#print 'all done'
+
+print k.get_contents_as_string()
