1b7e416acb5097cd48f8edb66d2c6169178fa3b2
? <dklothe@skylineinnovations.com>
2014-09-11 19:08:06 -0400

    merge default to new-bill-format

--- test/test_utilbill.py
+++ test/test_utilbill.py
@@ -16,7 +16,6 @@ class UtilBillTest(TestCase):
         and 'error_message' in its 'error' field.
         '''
         self.assertIsNone(c.quantity)
-        self.assertIsNone(c.rate)
         self.assertIsNone(c.total)
         self.assertIsNotNone(c.error)
         self.assertEqual(error_message, c.error)
@@ -45,37 +44,37 @@ class UtilBillTest(TestCase):
                 rsi_binding='CONSTANT',
                 quantity='100',
                 quantity_units='dollars',
-                rate='0.4',
+                rate=0.4,
             ),
             dict(
                 rsi_binding='LINEAR',
                 quantity='REG_TOTAL.quantity * 3',
                 quantity_units='therms',
-                rate='0.1',
+                rate=0.1,
             ),
             dict(
                 rsi_binding='LINEAR_PLUS_CONSTANT',
                 quantity='REG_TOTAL.quantity * 2 + 10',
                 quantity_units='therms',
-                rate='0.1',
+                rate=0.1,
             ),
             dict(
                 rsi_binding='BLOCK_1',
                 quantity='min(100, REG_TOTAL.quantity)',
                 quantity_units='therms',
-                rate='0.3',
+                rate=0.3,
             ),
             dict(
                 rsi_binding='BLOCK_2',
                 quantity='min(200, max(0, REG_TOTAL.quantity - 100))',
                 quantity_units='therms',
-                rate='0.2',
+                rate=0.2,
             ),
             dict(
                 rsi_binding='BLOCK_3',
                 quantity='max(0, REG_TOTAL.quantity - 200)',
                 quantity_units='therms',
-                rate='0.1',
+                rate=0.1,
             ),
             dict(
                 rsi_binding='REFERENCES_ANOTHER',
@@ -83,13 +82,13 @@ class UtilBillTest(TestCase):
                 quantity='REFERENCED_BY_ANOTHER.quantity + '
                          'REFERENCED_BY_ANOTHER.rate',
                 quantity_units='therms',
-                rate='1',
+                rate=1,
             ),
             dict(
                 rsi_binding='NO_CHARGE_FOR_THIS_RSI',
                 quantity='1',
                 quantity_units='therms',
-                rate='1',
+                rate=1,
             ),
             # this RSI has no charge associated with it, but is used to
             # provide identifiers in the formula of the "REFERENCES_ANOTHER"
@@ -98,19 +97,19 @@ class UtilBillTest(TestCase):
                 rsi_binding='REFERENCED_BY_ANOTHER',
                 quantity='2',
                 quantity_units='therms',
-                rate='3',
+                rate=3,
             ),
             dict(
                 rsi_binding='SYNTAX_ERROR',
                 quantity='5 + ',
                 quantity_units='therms',
-                rate='1',
+                rate=1,
             ),
             dict(
                 rsi_binding='DIV_BY_ZERO_ERROR',
-                quantity='1',
+                quantity='1 / 0',
                 quantity_units='therms',
-                rate='1 / 0',
+                rate=1,
             ),
             # shows that quantity formula error takes priority over rate
             # formula error
@@ -118,13 +117,12 @@ class UtilBillTest(TestCase):
                 rsi_binding='UNKNOWN_IDENTIFIER',
                 quantity='x * 2',
                 quantity_units='therms',
-                rate='1 / 0',
+                rate=1,
             ),
         ]
         utilbill.charges = [Charge(utilbill, "Insert description here", "",
-                0.0, c['quantity_units'], 0.0, c['rsi_binding'], 0.0,
-                rate_formula=c['rate'], quantity_formula=c['quantity'])
-                for c in charges]
+                0.0, c['quantity_units'], c['rate'], c['rsi_binding'], 0.0,
+                quantity_formula=c['quantity']) for c in charges]
 
         get = utilbill.get_charge_by_rsi_binding
 
@@ -169,6 +167,7 @@ class UtilBillTest(TestCase):
         self.assert_charge_values(5, 1, get('REFERENCES_ANOTHER'))
         self.assert_error(get('SYNTAX_ERROR'), 'Syntax error')
         self.assert_error(get('DIV_BY_ZERO_ERROR'), 'Error: division by zero')
+
         self.assert_error(get('UNKNOWN_IDENTIFIER'),
                 "Error: name 'x' is not defined")
 
@@ -227,16 +226,13 @@ class UtilBillTest(TestCase):
         utilbill.registers = [Register(utilbill, '', 150,
                 'kWh', '', False, "total", "REG_TOTAL", '', '')]
         utilbill.charges = [
-            Charge(utilbill, '', '', 0, 'kWh', 0, 'A', 0,
-                    quantity_formula='REG_TOTAL.quantity',
-                    rate_formula='1'),
-            Charge(utilbill, '', '', 0, 'kWh', 0, 'B', 0,
-                    quantity_formula='2',
-                    rate_formula='3'),
+            Charge(utilbill, '', '', 0, 'kWh', 1, 'A', 0,
+                    quantity_formula='REG_TOTAL.quantity'),
+            Charge(utilbill, '', '', 0, 'kWh', 3, 'B', 0,
+                    quantity_formula='2'),
             # this has an error
             Charge(utilbill, '', '', 0, 'kWh', 0, 'C', 0,
-                    quantity_formula='1/0',
-                    rate_formula='x + y'),
+                    quantity_formula='1/0'),
         ]
         Session().add(utilbill)
         utilbill.compute_charges()
@@ -264,23 +260,18 @@ class UtilBillTest(TestCase):
             # and B depends on A's "rate", which is not allowed even though
             # theoretically both could be computed.
             Charge(utilbill, '', '', 0, 'kWh', 0, 'A', 0,
-                    quantity_formula='B.quantity',
-                    rate_formula='0'),
+                    quantity_formula='B.quantity'),
             Charge(utilbill, '', '', 0, 'kWh', 0, 'B', 0,
-                    quantity_formula='0',
-                    rate_formula='A.rate'),
+                    quantity_formula='A.rate'),
             # C depends on itself
             Charge(utilbill, '', '', 0, 'kWh', 0, 'C', 0,
-                    quantity_formula='0',
-                    rate_formula='C.total'),
+                    quantity_formula='C.total'),
             # D depends on A, which has a circular dependency with B. it should
             # not be computable because A is not computable.
             Charge(utilbill, '', '', 0, 'kWh', 0, 'D', 0,
-                    quantity_formula='A.total',
-                    rate_formula='0'),
-            Charge(utilbill, '', '', 0, 'kWh', 0, 'E', 0,
-                    quantity_formula='2',
-                    rate_formula='3'),
+                    quantity_formula='A.total'),
+            Charge(utilbill, '', '', 0, 'kWh', 3, 'E', 0,
+                    quantity_formula='2'),
         ]
         Session().add(utilbill)
         utilbill.compute_charges()
