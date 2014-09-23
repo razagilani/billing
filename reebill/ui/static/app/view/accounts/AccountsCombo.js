2a0dbb669fdbef24ba60c7a809bee4f9865439c5
Rich Andrews <stuart.r.andrews@gmail.com>
2014-09-23 19:03:10 -0400

    79406428 section header alembic prevents the application from loading. Removed per MN.

---
+++ reebill/ui/static/app/view/accounts/AccountsCombo.js
@@ -0,0 +1,30 @@
+Ext.define('ReeBill.view.AccountsCombo', {
+    extend: 'Ext.form.field.ComboBox',
+    alias: 'widget.accountsCombo',
+    
+    store: 'Accounts',
+
+    fieldLabel: 'Based on',
+    tpl: Ext.create(
+        'Ext.XTemplate',
+        '<tpl for=".">',
+        '<div class="x-boundlist-item" >',
+        '{account} - {codename} - {casualname} - {primusname} - {lastrateclass}',
+        '</div>',
+        '</tpl>'
+    ),
+    displayTpl: Ext.create(
+        'Ext.XTemplate',
+        '<tpl for=".">',
+        '{account} - {codename} - {casualname} - {primusname} - {lastrateclass}',
+        '</tpl>'
+    ),
+
+    valueField: 'account',
+    queryMode: 'local',
+    typeAhead: true,
+    triggerAction: 'all',
+    emptyText:'Select...',
+    selectOnFocus:true,
+    allowBlank: false
+});
