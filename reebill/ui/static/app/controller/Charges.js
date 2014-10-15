a84f8dd31aef0940e86da3b9b3ec0938ee22048b
Thomas Hoffmann <thoffmann@skylineinnovations.com>
2014-10-15 11:26:56 -0400

    80651130 Fixed

--- reebill/ui/static/app/controller/Charges.js
+++ reebill/ui/static/app/controller/Charges.js
@@ -192,8 +192,17 @@ Ext.define('ReeBill.controller.Charges', {
      * Handle the regenerate button being clicked.
      */
     handleRegenerate: function() {
+        var grid = this.getChargesGrid();
+        grid.setLoading(true);
+        var store = this.getUtilityBillsStore();
+        store.suspendAutoSync();
         var selectedBill = this.getUtilityBillsGrid().getSelectionModel().getSelection()[0];
         selectedBill.set('action', 'regenerate_charges');
+        store.sync({success:function(batch){
+            this.getChargesStore().reload();
+            grid.setLoading(false);
+        }, scope: this});
+        store.resumeAutoSync();
     },
 
     /**
