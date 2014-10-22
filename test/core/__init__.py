fa01d5d38c02bfe161cf2c5b3bd3061c79bb4ba3
Raza Gilani <rgilani@skylineinnovations.com>
2014-10-22 18:21:11 -0400

    BILL-5699 refactored issue_process_and_mail

---
+++ test/core/__init__.py
@@ -0,0 +1,11 @@
+
+from .test_billupload import BillUploadTest
+from .test_charge import ChargeUnitTests
+from .test_rate_structure import RateStructureDAOTest
+from .test_utilbill import UtilBillTest
+from .test_utilbill_loader import UtilbillLoaderTest
+from .test_utilbill_processing import UtilbillProcessingTest
+
+__all__ = ['BillUploadTest', 'ChargeUnitTests', 'RateStructureDAOTest',
+           'UtilBillTest', 'UtilbillLoaderTest', 'UtilbillProcessingTest']
+
