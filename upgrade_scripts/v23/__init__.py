b3c90cd690a41b15e8b01078b4f31c35d1efd131
? <dklothe@skylineinnovations.com>
2014-10-29 13:44:28 -0400

    merge (all tests finally passing)

--- upgrade_scripts/v23/__init__.py
+++ upgrade_scripts/v23/__init__.py
@@ -8,7 +8,7 @@ import any other code that that expects an initialized data model without first
 calling :func:`.billing.init_model`.
 """
 from boto.s3.connection import S3Connection
-from sqlalchemy import func
+from sqlalchemy import func, distinct
 from sqlalchemy.sql.expression import select
 from sqlalchemy.sql.schema import MetaData, Table
 from upgrade_scripts import alembic_upgrade
@@ -16,7 +16,7 @@ import logging
 from pymongo import MongoClient
 from billing import config, init_model
 from billing.core.model.model import Session, Company, Customer, Utility, \
-    Address, UtilBill, Supplier
+    Address, UtilBill, Supplier, RateClass
 from billing.upgrade_scripts.v23.migrate_to_aws import upload_utilbills_to_aws
 
 log = logging.getLogger(__name__)
@@ -47,10 +47,11 @@ def read_initial_table_data(table_name, session):
     return {row['id']: row for row in result}
 
 def create_utilities(session):
-    for utility_name in utility_names:
+    bill_utilities = session.execute("select distinct utility from utilbill");
+    for bill_utility in bill_utilities:
         empty_address = Address('', '', '', '', '')
         empty_guid = ''
-        utility_company = Utility(utility_name, empty_address, empty_guid)
+        utility_company = Utility(bill_utility['utility'], empty_address, empty_guid)
         session.add(utility_company)
     session.flush()
     session.commit()
@@ -71,13 +72,15 @@ def migrate_customer_fb_utility(customer_data, session):
 def migrate_utilbill_utility(utilbill_data, session):
     company_map = {c.name.lower(): c for c in session.query(Company).all()}
     for utility_bill in session.query(UtilBill).all():
-        utility_name = utilbill_data[utility_bill.id]['utility'].lower() \
-        if utilbill_data[utility_bill.id]['utility'].lower()!='washgas' \
-            else 'Washington Gas'.lower()
+        utility_name = utilbill_data[utility_bill.id]['utility'].lower()
+        '''if utilbill_data[utility_bill.id]['utility'].lower()!='washgas' \
+            else 'Washington Gas'.lower()'''
         log.debug('Setting utility to %s for utilbill id %s' %
                   (utility_name, utility_bill.id))
         try:
             utility_bill.utility = company_map[utility_name]
+            if utility_bill.utility.name == 'washgas':
+                utility_bill.utility.name = 'washington gas'
         except KeyError:
             log.error("Could not locate company with name '%s' for utilbill %s"
                       % (utility_name, utility_bill.id))
@@ -92,6 +95,7 @@ def set_fb_utility_id(session):
             log.debug('Setting fb_utility_id to %s for customer id %s' %
                   (first_bill.utility_id, customer.id))
             customer.fb_utility_id = first_bill.utility_id
+    session.commit()
 
 def set_supplier_ids(session):
     for company in session.query(Company).all():
@@ -119,6 +123,34 @@ def set_supplier_ids(session):
             log.debug('Setting supplier_id to %s for utility bill id %s' %
                   (supplier, bill.id))
             bill.supplier_id = supplier
+
+def create_rate_classes(session):
+    utilbills = session.execute("select distinct rate_class, utility_id from utilbill")
+
+    for bill in utilbills:
+        log.debug('Creating RateClass object with name %s and utility_id %s'
+                  %(bill['rate_class'], bill['utility_id']))
+        utility = session.query(Utility).filter(Utility.id==bill['utility_id']).one()
+        rate_class = RateClass(bill['rate_class'], utility)
+        session.add(rate_class)
+    session.flush()
+
+def set_rate_class_ids(session):
+    utilbills = session.query(UtilBill).all()
+    for bill in utilbills:
+        u_rate_class = session.query(RateClass).filter(RateClass.utility_id==bill.utility_id).first()
+        log.debug('setting rate_class_id to %s for utilbill with id %s'
+                  %(u_rate_class.id, bill.id))
+        bill.rate_class_id = u_rate_class.id
+    customers = session.query(Customer).all()
+    for customer in customers:
+        c_rate_class = session.query(RateClass).filter(RateClass.utility_id==customer.fb_utility_id).first()
+        if c_rate_class is None:
+            customer.fb_rate_class_id = Session.query(RateClass).first().id
+        else:
+            log.debug('setting rate_class_id to %s for customer with id %s'
+                  %(c_rate_class.id, customer.id))
+            customer.fb_rate_class_id = c_rate_class.id
 
 def upgrade():
 
@@ -155,8 +187,24 @@ def upgrade():
     log.info('Committing to database')
     session.commit()
 
+    log.info('Upgrading schema to revision 18a02dea5969')
+    alembic_upgrade('18a02dea5969')
+
     log.info('Upgrading schema to revision 3566e62e7af3')
     alembic_upgrade('3566e62e7af3')
+
+
+    log.info('creating rate_classes')
+    create_rate_classes(session)
+
+    log.info('setting up rate_class ids for Customer an UtilBill records')
+    set_rate_class_ids(session)
+
+    log.info('Comitting to Database')
+    session.commit()
+
+    log.info('Upgrading to schema 4bc721447593')
+    alembic_upgrade('4bc721447593')
 
     log.info('Upgrade Complete')
 
