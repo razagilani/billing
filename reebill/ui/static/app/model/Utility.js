cd14be9883c80b8b03fff2c80139e3adb2fc8a0e
Raza Gilani <rgilani@skylineinnovations.com>
2014-11-03 12:14:58 -0500

    BILL-3820 added rate class with upgrade_scripts and ui

--- reebill/ui/static/app/model/Utility.js
+++ reebill/ui/static/app/model/Utility.js
@@ -2,6 +2,15 @@ Ext.define('ReeBill.model.Utility', {
     extend: 'Ext.data.Model',
     fields: [
         {name: 'name', type: 'string'},
-        {name: 'id', type: 'int'}
-    ]
+        {name: 'id', type: 'int'},
+        {name: 'address_id', type: 'int'},
+        {name: 'guid', type: 'string'},
+        {name: 'discriminator', type: 'string'}
+    ],
+    hasMany: {
+            model: 'RateClass',
+            name: 'rateclasses',
+            foreignKey: 'utility_id',
+            accosiationKey: 'rateclasses'
+        }
 });
