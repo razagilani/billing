8d74e34b86bc721149f8dc5f1fc6a5398168a21a
Raza Gilani <rgilani@skylineinnovations.com>
2014-11-03 18:29:07 -0500

    BILL-5731 redirected fakes3 stderr to stdout via arguments to popen

---
+++ conf/manifests/reebill-stage.pp
@@ -0,0 +1,47 @@
+# app level
+$username = "reebill-stage"
+$app = "billing"
+$env = "stage"
+
+host::app_user {'appuser':
+    app        => $app,
+    env        => $env,
+    username   => $username,
+}
+
+host::aws_standard_packages {'std_packages':}
+host::wsgi_setup {'wsgi':}
+
+package { 'httpd':
+    ensure  => installed
+}
+package { 'html2ps':
+    ensure  => installed
+}
+package { 'imageMagick':
+    ensure  => installed
+}
+package { 'poppler-utils':
+    ensure  => installed
+}
+package { 'libevent-dev':
+    ensure  => installed
+}
+file { "/var/local/${username}/www":
+    ensure      => directory,
+    owner       => $username,
+    group       => $username,
+    require => Host::App_user['appuser']
+}
+file { "/db-${env}":
+    ensure      => directory,
+    owner       => $username,
+    group       => $username,
+}
+file { "/etc/httpd/conf.d/${username}.conf":
+    ensure => file,
+    source => "puppet:///modules/conf/vhosts/reebill-stage.conf"
+}
+
+# apache vhost setup
+# full crontab?
---
+++ reebill/ui/static/app/store/Utilities.js
@@ -0,0 +1,36 @@
+Ext.define('ReeBill.store.Utilities', {
+    extend: 'Ext.data.Store',
+    model: 'ReeBill.model.Utility',
+
+    autoLoad: true,
+    autoSync: true,
+
+	proxy: {
+		type: 'rest',
+
+        simpleSortMode: true,		
+        pageParam: false,
+        startParam: false,
+        sortParam: false,
+        limitParam: false,
+
+        url: 'http://' + window.location.host + '/reebill/utilities',
+
+        reader: {
+            type: 'json',
+            root: 'rows',
+            totalProperty: 'results'
+        },
+
+        listeners:{
+            exception: utils.makeProxyExceptionHandler('Utilities'),
+            scope: this
+        }
+	},
+
+    sorters: [{
+        property: 'name',
+        direction: 'DESC'
+    }]
+
+});
