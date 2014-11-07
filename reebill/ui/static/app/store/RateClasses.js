a5435151be037daefd448bcf52d728b8275f4468
Raza Gilani <rgilani@skylineinnovations.com>
2014-11-07 16:59:27 -0500

    removed a blank line

---
+++ reebill/ui/static/app/store/RateClasses.js
@@ -0,0 +1,36 @@
+Ext.define('ReeBill.store.RateClasses', {
+    extend: 'Ext.data.Store',
+    model: 'ReeBill.model.RateClass',
+
+    autoLoad: true,
+    autoSync: true,
+
+	proxy: {
+		type: 'rest',
+
+        simpleSortMode: true,		
+        pageParam: false,
+        startParam: false,
+        sortParam: false,
+        limitParam: false,
+
+        url: 'http://' + window.location.host + '/reebill/rateclasses',
+
+        reader: {
+            type: 'json',
+            root: 'rows',
+            totalProperty: 'results'
+        },
+
+        listeners:{
+            exception: utils.makeProxyExceptionHandler('RateClasses'),
+            scope: this
+        }
+	},
+
+    sorters: [{
+        property: 'name',
+        direction: 'DESC'
+    }]
+
+});
