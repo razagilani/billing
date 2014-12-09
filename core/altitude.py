4ea68e8679379bb8a29bb6aec13990b311f8c61f
? <dklothe@skylineinnovations.com>
2014-12-08 19:37:14 -0500

    BILL-5813, BILL-5841: store "acount_guids" from acquisitor's utilbill messages

--- core/altitude.py
+++ core/altitude.py
@@ -14,6 +14,7 @@ all almost identical.
 '''
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm.exc import NoResultFound
 from billing.core.model import Base, Session, Utility, UtilityAccount
 
 
@@ -58,7 +59,7 @@ class AltitudeAccount(Base):
     __tablename__ = 'altitude_account'
 
     id = Column('id', Integer(), primary_key=True, nullable=False)
-    utility_id = Column('utility_account_id', Integer(),
+    utility_account_id = Column('utility_account_id', Integer(),
                         ForeignKey('utility_account.id'), nullable=False)
     guid = Column('guid', AltitudeGUID, nullable=False)
     utility_account = relationship('UtilityAccount')
@@ -72,3 +73,27 @@ def _altitude_to_billing(altitude_class, billing_class):
     return lambda guid: Session().query(billing_class).join(
         altitude_class).filter(altitude_class.guid==guid).one()
 get_utility_from_guid = _altitude_to_billing(AltitudeUtility, Utility)
+get_account_from_guid = _altitude_to_billing(AltitudeAccount, UtilityAccount)
+
+def update_altitude_account_guids(utility_account, guids):
+    '''For each GUID (string) in 'guids', either associate the AltitudeAccount
+    identified by the GUID with the given UtilityAccount, or create a new
+    AltitudeAccount associated with it.
+    '''
+    s = Session()
+    for guid in guids:
+        try:
+            altitude_account = s.query(AltitudeAccount).filter_by(
+                guid=guid).one()
+        except NoResultFound:
+            # if this AltitudeAccount does not exist, create it and associate
+            # it with 'utility_account
+            s.add(AltitudeAccount(utility_account, guid))
+        else:
+            # if this GUID does exist, and is associated with another
+            # utility account, change it.
+            # TODO: find out if this is the desired behavior. alternatives
+            # could include: raise an exception if it's not the same utility
+            # account as before, because it shouldn't change, or ignore the
+            # new value and keep the old one
+            altitude_account.utility_account = utility_account
