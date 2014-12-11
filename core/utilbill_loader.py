a0131844672117d6aafd5907b64c1c7e750a4088
Thomas Hoffmann <thoffmann@skylineinnovations.com>
2014-12-11 12:02:46 -0500

    BILL-5804 added s3 urlformat and ran optimize imports

--- core/utilbill_loader.py
+++ core/utilbill_loader.py
@@ -1,7 +1,5 @@
-from sqlalchemy import desc
 from billing.core.model import UtilBill, UtilityAccount
 from billing.exc import NoSuchBillException
-from reebill.utilbill_processor import U
 
 class UtilBillLoader(object):
     '''Data access object for utility bills, used to hide database details
