f838bbfe79050fc0c52ac4982e9c9c532ef7c565
? <dklothe@skylineinnovations.com>
2014-12-19 17:03:19 -0500

    move initialization of objects to process utilbill messages into a function so it can be used by both tests and production, + fix test failure where queue may not be purged even when "queue_purge" is called

--- run_amqp_consumers.py
+++ run_amqp_consumers.py
@@ -1,57 +1,18 @@
 #!/usr/bin/env/python
 '''Entry point for AMQP consumers: run this file to start consuming messages.
-All config file reading and instantiation of objects based on config file
-values should be done here. The substantive code that actually does things is
-in core/amqp_exchange.py.
+This file should be kept as short as possible; all substantive code should go
+in amqp_exchange.py (and should have test coverage).
 '''
-from boto.s3.connection import S3Connection
-import pika
-
 from billing import init_config, init_model, init_logging
-from billing.reebill.utilbill_processor import UtilbillProcessor
-from billing.core.utilbill_loader import UtilBillLoader
-from billing.core.pricing import FuzzyPricingModel
-from billing.core.amqp_exchange import consume_utilbill_file
-from billing.core.model import Session
-from billing.core.bill_file_handler import BillFileHandler
-
+from billing.core.amqp_exchange import create_dependencies, \
+    consume_utility_guid, consume_utilbill_file
 
 if __name__ == '__main__':
     init_config()
     init_model()
     init_logging()
-    from billing import config
 
-    host_name = config.get('amqp', 'host')
-    exchange_name = config.get('amqp', 'exchange')
-    queue_name = config.get('amqp', 'utilbill_queue')
-    bucket_name = config.get('aws_s3', 'bucket')
-
-    s3_connection = S3Connection(config.get('aws_s3', 'aws_access_key_id'),
-                                 config.get('aws_s3', 'aws_secret_access_key'),
-                                 is_secure=config.get('aws_s3', 'is_secure'),
-                                 port=config.get('aws_s3', 'port'),
-                                 host=config.get('aws_s3', 'host'),
-                                 calling_format=config.get('aws_s3',
-                                                           'calling_format'))
-    utilbill_loader = UtilBillLoader(Session())
-    url_format = 'http://%s:%s/%%(bucket_name)s/%%(key_name)s' % (
-        config.get('aws_s3', 'host'), config.get('aws_s3', 'port'))
-    bill_file_handler = BillFileHandler(s3_connection,
-                                      config.get('aws_s3', 'bucket'),
-                                      utilbill_loader, url_format)
-    rabbitmq_connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host_name))
-    channel = rabbitmq_connection.channel()
-    channel.exchange_declare(exchange=exchange_name)
-    channel.queue_declare(queue=queue_name)
-    channel.queue_bind(exchange=exchange_name, queue=queue_name)
-
-    s = Session()
-    utilbill_loader = UtilBillLoader(s)
-    pricing_model = FuzzyPricingModel(utilbill_loader)
-    utilbill_processor = UtilbillProcessor(pricing_model, bill_file_handler,
-                                           None)
+    _, channel, _, queue_name, utilbill_processor = create_dependencies()
 
     consume_utilbill_file(channel, queue_name, utilbill_processor)
 
