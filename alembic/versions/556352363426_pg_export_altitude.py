f5575992b9e61e1f5a220dd4f26c5b984b22ddb1
? <dklothe@skylineinnovations.com>
2015-01-19 13:34:08 -0500

    merge with default + fix error in upgrade_scripts/v24/__init__.py

--- alembic/versions/556352363426_pg_export_altitude.py
+++ alembic/versions/556352363426_pg_export_altitude.py
@@ -9,7 +9,7 @@ All data model changes related to exporting a CSV file of PG-related data for Al
 
 # revision identifiers, used by Alembic.
 revision = '556352363426'
-down_revision = '5a6d7e4f8b80'
+down_revision = '3cf530e68eb'
 
 from alembic import op
 import sqlalchemy as sa
@@ -20,7 +20,6 @@ def upgrade():
     op.create_table('pg_account',
         sa.Column('utility_account_id', sa.Integer(), primary_key=True),
         sa.ForeignKeyConstraint(['utility_account_id'], ['utility_account.id'], ),
-        sa.PrimaryKeyConstraint('id')
     )
     op.add_column(u'utilbill', sa.Column('date_modified', sa.DateTime(), nullable=True))
     op.add_column(u'charge',
