fb6d588311c421fac3356fcb3c2cacfdf3cbb1a4
? <dklothe@skylineinnovations.com>
2015-01-20 14:02:53 -0500

    change charge grid and model to have required columns (id, utilbill_id, rsi_binding, target_total)

--- pg/ui/static/app/model/Charge.js
+++ pg/ui/static/app/model/Charge.js
@@ -1,18 +1,9 @@
 Ext.define('ReeBill.model.Charge', {
     extend: 'Ext.data.Model',
     fields: [
-        {name: 'group', type: 'string'},
+        {name: 'id', type: 'int'},
+        {name: 'utilbill_id', type: 'int'},
         {name: 'rsi_binding', type: 'string'},
-        {name: 'description', type: 'string'},
-        {name: 'quantity', type: 'float'},
-        {name: 'unit', type: 'string'},
-        {name: 'rate', type: 'float'},
-        {name: 'total', type: 'float'},
-        {name: 'error', type: 'string', useNull: true},
-        {name: 'has_charge', type: 'boolean'},
-        {name: 'quantity_formula', type: 'string'},
-        {name: 'roundrule', type: 'string'},
-        {name: 'shared', type: 'boolean'},
-        {name: 'utilbill_id', type: 'int'}
+        {name: 'target_total', type: 'float'},
     ]
 });
