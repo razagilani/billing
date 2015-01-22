1c2492e362a0ffb7b32ee33cfd64e2e3ab11dd39
Raza Gilani <rgilani@nextility.com>
2015-01-22 18:36:36 -0500

    Merged in dklothe/billing/utilbill-estimated-meter-read-date (pull request #73)
    UtilBill.get_next_estimated_meter_read_date

---
+++ reebill/ui/static/app/config.js
@@ -0,0 +1,10 @@
+// Add static config items here. 
+var config = {
+	
+};
+
+var NO_UTILBILL_SELECTED_MESSAGE = 'images/select_utilbill.png';
+var NO_UTILBILL_FOUND_MESSAGE = 'images/select_utilbill_notfound.png';
+var NO_REEBILL_SELECTED_MESSAGE = 'images/select_reebill.png';
+var NO_REEBILL_FOUND_MESSAGE = 'images/select_reebill_notfound.png';
+var LOADING_MESSAGE = 'images/rotologo_white.gif';
