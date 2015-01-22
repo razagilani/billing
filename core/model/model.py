b7ee4c892284d54b2189448ce225eef763fead73
? <dklothe@skylineinnovations.com>
2015-01-22 18:36:38 -0500

    merge with default--fixes test failures

--- core/model/model.py
+++ core/model/model.py
@@ -3,7 +3,7 @@ SQLALchemy classes for all applications that use the utility bill database.
 Also contains some related classes that do not correspond to database tables.
 '''
 import ast
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 
 import sqlalchemy
@@ -156,7 +156,8 @@ class Address(Base):
                 self.city, self.state, self.postal_code)
 
     def __str__(self):
-        return '%s, %s, %s' % (self.street, self.city, self.state)
+        return '%s, %s, %s %s' % (self.street, self.city, self.state,
+                               self.postal_code)
 
     def column_dict(self):
         raise NotImplementedError
@@ -457,6 +458,15 @@ class UtilBill(Base):
         '''
         return self.utility.name
 
+    def get_estimated_next_meter_read_date(self):
+        '''Return approximate date of next meter read (which is usually the
+        end date of the next utility bill after this one), or None if no
+        estimate can be made.
+        '''
+        if self.period_end is None:
+            return None
+        return self.period_end + timedelta(days=30)
+
     def get_rate_class_name(self):
         '''Return name of this bill's rate class or None if the rate class is
         None (unknown).
@@ -472,6 +482,9 @@ class UtilBill(Base):
         if self.supplier is None:
             return None
         return self.supplier.name
+
+    def get_utility_account_number(self):
+        return self.utility_account.account_number
 
     def get_nextility_account_number(self):
         '''Return the "nextility account number" (e.g.  "10001") not to be
@@ -609,6 +622,23 @@ class UtilBill(Base):
         """
         return sum(charge.total for charge in self.charges
                 if charge.total is not None)
+
+    def get_total_energy(self):
+        # NOTE: this may have been implemented already on another branch;
+        # remove duplicate when merged
+        try:
+            total_register = next(r for r in self.registers if
+                                  r.register_binding == 'REG_TOTAL')
+        except StopIteration:
+            return 0
+        return total_register.quantity
+
+    def set_total_energy(self, quantity):
+        total_register = next(r for r in self.registers if
+                              r.register_binding == 'REG_TOTAL')
+        # TODO: maybe create REG_TOTAL register if it doesn't exist.
+        # this is hard because the unit is not known.
+        total_register.quantity = quantity
 
     def get_supply_target_total(self):
         '''Return the sum of the 'target_total' of all supply
