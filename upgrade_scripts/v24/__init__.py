4a7d250e2622432f36486f8b7d982b57874939d3
Raza Gilani <rgilani@skylineinnovations.com>
2015-01-23 16:25:31 -0500

    BILL-5861 classified charges as either distribution or supply

--- upgrade_scripts/v24/__init__.py
+++ upgrade_scripts/v24/__init__.py
@@ -13,15 +13,41 @@ from upgrade_scripts import alembic_upgrade
 import logging
 from core import config, init_model
 from core.model.model import Session, Utility, \
-    Address, UtilBill, Supplier, RateClass, UtilityAccount
+    Address, UtilBill, Supplier, RateClass, UtilityAccount, Charge
 
 log = logging.getLogger(__name__)
 
 
 def create_pg_accounts(session):
     for ua in session.query(UtilityAccount).filter(
-                    UtilityAccount.account >='20000').all():
+                    UtilityAccount.account >= '20000').all():
         session.add(PGAccount(ua))
+
+
+def mark_charges_as_distribution_or_supply(session):
+    supply = ['supply', 'Supply', 'SUPPLY', 'generation', 'Generation',
+        'GENERATION', 'transmission', 'Transmission', 'TRANSMISSION']
+    distribution = ['Distribution', 'DISTRIBUTION', 'Delivery',
+        'DELIVERY', 'Customer Charge', 'STRIDE', 'EmPower', 'Surcharge']
+    supply_count = 0
+    distribution_count = 0
+    other_count = 0
+    for charge in session.query(Charge).all():
+        if any(s in charge.group or s in charge.description for s in supply):
+            charge.type = 'supply'
+            supply_count += 1
+        elif any(s in charge.group or s in charge.description for s in distribution):
+            charge.type = 'distribution'
+            distribution_count += 1
+        else:
+            # distribution charges in this case probably makes more sense
+            # as distribution charges have relatively more sub categories
+            charge.type = 'distribution'
+            other_count += 1
+    print 'Found %s supply charges' % supply_count
+    print 'Found %s distribution charges' % distribution_count
+    print 'Found %s other charges' % other_count
+
 
 def upgrade():
     log.info('Beginning upgrade to version 24')
@@ -31,6 +57,7 @@ def upgrade():
     init_model(schema_revision='556352363426')
     session = Session()
     create_pg_accounts(session)
+    mark_charges_as_distribution_or_supply(session)
 
     session.commit()
 
