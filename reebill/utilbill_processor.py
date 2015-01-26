befe9bbae1a45d8243a972486f5ba2f3e30b9a5c
Thomas Hoffmann <thoffmann@skylineinnovations.com>
2015-01-26 17:24:51 -0500

    code cleanup for compactness

--- reebill/utilbill_processor.py
+++ reebill/utilbill_processor.py
@@ -54,9 +54,10 @@ class UtilbillProcessor(object):
 
         #toggle processed state of utility bill
         if processed is not None:
-            if utilbill.rate_class is None or utilbill.supplier is None:
-                raise BillingError("Bill with unknown supplier or rate class "
-                                   "can't become processed")
+            if processed and None in (utilbill.utility, utilbill.rate_class,
+                                      utilbill.supplier):
+                raise BillingError("Bill with unknown Utility, Supplier or "
+                                   "Rate Class can't become processed")
             utilbill.processed = processed
 
         if utilbill.editable():
