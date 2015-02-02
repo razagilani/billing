d57560f306c77fa3757d37e496ca52934679baa6
Raza Gilani <rgilani@skylineinnovations.com>
2015-02-02 18:29:58 -0500

    added unit_tests for P&G ui

--- test/test_pg/test_web.py
+++ test/test_pg/test_web.py
@@ -5,7 +5,8 @@ import tempfile
 from json import dumps, loads
 
 from core import init_model
-from core.model import Session, UtilityAccount, Address, UtilBill, Utility
+from core.model import Session, UtilityAccount, Address, UtilBill, Utility,\
+    Charge, Register
 from pg import wsgi
 from pg.pg_model import PGAccount
 from test import init_test_config
@@ -14,8 +15,8 @@ from test.setup_teardown import TestCaseWithSetup
 # this may change
 URL_PREFIX = '/utilitybills/'
 
-class TestPGWeb(unittest.TestCase):
 
+class TestPGWeb(unittest.TestCase):
     maxDiff = None
 
     def assertJson(self, expected, actual):
@@ -29,7 +30,7 @@ class TestPGWeb(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def setUp(self):
-        #self.db_fd, wsgi.app.config['DATABASE'] = tempfile.mkstemp()
+        # self.db_fd, wsgi.app.config['DATABASE'] = tempfile.mkstemp()
         wsgi.app.config['TESTING'] = True
         self.app = wsgi.app.test_client()
 
@@ -40,85 +41,177 @@ class TestPGWeb(unittest.TestCase):
         TestCaseWithSetup.truncate_tables(s)
         utility = Utility('Example Utility', Address())
         ua1 = UtilityAccount('Account 1', '11111', utility, None, None,
-                           Address(), Address(), '1')
+                             Address(), Address(), '1')
         ua2 = UtilityAccount('Account 2', '22222', utility, None, None,
-                           Address(), Address(), '2')
+                             Address(), Address(), '2')
         ua3 = UtilityAccount('Not PG', '33333', utility, None, None,
-                           Address(), Address(), '3')
+                             Address(), Address(), '3')
         ua1.id, ua2.id, ua3.id = 1, 2, 3
         s.add_all([ua1, ua2, ua3])
         s.add_all([PGAccount(ua1), PGAccount(ua2)])
-        s.add_all([
-            UtilBill(ua1, UtilBill.Complete, 'electric', utility, None,
-                     None, Address(), Address(street='1 Example St.')),
-            UtilBill(ua1, UtilBill.Complete, 'electric', utility, None,
-                     None, Address(), Address(street='2 Example St.'))
-        ])
+        ub1 = UtilBill(ua1, UtilBill.Complete, 'electric', utility, None,
+                       None, Address(), Address(street='1 Example St.'))
+        ub2 = UtilBill(ua1, UtilBill.Complete, 'electric', utility, None,
+                       None, Address(), Address(street='2 Example St.'))
+        ub1.id = 1
+        ub2.id = 2
+        register1 = Register(ub1, "ABCDEF description",
+                "ABCDEF", 'therms', False, "total", None, "GHIJKL",
+                quantity=150,
+                register_binding='REG_TOTAL')
+        register2 = Register(ub2, "ABCDEF description",
+                "ABCDEF", 'therms', False, "total", None, "GHIJKL",
+                quantity=150,
+                register_binding='REG_TOTAL')
+        s.add_all([register1, register2])
+        ub1.registers = [register1]
+        ub2.registers = [register2]
+
+        c1 = Charge(ub1, 'CONSTANT', 0.4, '100', unit='dollars',
+                    type='distribution', target_total=1)
+        c2 = Charge(ub1, 'LINEAR', 0.1, 'REG_TOTAL.quantity * 3',
+                    unit='therms', type='supply', target_total=2)
+        c3 = Charge(ub2, 'LINEAR_PLUS_CONSTANT', 0.1,
+                    'REG_TOTAL.quantity * 2 + 10', unit='therms',
+                    type='supply')
+        c4 = Charge(ub2, 'BLOCK_1', 0.3, 'min(100, REG_TOTAL.quantity)',
+                    unit='therms', type='distribution')
+        c5 = Charge(ub2, 'BLOCK_2', 0.4,
+                    'min(200, max(0, REG_TOTAL.quantity - 100))',
+                    unit='dollars', type='supply')
+        c1.id, c2.id, c3.id, c4.id, c5.id = 1, 2, 3, 4, 5
+        s.add_all([c1, c2, c3, c4, c5])
+        s.add_all([ub1, ub2])
+
         s.commit()
 
     def test_accounts(self):
         rv = self.app.get(URL_PREFIX + 'accounts')
         self.assertJson([
-            {'utility_account_number': '1', 'account': '11111', 'id': 1},
-            {'utility_account_number': '2', 'account': '22222', 'id': 2},
-        ], rv.data)
+                            {'utility_account_number': '1', 'account': '11111', 'id': 1},
+                            {'utility_account_number': '2', 'account': '22222', 'id': 2},
+                        ], rv.data)
 
     def test_utilbills_list(self):
-        #rv = self.app.get(URL_PREFIX + 'utilitybills', data=dict(id=1))
+        # rv = self.app.get(URL_PREFIX + 'utilitybills', data=dict(id=1))
         rv = self.app.get(URL_PREFIX + 'utilitybills?id=1')
 
         self.assertJson({
-            'rows': [
-                {'account': None,
-                 'computed_total': 0.0,
-                 'id': 12020,
-                 'next_estimated_meter_read_date': None,
-                 'pdf_url': '',
-                 'period_end': None,
-                 'period_start': None,
-                 'processed': False,
-                 'rate_class': 'None',
-                 'service': 'Electric',
-                 'service_address': '2 Example St., ,  ',
-                 'supplier': 'None',
-                 'supply_total': 0.0,
-                 'total_charges': 0.0,
-                 'total_energy': 0.0,
-                 'utility': 'Example Utility',
-                 'utility_account_number': '1'
-                },
-                {'account': None,
-                 'computed_total': 0.0,
-                 'id': 12019,
-                 'next_estimated_meter_read_date': None,
-                 'pdf_url': '',
-                 'period_end': None,
-                 'period_start': None,
-                 'processed': False,
-                 'rate_class': 'None',
-                 'service': 'Electric',
-                 'service_address': '1 Example St., ,  ',
-                 'supplier': 'None',
-                 'supply_total': 0.0,
-                 'total_charges': 0.0,
-                 'total_energy': 0.0,
-                 'utility': 'Example Utility',
-                 'utility_account_number': '1'
-                }
-        ],
-            'results': 0,
-        }, rv.data)
+                            'rows': [
+                                {'account': None,
+                                    'computed_total': 0.0,
+                                    'id': 2,
+                                    'next_estimated_meter_read_date': None,
+                                    'pdf_url': '',
+                                    'period_end': None,
+                                    'period_start': None,
+                                    'processed': False,
+                                    'rate_class': 'None',
+                                    'service': 'Electric',
+                                    'service_address': '2 Example St., ,  ',
+                                    'supplier': 'None',
+                                    'supply_total': 0.0,
+                                    'total_charges': 0.0,
+                                    'total_energy': 150.0,
+                                    'utility': 'Example Utility',
+                                    'utility_account_number': '1'
+                                },
+                                {'account': None,
+                                    'computed_total': 0.0,
+                                    'id': 1,
+                                    'next_estimated_meter_read_date': None,
+                                    'pdf_url': '',
+                                    'period_end': None,
+                                    'period_start': None,
+                                    'processed': False,
+                                    'rate_class': 'None',
+                                    'service': 'Electric',
+                                    'service_address': '1 Example St., ,  ',
+                                    'supplier': 'None',
+                                    'supply_total': 2.0,
+                                    'total_charges': 0.0,
+                                    'total_energy': 150.0,
+                                    'utility': 'Example Utility',
+                                    'utility_account_number': '1'
+                                }
+                            ],
+                            'results': 2,
+                        }, rv.data)
+
+    def test_charges_list(self):
+        rv = self.app.get(URL_PREFIX + 'charges?utilbill_id=1')
+        self.assertJson({
+                            'rows': [
+                                {
+                                    "target_total": 2.0,
+                                    "rsi_binding": "LINEAR",
+                                    "id": 2},
+                            ],
+                            'results': 1,
+                        }, rv.data)
+        rv = self.app.get(URL_PREFIX + 'charges?utilbill_id=2')
+        self.assertJson({
+                            'rows': [
+                                {
+                                    "target_total": None,
+                                    "rsi_binding": "LINEAR_PLUS_CONSTANT",
+                                    "id": 3
+                                },
+                                {
+                                    "target_total": None,
+                                    "rsi_binding": "BLOCK_2",
+                                    "id": 5
+                                }
+                            ],
+                            'results': 2,
+                        }, rv.data)
+
+    def test_charge(self):
+        rv = self.app.put(URL_PREFIX + 'charges/2', data=dict(
+            id=2,
+            rsi_binding='NON_LINEAR',
+            target_total=1
+        ))
+        self.assertJson({
+                            'rows':
+                                {
+                                    "target_total": 1,
+                                    "rsi_binding": "NON_LINEAR",
+                                    "id": 2
+                                },
+                                'results': 1
+                        }, rv.data)
 
     def test_utilbill(self):
-        rv = self.app.put(URL_PREFIX + 'utilitybills/?id=2', data=dict(
-            id=1,
-            period_start=datetime(2000,1,1).isoformat()
+        rv = self.app.put(URL_PREFIX + 'utilitybills/1', data=dict(
+            id=2,
+            period_start=datetime(2000, 1, 1).isoformat()
         ))
-        self.assertJson([
-        ], rv.data)
+        self.assertJson({
+                            'rows':
+                                {'account': None,
+                                    'computed_total': 85.0,
+                                    'id': 1,
+                                    'next_estimated_meter_read_date': None,
+                                    'pdf_url': '',
+                                    'period_end': None,
+                                    'period_start': '2000-01-01',
+                                    'processed': False,
+                                    'rate_class': 'None',
+                                    'service': 'Electric',
+                                    'service_address': '1 Example St., ,  ',
+                                    'supplier': 'None',
+                                    'supply_total': 2.0,
+                                    'total_charges': 0.0,
+                                    'total_energy': 150.0,
+                                    'utility': 'Example Utility',
+                                    'utility_account_number': '1'
+                                },
+                            'results': 1,
+                        } , rv.data)
 
     def tearDown(self):
         TestCaseWithSetup.truncate_tables(Session())
-        #os.close(self.db_fd)
+        # os.close(self.db_fd)
         #os.unlink(wsgi.app.config['DATABASE'])
 
