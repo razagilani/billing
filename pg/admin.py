c335080b410c76e345c6acc1ab9c34fb6c64d5ce
? <dklothe@skylineinnovations.com>
2015-02-04 15:42:06 -0500

    move code to set up admin UI to pg/admin.py

--- pg/admin.py
+++ pg/admin.py
@@ -1,8 +1,8 @@
 from flask import session, url_for, redirect, request
-from flask.ext.admin import AdminIndexView, expose
+from flask.ext.admin import AdminIndexView, expose, Admin
 from flask.ext.admin.contrib.sqla import ModelView
-from core.model import Supplier, Utility, RateClass
-from reebill.state import ReeBillCustomer
+from core.model import Supplier, Utility, RateClass, UtilityAccount, Session, UtilBill
+from reebill.state import ReeBillCustomer, ReeBill
 
 
 class MyAdminIndexView(AdminIndexView):
@@ -80,3 +80,17 @@ class RateClassModelView(LoginModelView):
     def __init__(self, session, **kwargs):
         super(RateClassModelView, self).__init__(RateClass, session, **kwargs)
 
+
+def make_admin(app):
+    '''Return a new Flask 'Admin' object associated with 'app' representing
+    the admin UI.
+    '''
+    admin = Admin(app, index_view=MyAdminIndexView())
+    admin.add_view(CustomModelView(UtilityAccount, Session()))
+    admin.add_view(CustomModelView(UtilBill, Session(), name='Utility Bill'))
+    admin.add_view(UtilityModelView(Session()))
+    admin.add_view(SupplierModelView(Session()))
+    admin.add_view(RateClassModelView(Session()))
+    admin.add_view(ReeBillCustomerModelView(Session(), name='ReeBill Account'))
+    admin.add_view(CustomModelView(ReeBill, Session(), name='Reebill'))
+    return admin
