d760caafea2bbfe53fe161cd34196055cc3101a1
Thomas Hoffmann <thoffmann@skylineinnovations.com>
2015-02-04 19:15:37 -0500

    removing more unused stuff

--- pg/static/app/store/ServiceTypes.js
+++
@@ -1,9 +0,0 @@
-Ext.define('ReeBill.store.ServiceTypes', {
-    extend: 'Ext.data.Store',
-    fields: ['display_name', 'service_type'],
-    data: [
-        {display_name: 'Thermal', service_type: 'thermal'},
-        {display_name: 'PV', service_type: 'pv'},
-        {display_name: 'None', service_type: null}
-    ]
-});
