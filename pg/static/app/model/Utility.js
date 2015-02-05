97a872e99ff0cb93554d292101105c83ae1f4f59
Thomas Hoffmann <thoffmann@skylineinnovations.com>
2015-02-04 19:02:49 -0500

    making utility/rate class work

--- pg/static/app/model/Utility.js
+++ pg/static/app/model/Utility.js
@@ -2,15 +2,6 @@ Ext.define('ReeBill.model.Utility', {
     extend: 'Ext.data.Model',
     fields: [
         {name: 'name', type: 'string'},
-        {name: 'id', type: 'int'},
-        {name: 'address_id', type: 'int'},
-        {name: 'guid', type: 'string'},
-        {name: 'discriminator', type: 'string'}
-    ],
-    hasMany: {
-            model: 'RateClass',
-            name: 'rateclasses',
-            foreignKey: 'utility_id',
-            accosiationKey: 'rateclasses'
-        }
+        {name: 'id', type: 'int'}
+    ]
 });
