3ee20428b996301d72d3a3f3a212dc222b74681b
Raza Gilani <rgilani@skylineinnovations.com>
2015-02-12 19:16:12 -0500

    fix for the bug

---
+++ brokerage/static/app/model/Charge.js
@@ -0,0 +1,9 @@
+Ext.define('ReeBill.model.Charge', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {name: 'id', type: 'int'},
+        {name: 'utilbill_id', type: 'int'},
+        {name: 'rsi_binding', type: 'string'},
+        {name: 'target_total', type: 'float'},
+    ]
+});
