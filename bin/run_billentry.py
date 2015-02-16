9750038a7d5d9c57b1d1e5a6533b13642938d25c
? <dklothe@skylineinnovations.com>
2015-02-16 16:01:53 -0500

    move run_... scripts to new "bin" directory

---
+++ bin/run_billentry.py
@@ -0,0 +1,8 @@
+"""This script is used to run the "Billentry" app. This can be used with apache to run as a wsgi application, or executed in development to run with the Flask web server.
+"""
+from core import initialize
+from brokerage.billentry import application
+initialize()
+
+if __name__ == '__main__':
+    application.run(debug=True)
