30e5eaec844af6c56645145191cf9a695e6db964
Raza Gilani <rgilani@skylineinnovations.com>
2015-02-24 18:30:58 -0500

    displayed the original error message generated by SQLAlchemy

--- brokerage/billentry.py
+++ brokerage/billentry.py
@@ -424,5 +424,5 @@ admin = make_admin(app)
 @app.errorhandler(AssertionError)
 def handler(error):
     Session.rollback()
-    flash('This record cannot be deleted due to database integrity violation', 'error')
+    flash('This record cannot be as %s' %error, 'error')
     return redirect(url_for('reebillcustomer.index_view'))
