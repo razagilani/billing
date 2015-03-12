d473bf6bb4cfba252342588a434e52eaf632fa9f
? <dklothe@skylineinnovations.com>
2015-03-12 15:23:44 -0400

    add Alembic upgrade script to clean up data in MySQL before migrating, + add Bill Entry tables to TestCaseWithSetup.truncate_tables (20 tests failing)

---
+++ alembic/versions/58383ed620d3_postgres.py
@@ -0,0 +1,27 @@
+"""postgres
+
+Things that need to be cleaned up in MySQL in preparation for migrating to
+PostgreSQL.
+
+Revision ID: 58383ed620d3
+Revises: 52a7069819cb
+Create Date: 2015-03-12 15:02:51.153207
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '58383ed620d3'
+down_revision = '52a7069819cb'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.alter_column('reebill', 'issued', existing_type=sa.Integer,
+                    type_=sa.Boolean)
+
+def downgrade():
+    op.alter_column('reebill', 'issued', existing_type=sa.Boolean,
+                    type_=sa.Integer)
+
