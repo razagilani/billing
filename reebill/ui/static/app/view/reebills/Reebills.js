ed971bd22bc84b4258dc7898b8f401d2aca426b5
? <dklothe@skylineinnovations.com>
2015-03-23 12:32:02 -0400

    a start

---
+++ reebill/ui/static/app/view/reebills/Reebills.js
@@ -0,0 +1,150 @@
+Ext.define('ReeBill.view.reebills.Reebills', {
+    extend: 'Ext.grid.Panel',
+    requires: ['ReeBill.store.Reebills'],
+    title: 'Reebills',
+    alias: 'widget.reebills',    
+    store: 'Reebills',
+    
+    viewConfig: {
+        trackOver: false,
+        stripeRows: true,
+        getRowClass: function(record) {
+            if (record.get('issued')) {
+                // issued bill
+            } else if (record.get('version') == 0) {
+                // unissued version-0 bill
+                if (record.get('processed')){
+                    return 'reebill-grid-unissued-processed';
+                }
+                return 'reebill-grid-unissued';
+            } else {
+                // unissued correction
+                return 'reebill-grid-unissued-correction';
+            }
+        }
+    },
+
+    columns: [{
+        header: 'Sequence',
+        dataIndex: 'sequence',
+        width: 90
+    },{
+        header: 'Corrections',
+        dataIndex: 'corrections',
+        width: 100
+    },{
+        header: 'Start Date',
+        dataIndex: 'period_start',
+        width: 120
+    },{
+        header: 'End Date',
+        dataIndex: 'period_end',
+        width: 120
+    },{
+        header: 'Issue Date',
+        dataIndex: 'issue_date',
+        width: 100,
+        renderer: function(value) {
+            return Ext.util.Format.date(value, 'Y-m-d');
+        }
+    },{
+        header: 'Processed',
+        dataIndex: 'processed',
+        width: 100,
+        renderer: function(value) {
+            return value ? 'Yes' : 'No';
+        }
+    },{
+        header: 'Hypo',
+        dataIndex: 'hypothetical_total',
+        width: 120,
+        align: 'right',
+        renderer: Ext.util.Format.usMoney
+    },{
+        header: 'Actual',
+        dataIndex: 'actual_total',
+        width: 120,
+        align: 'right',
+        renderer: Ext.util.Format.usMoney
+    },{
+        header: 'RE&E',
+        dataIndex: 'ree_quantity',
+        width: 120,
+        align: 'right',
+        renderer: function(value) {
+            if (typeof(value) == 'number')
+                return value.toFixed(5);
+
+            return value;
+        }
+    },{
+        header: 'RE&E Value',
+        dataIndex: 'ree_value',
+        width: 120,
+        align: 'right',
+        renderer: Ext.util.Format.usMoney
+    },{
+        header: 'RE&E Charges',
+        dataIndex: 'ree_charge',
+        align: 'right',
+        width: 120,
+        renderer: Ext.util.Format.usMoney
+    }],
+
+    dockedItems: [{
+        dock: 'top',
+        xtype: 'toolbar',
+        layout: {
+            overflowHandler: 'Menu'
+        },
+        items: [{
+            xtype: 'button',
+            text: 'Create Next',
+            action: 'createNext',
+            iconCls: 'silk-add'
+        },{
+            xtype: 'button',
+            text: 'Delete',
+            action: 'deleteReebill',
+            iconCls: 'silk-delete',
+            disabled: true
+        },{        
+            xtype: 'button',
+            text: 'Create New Version',
+            action: 'createNewVersion',
+            iconCls: 'silk-add',
+            disabled: true
+        },'-',{
+            xtype: 'button',
+            text: 'Bind RE&E Offset',
+            action: 'bindREOffset',
+            disabled: true
+        },{
+            xtype: 'button',
+            text: 'Compute',
+            action: 'computeReebill',
+            disabled: true
+        },{
+            xtype: 'button',
+            text: 'Update Readings',
+            action: 'updateReadings',
+            disabled: true
+        },{
+            xtype: 'button',
+            text: 'Toggle Processed',
+            action: 'toggleReebillProcessed',
+            disabled: true
+        },'-',{
+            xtype: 'button',
+            text: 'Render PDF',
+            action: 'renderPdf',
+            disabled: true
+        },'-',{
+            xtype: 'button',
+            text: 'Mail',
+            iconCls: 'silk-email',
+            action: 'email',
+            disabled: true
+        }]
+    }]
+});
---
+++ upgrade_scripts/v26/__init__.py
@@ -0,0 +1,22 @@
+"""Upgrade script for version 26.
+
+Script must define `upgrade`, the function for upgrading.
+
+Important: For the purpose of allowing schema migration, this module will be
+imported with the data model uninitialized! Therefore this module should not
+import any other code that that expects an initialized data model without first
+calling :func:`.billing.init_model`.
+"""
+import logging
+
+from upgrade_scripts import alembic_upgrade
+from core import init_model
+from core.model import Session
+
+log = logging.getLogger(__name__)
+
+def upgrade():
+    log.info('Beginning upgrade to version 26')
+
+    alembic_upgrade('23a21392b372')
+    init_model(schema_revision='23a21392b372')
