a883051f7f2be5ad90512823c9abf9ee0acffbdf
Raza Gilani <rgilani@skylineinnovations.com>
2015-03-26 16:21:04 -0400

    made Total Charges field uneditable

--- billentry/static/app/model/UtilityBill.js
+++ billentry/static/app/model/UtilityBill.js
@@ -31,6 +31,7 @@ Ext.define('ReeBill.model.UtilityBill', {
 
         // new
         {name: 'service_address'},
+        {name: 'computed_total', type: 'float'},
         {name: 'next_meter_read_date', type: 'date', dateFormat: 'Y-m-d' },
         {name: 'supply_total'},
         {name: 'utility_account_number'},
