e8765dd33aa2bd21ec8b3223eec571a8bcf08283
? <dklothe@skylineinnovations.com>
2015-03-27 18:57:14 -0400

    more code cleanup, seems unlikely to break anything

---
+++ billentry/static/app/store/AccountsFilter.js
@@ -0,0 +1,25 @@
+Ext.define('BillEntry.store.AccountsFilter', {
+    extend: 'Ext.data.Store',
+    fields: ['label', 'filter', 'value'],
+    data: [{
+        label: 'No filter',
+        filter: Ext.create('Ext.util.Filter', {
+            filterFn: function(item) {return true;},
+            root: 'data'}),
+        value: 'none'
+    },{
+        label: 'Accounts with Bills To Be Entered',
+        filter: Ext.create('Ext.util.Filter', {
+            filterFn: function (item) {
+                return item.get('bills_to_be_entered') == true ;},
+            root: 'data'}),
+        value: 'enter_bills'
+    },{
+        label: 'Accounts with Bills Previously Entered',
+        filter: Ext.create('Ext.util.Filter', {
+            filterFn: function(item){
+                return item.get('bills_to_be_entered') == false;},
+            root: 'data'}),
+        value: 'entered_bills'
+    }]
+});
