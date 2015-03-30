56b0f4b9e9b6a81eafc30d8a7ef34fbc40e4adb0
Thomas Hoffmann <thoffmann@nextility.com>
2015-03-30 19:49:41 -0400

    Implemented more useful filter on numeric columns

--- billentry/static/app/view/reports/UserUtilBillCount.js
+++ billentry/static/app/view/reports/UserUtilBillCount.js
@@ -18,19 +18,28 @@ Ext.define('BillEntry.view.reports.UserUtilBillCount', {
         header: '# of Gas Bills Entered',
         dataIndex: 'gas_count',
         width: 120,
-        items: utils.makeGridFilterTextField('count'),
+        items: [
+            utils.makeNumericGridFilterTextField('gas_count', '>'),
+            utils.makeNumericGridFilterTextField('gas_count', '<')
+        ],
         summaryType: 'sum'
     }, {
         header: '# of Electric Bills Entered',
         dataIndex: 'electric_count',
         width: 120,
-        items: utils.makeGridFilterTextField('count'),
+        items: [
+            utils.makeNumericGridFilterTextField('electric_count', '>'),
+            utils.makeNumericGridFilterTextField('electric_count', '<')
+        ],
         summaryType: 'sum'
     }, {
         header: 'Total # of Bills Entered',
         dataIndex: 'total_count',
-        width: 120,
-        items: utils.makeGridFilterTextField('count'),
+        width: 150,
+        items: [
+            utils.makeNumericGridFilterTextField('total_count', '>'),
+            utils.makeNumericGridFilterTextField('total_count', '<')
+        ],
         summaryType: 'sum'
     }]
 
