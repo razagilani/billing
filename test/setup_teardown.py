b8b50e737b2c29a4b2839968b5ecb546a1c5d5ab
? <dklothe@skylineinnovations.com>
2015-03-31 20:00:22 -0400

    change FakeS3 sleep interval from 0.1 to 1

--- test/setup_teardown.py
+++ test/setup_teardown.py
@@ -151,7 +151,7 @@ class FakeS3Manager(object):
         # make sure FakeS3 is actually running (and did not immediately exit
         # because, for example, another instance of it is already
         # running and occupying the same port)
-        sleep(0.1)
+        sleep(1)
         cls.check()
 
     @classmethod
