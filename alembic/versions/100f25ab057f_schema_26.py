f26be93d3ef48e12239f94bb6e77e9aaadb3fd85
Thomas Hoffmann <thoffmann@nextility.com>
2015-04-02 17:27:35 -0400

    BILL-6212 fix for bug that causes destaging to fail

--- alembic/versions/100f25ab057f_schema_26.py
+++ alembic/versions/100f25ab057f_schema_26.py
@@ -49,5 +49,3 @@ def upgrade():
                existing_type=mysql.VARCHAR(length=10),
                type_=mysql.VARCHAR(length=20))
     op.add_column('utilbill', sa.Column('tou', sa.Boolean(), nullable=False))
-
-    op.add_column('utilbill', sa.Column('tou', sa.Boolean(), nullable=False))
