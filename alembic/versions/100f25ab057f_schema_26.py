f7aa48cb538f6f0ba36b6ba1b72b50c2d01795d4
Thomas Hoffmann <thoffmann@nextility.com>
2015-04-02 16:04:49 -0400

    Merged in razagilani/billing/BILL-6195 (pull request #195)
    implemented meter_identifier and added it to altitude_export

--- alembic/versions/100f25ab057f_schema_26.py
+++ alembic/versions/100f25ab057f_schema_26.py
@@ -50,3 +50,4 @@ def upgrade():
                type_=mysql.VARCHAR(length=20))
     op.add_column('utilbill', sa.Column('tou', sa.Boolean(), nullable=False))
 
+    op.add_column('utilbill', sa.Column('tou', sa.Boolean(), nullable=False))
