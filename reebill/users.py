9fc176c489581f9d1568ccf390b7fcb42cc5c1f0
? <dklothe@skylineinnovations.com>
2015-04-20 17:49:20 -0400

    move admin script part of reebill/users.py to scripts/reebill_user_admin.py

--- reebill/users.py
+++ reebill/users.py
@@ -1,6 +1,5 @@
 import json
 import copy
-import argparse
 
 import bcrypt
 from sqlalchemy.orm.exc import NoResultFound
@@ -97,44 +96,3 @@ class UserDAO(object):
         # salt stays the same
         user.password_hash = bcrypt.hashpw(new_password, user.salt)
         return True
-        
-if __name__ == '__main__':
-    # command-line arguments
-    parser = argparse.ArgumentParser(
-            description='Create and authenticate user accounts')
-    parser.add_argument('command', choices=['add', 'check', 'change'],
-            help=('"add" to create a user, "check" to test authentication, '
-            '"change" to change password'))
-    parser.add_argument('identifier')
-    parser.add_argument('password')
-    parser.add_argument('newpassword', nargs='?') # optional
-    args = parser.parse_args()
-
-    dao = UserDAO()
-
-    if args.command == 'add':
-        try:
-            dao.create_user(args.identifier, args.password)
-        except ValueError:
-            print 'User "%s" already exists; use "change" to change password' \
-                    % args.identifier
-        else:
-            print 'New user created'
-
-    elif args.command == 'check':
-        result = dao.load_user(args.identifier, args.password)
-        if result is None:
-            print 'Authentication failed'
-        else:
-            print 'Authentication succeeded'
-
-    elif args.command == 'change':
-        if args.newpassword is None:
-            print 'New password must be specified'
-            exit(1)
-        result = dao.change_password(args.identifier, args.password,
-                args.newpassword)
-        if result:
-            print 'Password changed'
-        else:
-            print 'Password change failed'
