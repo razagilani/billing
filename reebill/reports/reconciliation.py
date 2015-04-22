83ae72e2d5b76730adf10a78b039378fc012a412
? <dklothe@skylineinnovations.com>
2015-04-22 13:57:23 -0400

    fix spelling of ReconciliationReport, add docstrings, add ReconciliationReport.write_json method + test

--- reebill/reports/reconciliation.py
+++ reebill/reports/reconciliation.py
@@ -1,11 +1,20 @@
 from tablib import Dataset
 
-class ReconcilationReport(object):
+class ReconciliationReport(object):
+    """Produces a report on how much energy was billed for in each bill vs.
+    actual energy sold as currently shown in the OLAP database.
+    """
     def __init__(self, reebill_dao, ree_getter):
+        """
+        :param reebill_dao: ReeBillDAO
+        :param ree_getter: RenewableEnergyGetter
+        """
         self.reebill_dao = reebill_dao
         self.ree_getter = ree_getter
 
     def get_dataset(self):
+        """Return a tablib.Dataset containing the report data.
+        """
         dataset = Dataset(headers=['Customer ID', 'Sequence', 'Total Energy',
                                    'Current Total Energy'])
         for reebill in self.reebill_dao.get_all_reebills():
@@ -17,3 +26,9 @@ class ReconcilationReport(object):
                  current_energy])
         return dataset
 
+    def write_json(self, output_file):
+        """Write report data to a file.
+        :param output_file: file object.
+        """
+        dataset = self.get_dataset()
+        output_file.write(dataset.json)
