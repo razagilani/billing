e9df3e5c57df1ab7c77565fc0ab9555b2447f8f2
Raza Gilani <rgilani@skylineinnovations.com>
2015-04-28 16:26:06 -0400

    initial changes

--- upgrade_scripts/v27/__init__.py
+++ upgrade_scripts/v27/__init__.py
@@ -8,11 +8,19 @@ import any other code that that expects an initialized data model without first
 calling :func:`.core.init_model`.
 """
 import logging
+from core.model import Session, UtilBill
+from core.model.model import SupplyGroup
 
 from upgrade_scripts import alembic_upgrade
 from core import init_model
 
 log = logging.getLogger(__name__)
+
+def create_and_assign_supply_groups(s):
+    bills = s.query(UtilBill).all()
+    for bill in bills:
+        supply_group = SupplyGroup('SOSGroup', bill['supplier'], bill.get_service())
+        s.add(supply_group)
 
 def upgrade():
     log.info('Beginning upgrade to version 27')
