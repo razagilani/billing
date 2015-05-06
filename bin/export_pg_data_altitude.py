d61ca6a115cc153330667089a3657c10b55b6b9a
Rich Andrews <stuart.r.andrews@gmail.com>
2015-05-06 16:35:31 -0400

    merged default down to branch

---
+++ bin/export_pg_data_altitude.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""Script to generate CSV file with data described in "Billing 24" Google
+doc. Can be executed by cron.
+
+This file doesn't/shouldn't have test coverage so don't put substantive code
+in it!
+"""
+from sys import stdout
+from argparse import ArgumentParser
+from uuid import uuid4
+from core import init_config, init_model
+from core import altitude
+from brokerage.export_altitude import PGAltitudeExporter, _load_pg_utilbills
+
+if __name__ == '__main__':
+    init_config()
+    init_model()
+    pgae = PGAltitudeExporter(uuid4, altitude)
+    pgae.write_csv(_load_pg_utilbills(), stdout)
