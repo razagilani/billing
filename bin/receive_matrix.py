e7acf463ddbbd737385eee7d38158db2e2fff9a4
? <dklothe@skylineinnovations.com>
2015-05-12 18:13:18 -0400

    add bin/receive_matrix.py

---
+++ bin/receive_matrix.py
@@ -0,0 +1,20 @@
+from sys import stdin
+from cStringIO import StringIO
+from brokerage.read_quotes import DirectEnergyMatrixParser
+from util.email_util import get_attachments
+
+if __name__ == '__main__':
+    email_text = stdin.read()
+    attachments = get_attachments(email_text)
+    assert len(attachments) == 1
+    name, content = attachments[0]
+    quote_file = StringIO()
+    quote_file.write(content)
+    quote_file.seek(0)
+
+    quote_parser = DirectEnergyMatrixParser()
+    quote_parser.load_file(quote_file)
+    quote_parser.validate()
+
+    for q in quote_parser.extract_quotes():
+        print q
