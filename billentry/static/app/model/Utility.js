2a2790ce309e6d7fb6f41493d541b061537e1fbb
Raza Gilani <rgilani@skylineinnovations.com>
2015-05-19 14:33:10 -0400

    refactored utility, rate_class and supplier

--- billentry/static/app/model/Utility.js
+++ billentry/static/app/model/Utility.js
@@ -2,6 +2,10 @@ Ext.define('BillEntry.model.Utility', {
     extend: 'Ext.data.Model',
     fields: [
         {name: 'name', type: 'string'},
-        {name: 'id', type: 'int'}
+        {name: 'id', type: 'int', useNull: true},
+        {name: 'address_id', type: 'int'},
+        {name: 'guid', type: 'string'},
+        {name: 'discriminator', type: 'string'},
+        {name: 'sos_supply_group_id', type: 'int'}
     ]
 });
