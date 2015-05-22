5a1f9f7277bd6590fe61478acedaf282f2fee98c
? <dklothe@skylineinnovations.com>
2015-05-22 15:24:29 -0400

    update model._schema_revision to 30597f9f53b9--almost tests pass except 2 weird failures about charge quantities being 0 or None

--- core/model/model.py
+++ core/model/model.py
@@ -128,7 +128,7 @@ class Base(object):
 Base = declarative_base(cls=Base)
 
 
-_schema_revision = '58383ed620d3'
+_schema_revision = '30597f9f53b9'
 def check_schema_revision(schema_revision=None):
     """Checks to see whether the database schema revision matches the
     revision expected by the model metadata.
