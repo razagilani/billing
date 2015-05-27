fa5d689d529131dc795d0f3f8e57b55ff775b461
? <dklothe@skylineinnovations.com>
2015-05-27 13:43:18 -0400

    add Utility.sos_supplier, create SOS suppliers when creating utilities, create SOS supply groups when creating rate classes (does not work with flask-admin)

--- alembic/versions/3e4ceae0f397_schema_27.py
+++ alembic/versions/3e4ceae0f397_schema_27.py
@@ -35,10 +35,16 @@ def upgrade():
         sa.ForeignKeyConstraint(['supplier_id'], ['supplier.id'], ),
         sa.PrimaryKeyConstraint('id')
     )
+    op.add_column('utility', sa.Column('sos_supplier_id', sa.Integer(),
+        sa.ForeignKey('supplier.id', ondelete='CASCADE'), nullable=True,
+        unique=True))
     op.add_column('utilbill', sa.Column('supply_group_id', sa.Integer(), sa.ForeignKey('supply_group.id'), nullable=True))
     op.add_column('reebill_customer', sa.Column('payee', sa.String(length=100), nullable=False, default='Nextility'))
     op.add_column('utility_account', sa.Column('fb_supply_group_id', sa.Integer(), sa.ForeignKey('supply_group.id'),nullable=True))
-    op.add_column('rate_class', sa.Column('sos_supply_group_id', sa.Integer(), sa.ForeignKey('supply_group.id'), nullable=True))
+    op.add_column('rate_class', sa.Column('sos_supply_group_id', sa.Integer(),
+                                          sa.ForeignKey('supply_group.id',
+                                                        ondelete='CASCADE'),
+                                          nullable=True))
 
     op.create_table('be_user_session',
     sa.Column('id', sa.Integer(), nullable=False),
