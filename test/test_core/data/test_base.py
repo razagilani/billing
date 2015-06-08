a052cab8fee34b4e3a440a7a9c5af778df72ca37
? <dklothe@skylineinnovations.com>
2015-06-08 14:27:18 -0400

    BaseTest.test_copy_data_from is working

--- test/test_core/data/test_base.py
+++ test/test_core/data/test_base.py
@@ -1,47 +1,48 @@
 """Unit tests for the core.model.Base class, which contains methods shared by
 all SQLAlchemy model objects."""
-from sqlalchemy import Column, Integer, ForeignKey
-from sqlalchemy.orm import relationship
-
+from sqlalchemy import Column, Integer, ForeignKey, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker, backref
 from core.model import Base
+
 from test.testing_utils import TestCase
 
-
 class B(Base):
+    """Example class used in test below.
+    """
     __tablename__ = 'b'
     id = Column(Integer, primary_key=True)
+    a_id = Column(Integer, ForeignKey('a.id'))
 
 
 class A(Base):
+    """Example class used in test below.
+    """
     __tablename__ = 'a'
     id = Column(Integer, primary_key=True)
     x = Column(Integer)
-    b_id = Column(Integer, ForeignKey('b.id'))
-    b = relationship(B, backref='a', uselist=False)
+    bs = relationship(B, backref='a')
 
 
 class BaseTest(TestCase):
 
     def test_copy_data_from(self):
-        b = B(id=1)
-        a1 = A(x=1, b=b)
-        a1.b = b
-        a2 = A(x=None)
+        # a1's attributes are filled in, a2's are empty
+        b = B()
+        a1 = A(x=1, bs=[b])
+        a2 = A()
 
-        # TOD: why does a1.b_id not get set?
-        print a1.b_id, a1.b
-        assert a1.b_id == 1
-
+        # copy empty values from a2 to a1
         a1._copy_data_from(a2)
         self.assertEqual(None, a1.x)
-        self.assertEqual(None, a1.b_id)
-        self.assertEqual(None, a1.b)
+        self.assertEqual([], a1.bs)
         self.assertEqual(None, b.a)
 
-        b = B(id=3)
-        a2.b = b
-        a2.x = 4
+        # copy non-empty values from a2 to a1
+        b = B(id=2)
+        a2.bs = [b]
+        a2.x = 3
         a1._copy_data_from(a2)
-        self.assertEqual(3, a1.b_id)
-        self.assertEqual(b, a1.b)
-        self.assertEqual(4, a1.x)
+        self.assertEqual([b], a1.bs)
+        self.assertEqual(a1, b.a)
+        self.assertEqual(3, a1.x)
