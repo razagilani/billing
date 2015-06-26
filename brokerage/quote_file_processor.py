969198e6d8b770c6ba3f20d3ac8508870c4dfac4
? <dklothe@skylineinnovations.com>
2015-06-26 17:22:46 -0400

    add file name for USGE spreadsheet and add code to pick the QuoteParser class for each supplier

--- brokerage/quote_file_processor.py
+++ brokerage/quote_file_processor.py
@@ -3,7 +3,7 @@ import logging
 import os
 import traceback
 from brokerage.brokerage_model import Company
-from brokerage.quote_parsers import DirectEnergyMatrixParser
+from brokerage.quote_parsers import DirectEnergyMatrixParser, USGEMatrixParser
 from core.model import AltitudeSession, Session, Supplier
 
 # TODO: can't get log file to appear where it's supposed to
@@ -14,6 +14,13 @@ class QuoteFileProcessor(object):
     """Checks for files containing matrix quotes in a particular directory,
     transfers quotes from them into a database, and deletes them.
     """
+    # maps each supplier id to the class for parsing its quote file
+    CLASSES_FOR_SUPPLIERS = {
+        14: DirectEnergyMatrixParser,
+        # 95: AEP
+        199: USGEMatrixParser,
+    }
+
     # number of quotes to read and insert at once. larger is faster as long
     # as it doesn't use up too much memory. (1000 is the maximum number of
     # rows allowed per insert statement in pymssql.)
@@ -25,17 +32,17 @@ class QuoteFileProcessor(object):
         self.quote_directory_path = config.get('brokerage', 'quote_directory')
         self.altitude_session = AltitudeSession()
 
-    def _read_file(self, quote_file, altitude_supplier):
+    def _read_file(self, quote_file, supplier, altitude_supplier):
         """Read and insert 'BATCH_SIZE' quotes fom the given file.
         :param quote_file: quote file to read from
+        :param supplier: core.model.Supplier instance
         :param altitude_supplier: brokerage.brokerage_model.Company instance
         corresponding to the Company table in the Altitude SQL Server database,
         representing a supplier. Not to be confused with the "supplier" table
         (core.model.Supplier) or core.altitude.AltitudeSupplier which is a
         mapping between these two. May be None if the supplier is unknown.
         """
-        # TODO: choose correct class for each supplier
-        quote_parser = DirectEnergyMatrixParser()
+        quote_parser = self.CLASSES_FOR_SUPPLIERS[supplier.id]()
         quote_parser.load_file(quote_file)
         quote_parser.validate()
 
@@ -58,12 +65,12 @@ class QuoteFileProcessor(object):
         """
         for supplier in Session().query(Supplier).filter(
                         Supplier.matrix_file_name != None).order_by(
-            Supplier.id):
+            Supplier.name):
             # check if the file for this supplier exists and is writable
             path = os.path.join(self.quote_directory_path,
                                 supplier.matrix_file_name)
             if not os.access(path, os.W_OK):
-                self.logger.info('Skipped "%s"' % path)
+                self.logger.info('Skipped "%s": not found' % path)
                 continue
 
             # match supplier in Altitude database by name--this means names
@@ -76,7 +83,8 @@ class QuoteFileProcessor(object):
             try:
                 with open(path, 'rb') as quote_file:
                     self.logger.info('Starting to read from "%s"' % path)
-                    for count in self._read_file(quote_file, altitude_supplier):
+                    for count in self._read_file(quote_file, supplier,
+                                                 altitude_supplier):
                         self.logger.debug('%s quotes so far' % count)
                     self.altitude_session.commit()
                 os.remove(path)
