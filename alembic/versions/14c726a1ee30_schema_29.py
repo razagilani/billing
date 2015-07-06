1b3577727ba57a64a17c993d07f5f81bc616a5f6
? <dklothe@skylineinnovations.com>
2015-07-06 16:07:28 -0400

    merge with default

--- alembic/versions/14c726a1ee30_schema_29.py
+++ alembic/versions/14c726a1ee30_schema_29.py
@@ -22,6 +22,21 @@ def upgrade():
     op.add_column('quote', sa.Column('limit_volume', sa.Float))
     op.add_column('quote', sa.Column('rate_class_alias', sa.String))
     op.alter_column('quote', 'supplier_id', nullable=True)
+    op.alter_column('quote', 'date_received', new_column_name='Created_On')
+    op.alter_column('quote', 'start_from',
+                    new_column_name='Earliest_Contract_Start_Date')
+    op.alter_column('quote', 'start_until',
+                    new_column_name='Latest_Contract_Start_Date')
+    op.alter_column('quote', 'term_months',
+                    new_column_name='Contract_Term_Months')
+    op.alter_column('quote', 'purchase_of_receivables',
+                    new_column_name='Purchase_Of_Receivables')
+    op.alter_column('quote', 'min_volume',
+                    new_column_name='Minimum_Annual_Volume_KWH_Therm')
+    op.alter_column('quote', 'limit_volume',
+                    new_column_name='Maximum_Annual_Volume_KWH_Therm')
+    op.alter_column('quote', 'price',
+                    new_column_name='Supplier_Price_Dollars_KWH_Therm')
     op.rename_table('quote', 'Rate_Matrix')
 
     op.add_column('supplier', sa.Column('matrix_file_name', sa.String))
