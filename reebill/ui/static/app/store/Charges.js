1a9c05c845c8927efdff89370c86d8090b20dc22
Thomas Hoffmann <thoffmann@nextility.com>
2015-07-14 15:16:41 -0400

    fixed BILL-6441: Show formula rather than its value in list of charges from previous bill

---
+++ reebill/ui/static/app/store/Charges.js
@@ -0,0 +1,37 @@
+Ext.define('ReeBill.store.Charges', {
+    extend: 'Ext.data.Store',
+    requires: ['ReeBill.model.Charge'],
+    model: 'ReeBill.model.Charge',
+
+    autoLoad: false,
+    autoSync: true,
+
+    groupField: 'type',
+
+	proxy: {
+		type: 'rest',
+
+        pageParam: false,
+
+        url: 'http://'+window.location.host+'/reebill/charges',
+
+		reader: {
+			type: 'json',
+			root: 'rows',
+			totalProperty: 'results'
+		},
+
+        listeners:{
+            exception: utils.makeProxyExceptionHandler('Charges'),
+            scope: this
+        }
+	},
+
+    sorters: [{
+        property: 'group',
+        direction: 'ASC'
+    }, {
+        property: 'rsi_binding',
+        direction: 'ASC'
+    }]
+});
