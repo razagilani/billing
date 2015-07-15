5655fd4917b31e2d3384df279740ce69c896c8eb
? <dklothe@skylineinnovations.com>
2015-07-15 11:50:54 -0400

    move type conversion functions from applier.py to new file type_conversion.py

---
+++ reebill/ui/static/app/store/Charges.js
@@ -0,0 +1,38 @@
+Ext.define('ReeBill.store.Charges', {
+    extend: 'Ext.data.Store',
+    requires: ['ReeBill.model.Charge'],
+    model: 'ReeBill.model.Charge',
+
+    autoLoad: false,
+    autoSync: true,
+
+    groupField: 'type',
+
+	proxy: {
+		type: 'rest',
+
+        pageParam: false,
+
+        url: 'http://'+window.location.host+'/reebill/charges',
+
+		reader: {
+			type: 'json',
+			root: 'rows',
+			totalProperty: 'results'
+		},
+
+        writer: {
+            writeAllFields: false
+        },
+
+        listeners:{
+            exception: utils.makeProxyExceptionHandler('Charges'),
+            scope: this
+        }
+	},
+
+    sorters: [{
+        property: 'group',
+        direction: 'ASC'
+    }]
+});
