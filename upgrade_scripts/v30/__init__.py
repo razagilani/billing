19c917cf8ef5ef5a644c6c8bc7d96e21bd4a2ccf
? <dklothe@skylineinnovations.com>
2015-07-15 18:55:18 -0400

    schema and data change to add reebill.utibill_id, replacing utilbill_reebill

--- upgrade_scripts/v30/__init__.py
+++ upgrade_scripts/v30/__init__.py
@@ -7,13 +7,25 @@ imported with the data model uninitialized! Therefore this module should not
 import any other code that that expects an initialized data model without first
 calling :func:`.core.init_model`.
 """
+from sqlalchemy.orm import joinedload
 from core import init_model, init_altitude_db
-from core.model import Session
+from core.model import Session, UtilBill
+from reebill.reebill_model import ReeBill
 from upgrade_scripts import alembic_upgrade
 
 
 
 def upgrade():
-    #alembic_upgrade('')
+    alembic_upgrade('1eca0edc1fb7')
 
     init_model()
+    s = Session()
+    s.execute('''update reebill as r
+    set utilbill_id = utilbill.id
+    from reebill join utilbill_reebill on r.id = utilbill_reebill.reebill_id
+    join utilbill on utilbill_reebill.utilbill_id = utilbill.id''')
+
+    # just to check that the above update worked
+    s.query(ReeBill).options(joinedload('utilbill')).all()
+
+    s.commit()
