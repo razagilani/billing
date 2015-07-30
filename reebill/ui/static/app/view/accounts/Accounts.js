9885a86eb9c7dc7aff5ead1047b5fc0422b5b98d
Raza Gilani <rgilani@skylineinnovations.com>
2015-07-30 12:26:56 -0400

    added a button to Utility Bills tab for replacing estimated bill with real

---
+++ reebill/ui/static/app/view/accounts/Accounts.js
@@ -0,0 +1,124 @@
+Ext.define('ReeBill.view.accounts.Accounts', {
+    extend: 'Ext.grid.Panel',
+    requires: [
+        'ReeBill.store.AccountsFilter'],
+    title: 'Accounts Processing Status',
+    alias: 'widget.accounts',   
+    store: 'Accounts',
+
+    plugins: [
+        Ext.create('Ext.grid.plugin.CellEditing', {
+            clicksToEdit: 2
+        })
+    ],
+    
+    viewConfig: {
+        trackOver: false,
+        stripeRows: true,
+        getRowClass: function(record) {
+            if (record.get('provisionable'))
+                return 'account-grid-gray';
+        }
+    },
+    
+    columns: [{
+        header: 'Account',
+        dataIndex: 'account',
+        width: 100,
+        items: utils.makeGridFilterTextField('account')
+    },{
+        header: 'Tags',
+        dataIndex: 'tags',
+        editor: {
+            xtype: 'textfield',
+            emptyText: 'Enter comma-separated list'
+        },
+        tdCls: 'grid-cell-wrap-text',
+        width: 150,
+        items: utils.makeGridFilterTextField('tags'),
+        renderer: function(value){
+            var rtn = [];
+            Ext.Array.each(value.split(','), function(tag){
+                tag = tag.trim();
+                if(tag){
+                    rtn.push('<span class="accounts-tag">');
+                    rtn.push(tag);
+                    rtn.push('</span>');
+                }
+            });
+            return rtn.join('')
+        }
+    },{
+        header: 'Utility Account Number',
+        dataIndex: 'utility_account_number',
+        editor: {
+            xtype: 'textfield'
+        },
+        width: 100,
+        items: utils.makeGridFilterTextField('utility_account_number')
+    },{
+        header: 'Remit To',
+        dataIndex: 'payee',
+        editor:{
+            xtype: 'textfield'
+        },
+        width: 120,
+        items: utils.makeGridFilterTextField('payee')
+    },{
+        header: 'Codename',
+        dataIndex: 'codename',
+        width: 120,
+        items: utils.makeGridFilterTextField('codename')
+    },{
+        header: 'Casual Name',
+        dataIndex: 'casualname',
+        width: 200,
+        items: utils.makeGridFilterTextField('casualname')
+    },{
+        header: 'Primus Name',
+        dataIndex: 'primusname',
+        width: 120,
+        items: utils.makeGridFilterTextField('primusname')
+    },{
+        header: 'Utility Service Address',
+        dataIndex: 'utilityserviceaddress',
+        width: 200,
+        items: utils.makeGridFilterTextField('utilityserviceaddress')
+    },{
+        header: 'Last Event',
+        dataIndex: 'lastevent',
+        minWidth: 350,
+        flex:1,
+        items: utils.makeGridFilterTextField('lastevent')
+    }],
+    dockedItems: [
+    {
+        xtype: 'toolbar',
+        dock: 'bottom',
+        items: ['->', {
+            xtype: 'combo',
+            name: 'accountsFilter',
+            fieldLabel: 'Filter',
+            labelWidth: 50,
+            width: 400,
+            value: 'none',
+            editable: false,
+            store: 'AccountsFilter',
+            triggerAction: 'all',
+            valueField: 'value',
+            displayField: 'label',
+            forceSelection: true,
+            listeners:{
+                scope: this,
+                'select': function(combo, record, index) {
+                    var g = combo.findParentByType('grid');
+                    g.getStore().clearFilter();
+                    if (combo.getValue() == 'reebillcustomers')
+                        g.getStore().filter('reebill_customer', true);
+                    else if(combo.getValue() == 'brokeragecustomers')
+                        g.getStore().filter('brokerage_account', true);
+                }
+            }
+            }, '->']
+    }]
+});
---
+++ test/test_core/test_extraction.py
@@ -0,0 +1,384 @@
+from datetime import date, datetime
+import os
+from unittest import TestCase, skip
+
+from boto.s3.connection import S3Connection
+from celery.result import AsyncResult
+from mock import Mock, NonCallableMock
+
+# init_test_config has to be called first in every test module, because
+# otherwise any module that imports billentry (directly or indirectly) causes
+# app.py to be initialized with the regular config  instead of the test
+# config. Simply calling init_test_config in a module that uses billentry
+# does not work because test are run in a indeterminate order and an indirect
+# dependency might cause the wrong config to be loaded.
+from test import init_test_config
+
+from core import init_model, ROOT_PATH
+from core.bill_file_handler import BillFileHandler
+from core.extraction.extraction import Field, Extractor, Main, TextExtractor
+from core.extraction.applier import Applier, UtilBillApplier
+from core.model import UtilityAccount, Utility, Session, Address, \
+    RateClass, Charge
+from core.model.utilbill import UtilBill, Charge
+from core.utilbill_loader import UtilBillLoader
+from exc import ConversionError, ExtractionError, MatchError, ApplicationError
+from test import init_test_config, clear_db, create_tables
+from test.setup_teardown import FakeS3Manager
+
+def setUpModule():
+    init_test_config()
+
+class FieldTest(TestCase):
+    def setUp(self):
+        self.field = Field()
+        self.field._extract = Mock(return_value='value string')
+        self.input = 'input string'
+
+        # mock the type conversion function by putting a mock into the TYPES
+        # dictionary. maybe there's a way to do this without modifying the
+        # class.
+        self.type_convert_func = Mock(return_value=1)
+        self.field.TYPES[Field.STRING] = self.type_convert_func
+
+    def test_get_value(self):
+        value = self.field.get_value(self.input)
+        self.field._extract.assert_called_once_with(self.input)
+        self.type_convert_func.assert_called_once_with('value string')
+        self.assertEqual(1, value)
+
+    def test_convert_error(self):
+        self.type_convert_func.side_effect = Exception
+        with self.assertRaises(ConversionError):
+            self.field.get_value(self.input)
+
+
+class ApplierTest(TestCase):
+    def setUp(self):
+        self.applier = UtilBillApplier.get_instance()
+        self.bill = NonCallableMock()
+        self.bill.set_total_energy = Mock()
+        self.bill.set_next_meter_read_date = Mock()
+
+    def test_default_applier(self):
+        d = date(2000,1,1)
+        self.applier.apply(UtilBillApplier.START, d, self.bill)
+        self.assertEqual(d, self.bill.period_start)
+
+        self.bill.reset_mock()
+        self.applier.apply(UtilBillApplier.END, d, self.bill)
+        self.assertEqual(d, self.bill.period_end)
+
+        self.bill.reset_mock()
+        self.applier.apply(UtilBillApplier.NEXT_READ, d, self.bill)
+        self.bill.set_next_meter_read_date.assert_called_once_with(d)
+
+        self.bill.reset_mock()
+        self.applier.apply(UtilBillApplier.ENERGY, 123.456, self.bill)
+        self.bill.set_total_energy.assert_called_once_with(123.456)
+
+    def test_apply_values(self):
+        # one field is good, 2 have ApplicationErrors (one with wrong value
+        # type, one with unknown key)
+        extractor = Mock(autospec=Extractor)
+        good = {UtilBillApplier.START: date(2000, 1, 1),
+                UtilBillApplier.CHARGES: 'wrong type', 'wrong key': 1}
+        extractor_errors = {UtilBillApplier.END: ExtractionError('an error')}
+        extractor.get_values.return_value = (good, extractor_errors)
+        bfh = Mock(autospec=BillFileHandler)
+
+        success_count, applier_errors = self.applier.apply_values(
+            extractor, self.bill, bfh)
+        self.assertEqual(1, success_count)
+        self.assertEqual(3, len(applier_errors))
+        self.assertIsInstance(applier_errors['wrong key'], ApplicationError)
+        self.assertIsInstance(applier_errors[UtilBillApplier.CHARGES],
+                              ApplicationError)
+
+    def test_errors(self):
+        # wrong key
+        with self.assertRaises(ApplicationError):
+            self.applier.apply('wrong key', 1, self.bill)
+
+        # wrong value type
+        with self.assertRaises(ApplicationError):
+            self.applier.apply(UtilBillApplier.START, 1, self.bill)
+
+        # exception in target method
+        self.bill.reset_mock()
+        self.bill.set_total_energy.side_effect = Exception
+        with self.assertRaises(ApplicationError):
+            self.applier.apply(UtilBillApplier.ENERGY, 123.456, self.bill)
+
+
+class ExtractorTest(TestCase):
+    def setUp(self):
+        # a Mock can't be used as a Field because it lacks SQLAlchemy
+        # attributes, but its methods can be mocked.
+        f1 = Field(applier_key='a')
+        f1.get_value = Mock(return_value=123)
+        f2 = Field(applier_key='b')
+        f2.get_value = Mock(side_effect=ExtractionError)
+        f3 = Field(applier_key='c')
+        f3.get_value = Mock(return_value=date(2000, 1, 1))
+
+        self.e = Extractor()
+        self.e.fields = [f1, f2, f3]
+        self.e._prepare_input = Mock(return_value='input string')
+
+        self.utilbill = Mock(autospec=UtilBill)
+        self.bill_file_handler = Mock(autospec=BillFileHandler)
+
+        # applying f1 succeeds, applying f3 fails (and f2 never gets applied
+        # because its value couldn't be extracted)
+        self.applier = Mock(autospec=Applier)
+        self.applier.apply.side_effect = [None, ApplicationError]
+
+    def test_get_values(self):
+        good, errors = self.e.get_values(self.utilbill, self.bill_file_handler)
+        self.assertEqual({'a': 123, 'c': date(2000, 1, 1)}, good)
+        self.assertEqual(['b'], errors.keys())
+        self.assertIsInstance(errors['b'], ExtractionError)
+
+
+class TextFieldTest(TestCase):
+    def setUp(self):
+        self.field = TextExtractor.TextField(
+            regex=r'([A-Za-z]+ [0-9]{1,2}, [0-9]{4})', type=Field.DATE)
+
+    def test_get_value(self):
+        self.assertEqual(date(2000, 1, 1),
+                         self.field.get_value('January 1, 2000'))
+
+        # regex doesn't match
+        with self.assertRaises(MatchError):
+            print self.field.get_value('xyz')
+
+        # matched a string but couldn't convert to a date
+        with self.assertRaises(ConversionError):
+            print self.field.get_value('Somemonth 0, 7689')
+
+
+class TextExtractorTest(TestCase):
+    def setUp(self):
+        self.text = 'Bill Text 1234.5 More Text  '
+        self.bfh = Mock(autospec=BillFileHandler)
+        self.te = TextExtractor()
+        self.bill = Mock(autospec=UtilBill)
+        self.bill.get_text.return_value = self.text
+
+    def test_prepare_input(self):
+        self.assertEqual(self.text, self.te._prepare_input(self.bill, self.bfh))
+
+
+class TestIntegration(TestCase):
+    """Integration test for all extraction-related classes with real bill and
+    database.
+    """
+    EXAMPLE_FILE_PATH = os.path.join(ROOT_PATH,
+                                     'test/test_core/data/utility_bill.pdf')
+
+    @classmethod
+    def setUpClass(cls):
+        init_test_config()
+        create_tables()
+        init_model()
+        FakeS3Manager.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        FakeS3Manager.stop()
+
+    def setUp(self):
+        clear_db()
+
+        from core import config
+        s3_connection = S3Connection(
+            config.get('aws_s3', 'aws_access_key_id'),
+            config.get('aws_s3', 'aws_secret_access_key'),
+            is_secure=config.get('aws_s3', 'is_secure'),
+            port=config.get('aws_s3', 'port'),
+            host=config.get('aws_s3', 'host'),
+            calling_format=config.get('aws_s3', 'calling_format'))
+        url_format = 'http://%s:%s/%%(bucket_name)s/%%(key_name)s' % (
+            config.get('aws_s3', 'host'), config.get('aws_s3', 'port'))
+        self.bfh = BillFileHandler(s3_connection, config.get('aws_s3', 'bucket'),
+                              UtilBillLoader(), url_format)
+
+        # create utility and rate class
+        utility = Utility(name='washington gas')
+        utility.charge_name_map = {
+            'Distribution Charge': 'DISTRIBUTION_CHARGE',
+            'Customer Charge': 'CUSTOMER_CHARGE',
+            'PGC': 'PGC',
+            'Peak Usage Charge': 'PEAK_USAGE_CHARGE',
+            'DC Rights-of-Way Fee': 'RIGHT_OF_WAY',
+            'Sustainable Energy Trust Fund': 'SETF',
+            'Energy Assistance Trust Fund': 'EATF',
+            'Delivery Tax': 'DELIVERY_TAX',
+            'Sales Tax': 'SALES_TAX',
+        }
+        rate_class = RateClass(utility=utility)
+        account = UtilityAccount('', '123', None, None, None, Address(),
+                                 Address())
+
+        # create bill with file
+        self.bill = UtilBill(account, utility, rate_class)
+        with open(self.EXAMPLE_FILE_PATH, 'rb') as bill_file:
+            self.bfh.upload_file_for_utilbill(self.bill, bill_file)
+        self.bill.date_extracted = None
+
+        # create extractor
+        e1 =  TextExtractor(name='Example')
+        date_format = r'[A-Za-z]+\s*[0-9]{1,2},\s*[0-9]{4}'
+        num_format = r'[0-9,\.]+'
+        wg_start_regex = r'(%s)-%s\s*\(\d+ Days\)' % (date_format, date_format)
+        wg_end_regex = r'%s-(%s)\s*\(\d+ Days\)' % (date_format, date_format)
+        wg_energy_regex = r"Distribution Charge\s+(%s)" % num_format
+        wg_next_meter_read_regex = r'Your next meter reading date is (%s)' % \
+                                   date_format
+        wg_charges_regex = r'(DISTRIBUTION SERVICE.*?(?:Total Washington Gas ' \
+                           r'Charges This Period|the easiest way to pay))'
+        #wg_rate_class_regex = r'Rate Class:\s+Meter number:\s+(.*\n)\n\n.*'
+        # wg_rate_class_regex = r'Rate Class:\s+Meter number:\s+^(.*)$^.*$Next read date'
+        wg_rate_class_regex = r'Rate Class:\s+Meter number:\s+([^\n]+).*Next read date'
+        tf = TextExtractor.TextField
+        e1.fields = [
+            TextExtractor.TextField(regex=wg_start_regex, type=Field.DATE,
+                                    applier_key=UtilBillApplier.START),
+            TextExtractor.TextField(regex=wg_end_regex, type=Field.DATE,
+                                    applier_key=UtilBillApplier.END),
+            TextExtractor.TextField(regex=wg_energy_regex, type=Field.FLOAT,
+                                    applier_key=UtilBillApplier.ENERGY),
+            TextExtractor.TextField(regex=wg_next_meter_read_regex,
+                                    type=Field.DATE,
+                                    applier_key=UtilBillApplier.NEXT_READ),
+            TextExtractor.TextField(regex=wg_charges_regex,
+                                    type=Field.WG_CHARGES,
+                                    applier_key=UtilBillApplier.CHARGES),
+            TextExtractor.TextField(regex=wg_rate_class_regex,
+                                    type=Field.STRING,
+                                    applier_key=UtilBillApplier.RATE_CLASS),
+        ]
+
+        e2 = TextExtractor(name='Another')
+        Session().add_all([self.bill, e1, e2])
+        self.e1, self.e2 = e1, e2
+
+    def tearDown(self):
+        clear_db()
+
+    def test_extract_real_bill(self):
+        Main(self.bfh).extract(self.bill)
+
+        self.assertEqual(date(2014, 3, 19), self.bill.period_start)
+        self.assertEqual(date(2014, 4, 16), self.bill.period_end)
+        self.assertEqual(504.6, self.bill.get_total_energy())
+        self.assertEqual(date(2014, 5, 15),
+                         self.bill.get_next_meter_read_date())
+        D, S = Charge.DISTRIBUTION, Charge.SUPPLY
+        # Charges are temporarily disabled, until a better way to disable
+        # specific fields is put into release 30
+
+        expected = [
+             Charge('DISTRIBUTION_CHARGE', name='Distribution Charge',
+                    target_total=158.7, type=D, unit='therms'),
+             Charge('CUSTOMER_CHARGE', name='Customer Charge', target_total=14.0,
+                    type=D, unit='therms'),
+             Charge('PGC', name='PGC', target_total=417.91, type=S,
+                    unit='therms'),
+             Charge('PEAK_USAGE_CHARGE', name='Peak Usage Charge',
+                    target_total=15.79, type=D, unit='therms'),
+             Charge('RIGHT_OF_WAY', name='DC Rights-of-Way Fee',
+                    target_total=13.42, type=D, unit='therms'),
+             Charge('SETF', name='Sustainable Energy Trust Fund',
+                    target_total=7.06, type=D, unit='therms'),
+             Charge('EATF', name='Energy Assistance Trust Fund',
+                    target_total=3.03, type=D, unit='therms'),
+             Charge('DELIVERY_TAX', name='Delivery Tax', target_total=39.24,
+                    type=D, unit='therms'),
+             Charge('SALES_TAX', name='Sales Tax', target_total=38.48, type=D,
+                    unit='therms')]
+        
+        self.assertEqual(len(expected), len(self.bill.charges))
+        for expected_charge, actual_charge in zip(expected, self.bill.charges):
+            self.assertEqual(expected_charge.rsi_binding,
+                             actual_charge.rsi_binding)
+            self.assertEqual(expected_charge.name, actual_charge.name)
+            self.assertEqual(expected_charge.rate, actual_charge.rate)
+            self.assertEqual(expected_charge.target_total,
+                             actual_charge.target_total)
+
+        # TODO: this seems to fail with "" as the rate class name, only when
+        # run as part of the whole test_extraction module or larger unit,
+        # not when run by itself
+        # self.assertEqual('Commercial and Industrial Non-heating/Non-cooling',
+        #                  self.bill.get_rate_class_name())
+        self.assertIsInstance(self.bill.date_extracted, datetime)
+
+    @skip(
+        "Broken: when a Field is deleted, the before_update method is called, "
+        "but the 'extractor' attribute there is None, and before_delete is "
+        "not called")
+    def test_created_modified(self):
+        self.assertIsNone(self.e1.created)
+        self.assertIsNone(self.e1.modified)
+
+        s = Session()
+        s.add(self.e1)
+        s.add_all(self.e1.fields)
+        s.flush()
+        last_modified = self.e1.modified
+        self.assertIsNotNone(self.e1.created)
+        self.assertIsNotNone(last_modified)
+        self.assertLessEqual(self.e1.created, last_modified)
+
+        # changing a Field updates the modification date of the Extractor
+        self.e1.fields[0].regex = 'something else'
+        s.flush()
+        self.assertGreater(self.e1.modified, last_modified)
+        last_modified = self.e1.modified
+
+        # adding a field
+        last_modified = self.e1.modified
+        self.e1.fields.append(TextExtractor.TextField(regex='a'))
+        s.flush()
+        self.assertGreater(self.e1.modified, last_modified)
+        last_modified = self.e1.modified
+
+        # deleting a field
+        # TODO this doesn't work
+        del self.e1.fields[-1]
+        s.flush()
+        self.assertGreater(self.e1.modified, last_modified)
+
+    @skip('this task was deleted, might come back')
+    def test_test_extractor(self):
+        # TODO: it might be possible to write this as a unit test, without the
+        # database. database queries in tasks would be moved to a DAO like
+        # UtilbillLoader, which could be mocked.
+
+        # do everything in memory without requiring real celery server
+        from core import celery
+        celery.conf.update(
+            dict(BROKER_BACKEND='memory', CELERY_ALWAYS_EAGER=True))
+
+        # primary keys need to be set so they can be queried. also,
+        # the transaction needs to be committed because the task is a
+        # separate thread so it has a different transaction
+        Session().commit()
+        # TODO: session is gone after committing here, so we would have to
+        # create a new session and re-load all the objects that are used in
+        # the assertions below.
+
+        result = test_extractor.apply(args=[self.e1.extractor_id])
+        metadata = AsyncResult(result.task_id).info
+        self.assertEqual((1, 1, 1), result.get())
+        self.assertEqual({'all_count': 1, 'any_count': 1, 'total_count': 1},
+            metadata)
+
+        result = test_extractor.apply(args=[self.e2.extractor_id])
+        metadata = AsyncResult(result.task_id).info
+        self.assertEqual((0, 0, 1), result.get())
+        self.assertEqual({'all_count': 0, 'any_count': 0, 'total_count': 1},
+                         metadata)
