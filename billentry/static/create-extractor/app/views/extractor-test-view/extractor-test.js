b6e2c3b647a51fdafee688b0f3ff78f86bbefee9
Raphael Kargon rkargon <Raphael Kargon rkargon@nextility.com>
2015-08-05 17:48:28 -0400

    Started work on test view in extractor-ui, created mockup of basic layout

--- billentry/static/create-extractor/app/views/extractor-test-view/extractor-test.js
+++ billentry/static/create-extractor/app/views/extractor-test-view/extractor-test.js
@@ -2,6 +2,23 @@
 
 angular.module('createExtractor').
 
-controller('extractorTestViewCtrl', ['$scope', function($scope) {
+controller('extractorTestViewCtrl', ['$scope', 'DBService', 'dataModel', function($scope, DBService, dataModel) {
+	$scope.extractor = dataModel.extractor;
+	$scope.applier_keys = dataModel.applier_keys;
+	$scope.field_types = dataModel.field_types;
+	$scope.data_types = dataModel.data_types;
+	$scope.utilities = dataModel.utilities;
+
+	$scope.newExtractor = dataModel.newExtractor;
+	$scope.saveExtractor = dataModel.saveExtractor;
+	$scope.loadExtractor = dataModel.loadExtractor;
+
+	// the template for creating new tests. 
+	// This template is modified by the UI, and when the user starts a test the template's variables are used as parameters.
+	$scope.test_template = {};
+
+	$scope.tests = [];
+
 	
+
 }]);
