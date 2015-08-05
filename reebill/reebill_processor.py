65fd30330f14f873e7f46b064e7ad8909664be07
Raza Gilani <rgilani@skylineinnovations.com>
2015-08-05 18:01:19 -0400

    fixed the issue of a utility_account with no reebill_customer

--- reebill/reebill_processor.py
+++ reebill/reebill_processor.py
@@ -716,21 +716,30 @@ class ReebillProcessor(object):
 
     def set_payee_for_utility_account(self, account_id, payee):
         s = Session()
-        customer = s.query(ReeBillCustomer).filter_by(
+        try:
+            customer = s.query(ReeBillCustomer).filter_by(
             utility_account_id=account_id).one()
-        customer.payee = payee
+            customer.payee = payee
+        except NoResultFound:
+            return
 
     def get_payee_for_utility_account(self, account_id):
         s = Session()
-        customer = s.query(ReeBillCustomer).filter_by(
-            utility_account_id=account_id).one()
+        try:
+            customer = s.query(ReeBillCustomer).filter_by(
+                utility_account_id=account_id).one()
+        except NoResultFound:
+            return None
         return customer.get_payee()
 
 
     def set_groups_for_utility_account(self, account_id, group_name_array):
         s = Session()
-        customer = s.query(ReeBillCustomer).filter_by(
-            utility_account_id=account_id).one()
+        try:
+            customer = s.query(ReeBillCustomer).filter_by(
+                utility_account_id=account_id).one()
+        except NoResultFound:
+            return
 
         # Remove the customer from groups whose names are not in
         # 'group_name_array'
