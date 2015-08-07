40da05916da21322afd49a56edae1c3e9ffa9ac6
? <dklothe@skylineinnovations.com>
2015-08-07 13:13:28 -0400

    enable extraction in UtilbillProcessor._set_utilbill_data

---
+++ reebill/ui/static/app/controller/Accounts.js
@@ -0,0 +1,243 @@
+Ext.define('ReeBill.controller.Accounts', {
+    extend: 'Ext.app.Controller',
+
+    stores: [
+        'Accounts',
+        'AccountsFilter',
+        'Preferences'
+    ],
+
+    views:[
+        'accounts.Accounts',
+        'accounts.AccountForm',
+    ],
+
+    refs: [{
+        ref: 'accountForm',
+        selector: 'accountForm'
+    },{
+        ref: 'accountsGrid',
+        selector: 'grid[id=accountsGrid]'
+    },{
+        ref: 'accountsFilter',
+        selector: 'combo[name=accountsFilter]'
+    },{
+        ref: 'mergeBtn',
+        selector: 'button#mergeAccountRecord'
+    }],
+    
+    init: function() {
+        this.application.on({
+            scope: this
+        });
+        
+        this.control({
+            'panel[name=accountsTab]': {
+                activate: this.handleActivate
+            },
+            'accountForm': {
+                expand: this.loadNextAccountNumber
+            },
+            'grid[id=accountsGrid]': {
+                selectionchange: this.handleAccountSelect
+            },
+            'button[action=saveNewAccount]': {
+                click: this.saveNewAccount
+            },
+            'combo[name=accountsFilter]': {
+                change: this.handleFilter
+            },
+            '[action=mergeRecords]': {
+                click: this.handleMerge
+            }
+        });
+
+        this.getAccountsStore().on({
+            beforeload: function(store){
+                if(this.getAccountsGrid) {
+                    var grid = this.getAccountsGrid();
+                    grid.setLoading(true);
+                }
+            },
+            load: function(store, records, successful, eOpts ){
+                if(this.getAccountsGrid) {
+                    var grid = this.getAccountsGrid();
+                    grid.setLoading(false);
+                }
+            },
+            scope: this
+        });
+
+        // Set up sorting & filtering
+        this.getPreferencesStore().on({
+            load: function(store, records, successful, eOpts ){
+                // Update the store for sorts
+                var memStore = this.getAccountsStore();
+                var sortColumn = store.getOrCreate('default_account_sort_field', 'account').get('value');
+                var sortDir = store.getOrCreate('default_account_sort_direction', 'DESC').get('value');
+
+                memStore.sort({property: sortColumn, direction: sortDir});
+                memStore.loadPage(1);
+            },
+            scope: this
+        });
+
+        this.getAccountsStore().on({
+            beforeload: function(store, operation, eOpts){
+                var prefStore = this.getPreferencesStore();
+                var accountsStore = this.getAccountsStore();
+                if(prefStore.count()!=0 && accountsStore.count()!=0){
+                    // Filter
+                    var filterStore = this.getAccountsFilterStore();
+                    var allRecords = accountsStore.getRange();
+
+                    // Find the correct filter record
+                    var filterRecord = prefStore.getOrCreate('filtername', 'none');
+                    var filter = filterStore.findRecord('value', filterRecord.get('value'));
+                    if(filter == null){
+                        filterRecord.set('value', 'none');
+                        filter = filterStore.findRecord('value', 'none');
+                    }
+                    
+                    // apply the filter
+                    store.getProxy().data = Ext.Array.filter(allRecords, filter.get('filter').filterFn);
+                    this.initFilterCombo();
+                }
+            },
+            load: function(store, records, successful, eOpts ){
+                var prefStore = this.getPreferencesStore();
+                if(prefStore.count()!=0 && store.sorters.items.length){
+                    // Sort
+                    prefStore.setOrCreate('default_account_sort_field', store.sorters.items[0].property);
+                    prefStore.setOrCreate('default_account_sort_direction', store.sorters.items[0].direction);
+                }
+            },
+            scope: this
+        });
+    },
+
+    /**
+     * Handle the panel being activated.
+     */
+    handleActivate: function() {
+    },
+
+    /**
+     * Select the filter from preference store
+     */
+    initFilterCombo: function(){
+        var store = this.getPreferencesStore();
+        var filterCombo = this.getAccountsFilter();
+        if(store.count()!=0 && filterCombo){
+            var filterPrefRec = store.findRecord('key', 'filtername');
+            if(filterPrefRec){
+                filterCombo.setValue(filterPrefRec.get('value'));
+            }
+        }
+    },
+
+    /**
+     * Handle the filter being changed.
+     */
+    handleFilter: function( combo, newValue, oldValue, eOpts) {
+        // We're filtering every record, so we have to use AccountsStore
+        // and not AccountsMemoryStore
+        var memStore = this.getAccountsStore();
+        var prefStore = this.getPreferencesStore();
+        if(memStore.count()!=0 && prefStore.count()!=0 && newValue){
+            var rec= prefStore.findRecord('key', 'filtername');
+            rec.set('value', newValue);
+            memStore.loadPage(1);
+        }
+    },
+
+    /**
+     * Get the next account number and add it populate the new account numnber field
+     */
+    loadNextAccountNumber: function() {
+        var newAccountField = this.getAccountForm().down('textfield[name=account]');
+        var store = this.getAccountsStore();
+
+        newAccountField.setValue(store.getNextAccountNumber());
+    },
+
+    /**
+     * Save the new account.
+     */
+    saveNewAccount: function() {
+        var accountForm = this.getAccountForm(),
+            accountsGrid = this.getAccountsGrid(),
+            makeAnotherAccount = accountForm.down('[name=makeAnotherAccount]').checked;
+        var store = this.getAccountsStore();
+
+        if (accountForm.getForm().isValid()) {
+            var values = accountForm.getForm().getValues();
+            store.suspendAutoSync();
+            store.add(values);
+            if (!makeAnotherAccount) {
+                store.sync({
+                    success: function (batch, options) {
+                        var filter = this.getAccountsFilter().getValue();
+                        var filterStore = this.getAccountsFilterStore();
+                        var filterRec = filterStore.findRecord('value', filter);
+                        var accountRec = batch.operations[0].records[0];
+
+                        // Test if the current filter would filter out the newly
+                        // created account and if yes, set the filter to none
+                        if(!filterRec.get('filter').filterFn(accountRec)){
+                            var noneFilter = filterStore.findRecord('value', 'none');
+                            var filterCombo = this.getAccountsFilter();
+                            filterCombo.select(noneFilter);
+                        }
+
+                        store.sort({
+                            property: 'account',
+                            direction: 'DESC'
+                        });
+                        store.loadPage(1);
+                        accountsGrid.getSelectionModel().select([accountRec]);
+
+                        var accountForm = this.getAccountForm();
+                        accountForm.getForm().reset();
+                    },
+                    callback: function(){
+                        store.resumeAutoSync();
+                    },
+                    scope: this
+                });
+                accountsGrid.expand();
+            }else {
+                this.loadNextAccountNumber();
+                store.sync({
+                    callback: function(){
+                        store.resumeAutoSync();
+                    },
+                    scope: this
+                });
+            }
+        }
+    },
+
+    /**
+     * Handle the account selection.
+     */
+    handleAccountSelect: function() {
+        var selected = this.getAccountsGrid().getSelectionModel().getSelection();
+        this.getMergeBtn().setDisabled(!(selected.length == 2));
+    },
+
+    /**
+     * handle merge records button
+     */
+    handleMerge: function () {
+        var records = this.getAccountsGrid().getSelectionModel().getSelection();
+        Ext.create('ReeBill.view.accounts.MergeDialog', {
+            records: records,
+            basedOn: 'ReeBill.view.accounts.Accounts',
+            store: this.getAccountsStore(),
+            exclude: ['casualname', 'primusname', 'codename', 'lastevent',
+            'service_type', 'template_account']
+        }).show();
+    }
+
+});
---
+++ test/test_reebill/test_excel_export.py
@@ -0,0 +1,384 @@
+from copy import deepcopy
+from datetime import date, datetime
+from itertools import cycle
+from StringIO import StringIO
+import unittest
+
+import mock
+
+from core import init_config, init_model
+from core.model import Register, Charge, Session, Utility, Address,\
+    Supplier, RateClass, UtilityAccount
+from core.model.utilbill import UtilBill, Charge
+from reebill.reports.excel_export import Exporter
+from reebill.reebill_model import ReeBill, Payment
+from reebill.reebill_dao import ReeBillDAO
+from reebill.payment_dao import PaymentDAO
+from test import create_tables
+
+
+class ExporterSheetTest(unittest.TestCase):
+
+    def setUp(self):
+        #Set up the mock
+        self.mock_StateDB = mock.create_autospec(ReeBillDAO)
+        self.payment_dao = mock.Mock(autospec=PaymentDAO)
+        self.exp = Exporter(self.mock_StateDB, self.payment_dao)
+
+    def test_get_reebill_details_dataset(self):
+
+        def make_reebill(id, month):
+            result = mock.Mock(autospec=ReeBill)
+            result.id = id
+            result.sequence = 1
+            result.version = 0
+            result.issued = 1
+            result.utilbills = [mock.Mock(autospec=UtilBill)]
+            result.billing_address = 'Monroe Towers, Silver Spring, MD'
+            result.service_address = 'Monroe Towers, Silver Spring, MD'
+            result.issue_date = date(2013, 4, 1)
+            result.ree_value = 4.3
+            result.prior_balance = 2.20
+            result.payment_received = None
+            result.total_adjustment = 0.00
+            result.balance_forward = 62.29
+            result.ree_charge = 122.20
+            result.late_charge = 32.2
+            result.balance_due = 5.01
+
+            result.get_period.return_value = (
+                date(2011, month, 3), date(2011, month+1, 2))
+            result.get_total_actual_charges.return_value = 743.49
+            result.get_total_hypothetical_charges.return_value = 980.33
+            result.get_total_renewable_energy.return_value = 188.20
+
+            return result
+
+        def make_payment(date_applied, amount):
+            result = mock.Mock(autospec=Payment)
+            result.date_applied = date_applied
+            result.credit = amount
+            return result
+
+        def get_payments_for_reebill_id(id):
+            if id == 1:
+                return [
+                    make_payment(datetime(2011, 1, 30, 0, 0, 0), 400.13),  # '10003'
+                    make_payment(datetime(2011, 2, 01, 0, 0, 0), 13.37)
+                ]
+            else:
+                return []
+
+        self.payment_dao.get_payments_for_reebill_id.side_effect = \
+            get_payments_for_reebill_id
+        self.mock_StateDB.get_all_reebills_for_account.side_effect = cycle([
+            [make_reebill(1, 1)],   # For account '10003'
+            [make_reebill(2, 2), make_reebill(3, 3), make_reebill(4, 4)] # 10004
+        ])
+
+        # No start or end date
+        dataset = self.exp.get_export_reebill_details_dataset(
+            ['10003', '10004'])
+        correct_data = [
+            ('10003', 1, 0, u'Monroe Towers, Silver Spring, MD',
+             u'Monroe Towers, Silver Spring, MD', '2013-04-01',
+             '2011-01-03', '2011-02-02', '980.33', '743.49', '4.30',
+             '2.20', None, '2011-01-30', '400.13', '0.00', '62.29',
+             '122.20', 32.2, '5.01', '', '-117.90', '-117.90',
+             '188.20', '1.26'),
+            ('10003', 1, 0, None, None, None, None, None, None, None,
+             None, None, None, '2011-02-01', '13.37', None, None,
+             None, None, None, None, None, None, None, None),
+            ('10004', 1, 0, u'Monroe Towers, Silver Spring, MD',
+             u'Monroe Towers, Silver Spring, MD', '2013-04-01',
+             '2011-02-03', '2011-03-02', '980.33', '743.49', '4.30',
+             '2.20', None, None, None, '0.00', '62.29', '122.20',
+             32.2, '5.01', '', '-117.90', '-117.90', '188.20',
+             '1.26'),
+            ('10004', 1, 0, u'Monroe Towers, Silver Spring, MD',
+             u'Monroe Towers, Silver Spring, MD', '2013-04-01',
+             '2011-03-03', '2011-04-02', '980.33', '743.49', '4.30',
+             '2.20', None, None, None, '0.00', '62.29', '122.20',
+             32.2, '5.01', '', '-117.90', '-235.80', '188.20',
+             '1.26'),
+            ('10004', 1, 0, u'Monroe Towers, Silver Spring, MD',
+             u'Monroe Towers, Silver Spring, MD', '2013-04-01',
+             '2011-04-03', '2011-05-02', '980.33', '743.49', '4.30',
+             '2.20', None, None, None, '0.00', '62.29', '122.20',
+             32.2, '5.01', '', '-117.90', '-353.70', '188.20',
+             '1.26')
+        ]
+        for indx,row in enumerate(dataset):
+            self.assertEqual(row, correct_data[indx])
+        self.assertEqual(len(dataset), len(correct_data))
+
+        # Only start date
+        dataset = self.exp.get_export_reebill_details_dataset(
+            ['10003', '10004'], begin_date=date(2011, 4, 1))
+        correct_data = [
+            ('10004', 1, 0, u'Monroe Towers, Silver Spring, MD',
+             u'Monroe Towers, Silver Spring, MD', '2013-04-01',
+             '2011-04-03', '2011-05-02', '980.33', '743.49', '4.30',
+             '2.20', None, None, None, '0.00', '62.29', '122.20',
+             32.2, '5.01', '', '-117.90',  '-117.90', '188.20',
+             '1.26')
+        ]
+        for indx,row in enumerate(dataset):
+            self.assertEqual(row, correct_data[indx])
+        self.assertEqual(len(dataset), len(correct_data))
+
+        # Only end date
+        dataset = self.exp.get_export_reebill_details_dataset(
+            ['10003', '10004'], end_date=date(2011, 3, 5))
+        correct_data = [
+            ('10003', 1, 0, u'Monroe Towers, Silver Spring, MD',
+             u'Monroe Towers, Silver Spring, MD', '2013-04-01',
+             '2011-01-03', '2011-02-02', '980.33', '743.49', '4.30',
+             '2.20', None, '2011-01-30', '400.13', '0.00', '62.29',
+             '122.20', 32.2, '5.01', '', '-117.90', '-117.90',
+             '188.20', '1.26'),
+            ('10003', 1, 0, None, None, None, None, None, None, None,
+             None, None, None, '2011-02-01', '13.37', None, None,
+             None, None, None, None, None, None, None, None),
+            ('10004', 1, 0, u'Monroe Towers, Silver Spring, MD',
+             u'Monroe Towers, Silver Spring, MD', '2013-04-01',
+             '2011-02-03', '2011-03-02', '980.33', '743.49', '4.30',
+             '2.20', None, None, None, '0.00', '62.29', '122.20',
+             32.2, '5.01', '', '-117.90', '-117.90', '188.20',
+             '1.26')
+        ]
+        for indx,row in enumerate(dataset):
+            self.assertEqual(row, correct_data[indx])
+        self.assertEqual(len(dataset), len(correct_data))
+
+        # Start and end date
+        dataset = self.exp.get_export_reebill_details_dataset(
+            ['10003', '10004'], begin_date=date(2011, 2, 1),
+            end_date=date(2011, 3, 5))
+        correct_data = [
+            ('10004', 1, 0, u'Monroe Towers, Silver Spring, MD',
+             u'Monroe Towers, Silver Spring, MD', '2013-04-01',
+             '2011-02-03', '2011-03-02', '980.33', '743.49', '4.30',
+             '2.20', None, None, None, '0.00', '62.29', '122.20',
+             32.2, '5.01', '', '-117.90', '-117.90', '188.20',
+             '1.26')
+        ]
+        for indx,row in enumerate(dataset):
+            self.assertEqual(row, correct_data[indx])
+        self.assertEqual(len(dataset), len(correct_data))
+
+    def test_account_charges_sheet(self):
+        def make_utilbill(month):
+            result = mock.Mock(autospec=UtilBill)
+            result.period_start = datetime(2013, month, 3)
+            result.period_end = datetime(2013, month+1, 4)
+            result.state = UtilBill.Complete
+            return result
+
+        def make_charge(group, desc, number):
+            result = mock.Mock(autospec=Charge)
+            result.total = number
+            result.group = group
+            result.description = desc
+            return result
+
+        def make_reebill(seq):
+            result = mock.Mock(autospec=ReeBill)
+            result.sequence = seq
+            ub = make_utilbill(seq)
+            result.utilbills = [ub]
+            result.utilbill = ub
+            return result
+
+        r1 = make_reebill(1)
+        r1.utilbill.charges = [make_charge(x,y,z) for x,y,z in [
+            ('Group1', "Description1", 1.11),
+            ('Group1', "Description2", 2.22),
+            ('Group2', "Description3", 3.33),
+        ]]
+        r2 = make_reebill(2)
+        r2.utilbill.charges = [make_charge(x,y,z) for x,y,z in [
+            ('Group1', "Description1", 4.44),
+            ('Group2', "Description2", 5.55),
+            ('Group2', "Description3", 6.66),
+        ]]
+        r3 = make_reebill(3)
+        r3.utilbill.charges = [make_charge(x,y,z) for x,y,z in [
+            ('Group1', "Description1", 4.44),
+            ('Group2', "Description2", 5.55),
+            ('Group2', "Description3", 6.66),
+        ]]
+
+        # No start date or end date
+        dataset = self.exp.get_account_charges_sheet('999999', [r1, r2, r3])
+        correct_data = [('999999', 1, '2013-01-03', '2013-02-04', '2013-01',
+                         'No', '1.11', '2.22', '3.33', ''),
+                        ('999999', 2, '2013-02-03', '2013-03-04', '2013-02',
+                         'No', '4.44', '', '6.66', '5.55'),
+                        ('999999', 3, '2013-03-03', '2013-04-04', '2013-03',
+                         'No', '4.44', '', '6.66', '5.55')]
+        headers = ['Account', 'Sequence', 'Period Start', 'Period End',
+                   'Billing Month', 'Estimated', 'Group1: Description1',
+                   'Group1: Description2', 'Group2: Description3',
+                   'Group2: Description2']
+        self.assertEqual(headers, dataset.headers)
+        for indx, row in enumerate(dataset):
+            self.assertEqual(row, correct_data[indx])
+        self.assertEqual(len(dataset), len(correct_data))
+        # Only start date
+        dataset = self.exp.get_account_charges_sheet(
+            '999999', [r1, r2, r3], start_date=datetime(2013, 2, 1))
+        correct_data = [('999999', 2, '2013-02-03', '2013-03-04', '2013-02',
+                         'No', '4.44', '5.55', '6.66'),
+                        ('999999', 3, '2013-03-03', '2013-04-04', '2013-03',
+                         'No', '4.44', '5.55', '6.66')]
+        headers = ['Account', 'Sequence', 'Period Start', 'Period End',
+                   'Billing Month', 'Estimated', 'Group1: Description1',
+                   'Group2: Description2', 'Group2: Description3']
+        self.assertEqual(headers, dataset.headers)
+        for indx, row in enumerate(dataset):
+            self.assertEqual(row, correct_data[indx])
+        self.assertEqual(len(dataset), len(correct_data))
+        # Only end date
+        dataset = self.exp.get_account_charges_sheet(
+            '999999', [r1, r2, r3], end_date=datetime(2013, 3, 5))
+        correct_data = [('999999', 1, '2013-01-03', '2013-02-04', '2013-01',
+                         'No', '1.11', '2.22', '3.33', ''),
+                        ('999999', 2, '2013-02-03', '2013-03-04', '2013-02',
+                         'No', '4.44', '', '6.66', '5.55')]
+        headers = ['Account', 'Sequence', 'Period Start', 'Period End',
+                   'Billing Month', 'Estimated', 'Group1: Description1',
+                   'Group1: Description2', 'Group2: Description3',
+                   'Group2: Description2']
+        self.assertEqual(headers, dataset.headers)
+        for indx, row in enumerate(dataset):
+            self.assertEqual(row, correct_data[indx])
+        self.assertEqual(len(dataset), len(correct_data))
+        # Both start and end date
+        dataset = self.exp.get_account_charges_sheet(
+            '999999', [r1, r2], start_date=datetime(2013, 2, 1),
+            end_date=datetime(2013, 3, 5))
+        correct_data = [('999999', 2, '2013-02-03', '2013-03-04', '2013-02',
+                         'No', '4.44', '5.55', '6.66')]
+        headers = ['Account', 'Sequence', 'Period Start', 'Period End',
+                   'Billing Month', 'Estimated', 'Group1: Description1',
+                   'Group2: Description2', 'Group2: Description3']
+        self.assertEqual(headers, dataset.headers)
+        for indx, row in enumerate(dataset):
+            self.assertEqual(row, correct_data[indx])
+        self.assertEqual(len(dataset), len(correct_data))
+
+    def test_get_energy_usage_sheet(self):
+        def make_charge(number):
+            result = mock.Mock(autospec=Charge)
+            result.total = number
+            result.group = str(number)
+            result.description = ''
+            return result
+
+        #Setup Mock
+        u1 = mock.Mock(autospec=UtilBill)
+        u1.utility_account.account = '10003'
+        u1.rate_class.name = 'DC Non Residential Non Heat'
+        u1.period_start = date(2011,11,12)
+        u1.period_end = date(2011,12,14)
+        u1.charges = [make_charge(x) for x in [3.37, 17.19, 43.7, 164.92,
+                23.14, 430.02, 42.08, 7.87, 11.2]]
+        # replacement for document above
+        register1 = mock.Mock(autospec=Register)
+        register1.description = ''
+        register1.quantity = 561.9
+        register1.unit = 'therms'
+        register1.estimated = False
+        register1.reg_type = 'total'
+        register1.register_binding = Register.TOTAL
+        register1.active_periods = None
+        u1._registers = [register1]
+        u2 = deepcopy(u1)
+        u2.period_start = date(2011,12,15)
+        u2.period_end = date(2012,01,14)
+        u2._registers = [deepcopy(register1)]
+
+        dataset = self.exp.get_energy_usage_sheet([u1, u2])
+        correct_data = [('10003', u'DC Non Residential Non Heat', 561.9, u'therms', '2011-11-12', '2011-12-14', 3.37, 17.19, 43.7, 164.92, 23.14, 430.02, 42.08, 7.87, 11.2),
+            ('10003', u'DC Non Residential Non Heat', 561.9, u'therms', '2011-12-15', '2012-01-14', 3.37, 17.19, 43.7, 164.92, 23.14, 430.02, 42.08, 7.87, 11.2),]
+        headers = ['Account', 'Rate Class', 'Total Energy', 'Units',
+                'Period Start', 'Period End', '3.37: ', '17.19: ', '43.7: ',
+                '164.92: ', '23.14: ', '430.02: ', '42.08: ', '7.87: ',
+                '11.2: ']
+        self.assertEqual(headers, dataset.headers)
+        for indx,row in enumerate(dataset):
+            self.assertEqual(row, correct_data[indx])
+        self.assertEqual(len(dataset), len(correct_data))
+
+
+class ExporterDataBookTest(unittest.TestCase):
+
+    def setUp(self):
+        init_config('test/tstsettings.cfg')
+        create_tables()
+        init_model()
+
+        self.exp = Exporter(ReeBillDAO(), PaymentDAO())
+
+        s = Session()
+        utility = Utility(name='New Utility', address=Address())
+        s.add(utility)
+
+        supplier = Supplier(name='New Supplier', address=Address())
+        s.add(supplier)
+
+        rate_class = RateClass(name='New Rate Class', utility=utility,
+                               service='electric')
+        s.add(rate_class)
+
+        utility_account = UtilityAccount(
+                'some name',
+                '20001',
+                utility,
+                supplier,
+                rate_class,
+                Address(),
+                Address(),
+                '1234567890'
+        )
+        s.add(utility_account)
+
+        s.add(
+            UtilBill(
+                utility_account, utility,
+                rate_class, supplier=supplier,
+                period_start=date(2010, 11, 1), period_end=date(2011, 2, 3),
+                date_received=datetime.utcnow().date(),
+                state=UtilBill.Estimated,
+            )
+        )
+
+    def test_exports_returning_binaries(self):
+        """
+        This test simply calls all export functions returning binaries. This
+        way we can at least verify that the code in those functions is
+        syntactically correct and calls existing methods
+        """
+
+        string_io = StringIO()
+
+        # export_account_charges
+        self.exp.export_account_charges(string_io)
+        self.exp.export_account_charges(string_io, '20001')
+        self.exp.export_account_charges(string_io, '20001', date(2010, 11, 1))
+        self.exp.export_account_charges(string_io, '20001',
+                                        date(2010, 11, 1), date(2011, 2, 3))
+
+        # export_energy_usage
+        self.exp.export_energy_usage(string_io)
+        the_account = Session().query(UtilityAccount).filter_by(
+            account='20001').one()
+        self.exp.export_energy_usage(string_io, the_account)
+
+        # export_reebill_details
+        self.exp.export_reebill_details(string_io)
+        self.exp.export_reebill_details(string_io, '20001')
+        self.exp.export_reebill_details(string_io, '20001', date(2010, 11, 1))
+        self.exp.export_reebill_details(string_io, '20001',
+                                        date(2010, 11, 1), date(2011, 2, 3))
