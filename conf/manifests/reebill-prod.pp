0fcac4d6133346bd9a7be7a63395d1fb267e47ec
Raza Gilani <rgilani@skylineinnovations.com>
2015-08-24 12:27:59 -0400

    updated cron entry that runs the billing export

--- conf/manifests/reebill-prod.pp
+++ conf/manifests/reebill-prod.pp
@@ -109,5 +109,6 @@ cron { read_quote_files:
 cron { export_accounts_data:
     command => "source /home/reebill-prod/.bash_profile && cd /var/local/reebill-prod/billing/bin && python export_accounts_to_xls.py -f /home/skyline-etl-prod/Dropbox/skyline-etl/reebill_accounts_export.xls  2> /home/reebill-prod/logs/export_accounts_to_xls_stderr.log",
     user => $username,
+    hour => 1,
     minute => 0
 }
