840320dc40befd41fb199534e0ad3bada34c6d8c
? <dklothe@skylineinnovations.com>
2015-08-27 14:48:49 -0400

    fix example file paths in test/utils/test_email.py to be independent of current directory

--- test/utils/test_email.py
+++ test/utils/test_email.py
@@ -1,5 +1,8 @@
 import email
 from unittest import TestCase
+from os.path import join
+
+from core import ROOT_PATH
 from util.email_util import get_attachments
 
 
@@ -9,9 +12,10 @@ class EmailUtilsTest(TestCase):
     EMAIL_NO_ATTACHMENT_PATH = 'example_email_no_attachment.txt'
 
     def setUp(self):
-        with open(self.EMAIL_WITH_ATTACHMENT_PATH) as email_file:
+        dir = join(ROOT_PATH, 'test', 'utils')
+        with open(join(dir, self.EMAIL_WITH_ATTACHMENT_PATH)) as email_file:
             self.message_with_attachment = email.message_from_file(email_file)
-        with open(self.EMAIL_NO_ATTACHMENT_PATH) as email_file:
+        with open(join(dir, self.EMAIL_NO_ATTACHMENT_PATH)) as email_file:
             self.message_no_attachment = email.message_from_file(email_file)
 
     def test_get_attachments_1(self):
