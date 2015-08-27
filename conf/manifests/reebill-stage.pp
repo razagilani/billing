fd16fbed0dc96103fe1241006998d600a2d7e5a6
? <dklothe@skylineinnovations.com>
2015-08-27 12:43:13 -0400

    remove crontab entry for receive_matrix_files.py since this is no longer run by cron

--- conf/manifests/reebill-stage.pp
+++ conf/manifests/reebill-stage.pp
@@ -117,9 +117,3 @@ cron { read_quote_files:
     minute => '*/10'
 }
 
-cron { receive_matrix_files:
-    command => "source /var/local/reebill-stage/bin/activate && python /var/local/reebill-stage/billing/bin/receive_matrix_files.py"
-    user => $username,
-    hour => 0,
-    minute => 5
-}
