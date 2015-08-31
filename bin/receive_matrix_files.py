6b5675ee56c430e7627bdf93ed6577a45f62675e
? <dklothe@skylineinnovations.com>
2015-08-31 11:58:20 -0400

    copy receive_matrix_files.py to receive_matrix_email.py, restore original receive_matrix_files.py from default

--- bin/receive_matrix_files.py
+++ bin/receive_matrix_files.py
@@ -1,14 +1,9 @@
 """Receive quotes from suppliers' "matrix" spreadsheets (files from a Dropbox
 directory or email attachments).
 """
-import logging
 from fcntl import flock, LOCK_EX, LOCK_NB
-from sys import stdin
-import traceback
-from brokerage.quote_email_processor import QuoteEmailProcessor, LOG_NAME
-from brokerage.quote_email_processor import CLASSES_FOR_SUPPLIERS, QuoteDAO
 from core import initialize
-from core.model import AltitudeSession, Session
+from brokerage.quote_file_processor import QuoteFileProcessor
 
 if __name__ == '__main__':
     # prevent multiple instances from running at once
@@ -18,13 +13,5 @@ if __name__ == '__main__':
     except IOError:
         exit(1)
 
-    try:
-        initialize()
-        qep = QuoteEmailProcessor(CLASSES_FOR_SUPPLIERS, QuoteDAO())
-        qep.process_email(stdin)
-    except Exception as e:
-        logging.error('Error when processing email:\n%s' % (
-            traceback.format_exc()))
-    finally:
-        Session.remove()
-        AltitudeSession.remove()
+    initialize()
+    QuoteFileProcessor().run()
