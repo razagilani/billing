19850ca572f6b71eca3e90a6d2dd183e1037cd49
Jared Davenport <jdavenport@nextility.com>
2015-09-01 14:17:58 -0400

    Updating script for environmental strings.

--- conf/files/receive_matrix_files.sh
+++ conf/files/receive_matrix_files.sh
@@ -6,14 +6,17 @@
 # together with Postfix.  An email alias can be created to 
 # pipe incoming email to this script.
 
+#Set to "prod", "stage", or "dev", depending on your host.
+BILL_ENV="dev"
+
 #Receive input from stdin.
 input="$1"
 
 #Set up the python virtual environment
-VIRT_PYTHON="/var/local/reebill-dev/bin/python"
-VIRT_FILE="/var/local/reebill-dev/billing/bin/receive_matrix_email.py"
-export PYTHONPATH=/var/local/reebill-dev/billing:/var/local/reebill-dev/lib/python2.7/site-packages:/var/local/reebill-dev/lib/python2.7/dist-packages:
-source /var/local/reebill-dev/bin/activate
+VIRT_PYTHON="/var/local/reebill-$BILL_ENV/bin/python"
+VIRT_FILE="/var/local/reebill-$BILL_ENV/billing/bin/receive_matrix_email.py"
+export PYTHONPATH=/var/local/reebill-$BILL_ENV/billing:/var/local/reebill-$BILL_ENV/lib/python2.7/site-packages:/var/local/reebill-$BILL_ENV/lib/python2.7/dist-packages:
+source /var/local/reebill-$BILL_ENV/bin/activate
 
 #Output the contents of stdin to the python application
 cat $input | $VIRT_PYTHON $VIRT_FILE
