605a11322bb0aefe9e0f5854d16577579e7aa50f
? <dklothe@skylineinnovations.com>
2015-09-03 18:22:26 -0400

    constellation basically working, but needs a lot of cleanup. also changes to framework code

--- brokerage/quote_parsers.py
+++ brokerage/quote_parsers.py
@@ -190,12 +190,12 @@ class SpreadsheetReader(object):
         def get_neighbor_str():
             result = ''
             # clockwise: left up right down
-            for dir, nx, ny in [('up', x - 1, y), ('down', x, y - 1),
-                                ('left', x + 1, y), ('right', x, y + 1)]:
+            for dir, nx, ny in [('up', x, y - 1), ('down', x, y + 1),
+                                ('left', x - 1, y), ('right', x + 1, y)]:
                 try:
                     nvalue = self._get_cell(sheet, nx, ny)
                 except IndexError as e:
-                    nvalue = e
+                    nvalue = repr(e)
                 result += '%s: %s ' % (dir, nvalue)
             return result
 
@@ -796,13 +796,15 @@ class ConstellationMatrixParser(QuoteParser):
     FILE_FORMAT = formats.xlsx
 
     HEADER_ROW = 4
-    VOLUME_RANGE_ROW = 4
+    VOLUME_RANGE_ROW = 5
     QUOTE_START_ROW = 6
     STATE_COL = 'A'
     UTILITY_COL = 'B'
     TERM_COL = 'C'
-    PRICE_START_COL = 'D'
-    PRICE_END_COL = 'E'
+    VOLUME_RANGE_START_COL = 3
+    DATE_COL = 'K'
+    PRICE_START_COL = 3
+    PRICE_END_COL = 38
 
     EXPECTED_SHEET_TITLES = [
         'SMB Cost+ Matrix',
@@ -815,52 +817,74 @@ class ConstellationMatrixParser(QuoteParser):
         (0, 4, 'B', 'Utility'),
         (0, 4, 'C', 'Term'),
     ]
-    DATE_CELL = (0, 2, 'L', None)
+    DATE_CELL = (0, 2, DATE_COL, None)
 
     def _extract_volume_range(self, row, col):
-        # these cells are strings like like "75-149" where "149" really
-        # means < 150, so 1 is added to the 2nd number--unless it is the
-        # highest volume range, in which case the 2nd number really means
-        # what it says.
-        regex = r'(\d+)\s*-\s*(\d+)'
+        regex = r'(\d+)\s*-\s*(\d+)\s+MWh'
         low, high = self._reader.get_matches(0, row, col, regex, (float, float))
-        if col != self.PRICE_END_COL:
-            high += 1
+        # low value is really 1 higher than it should be in cases like
+        # "151-300" preceded by "0-150"
+        _assert_true(low == 0 or low % 10 == 1)
+        if low % 10 == 1:
+            low -= 1
         return low * 1000, high * 1000
 
     def _extract_quotes(self):
-        return
         volume_ranges = [self._extract_volume_range(self.VOLUME_RANGE_ROW, col)
                          for col in xrange(self.PRICE_START_COL,
                                            self.PRICE_END_COL + 1)]
-        # volume ranges should be contiguous
+        # # volume ranges should be contiguous or restarting at 0
         for i, vr in enumerate(volume_ranges[:-1]):
             next_vr = volume_ranges[i + 1]
-            _assert_equal(vr[1], next_vr[0])
+            if next_vr[0] != 0:
+                _assert_equal(vr[1], next_vr[0])
 
         for row in xrange(self.QUOTE_START_ROW, self._reader.get_height(0)):
-            # TODO use time zone here
-            start_from = excel_number_to_datetime(
-                self._reader.get(0, row, 0, (int, float)))
-            start_until = date_to_datetime((Month(start_from) + 1).first)
+            utility = self._reader.get(0, row, self.UTILITY_COL,
+                                       (basestring, type(None), datetime))
+            if utility is None:
+                continue
+            elif isinstance(utility, datetime):
+                # repeat of the top of the spreadsheet
+                _assert_equal('Fixed Fully Bundled',
+                              self._reader.get(0, row, 0, basestring))
+                _assert_equal('Small Business Cost+ Pricing',
+                              self._reader.get(0, row, 'I', basestring))
+                _assert_equal(self._date,
+                              self._reader.get(0, row + 1,
+                                               self.DATE_COL, datetime))
+                continue
+            elif utility == 'Utility':
+                # repeat of the header row
+                continue
             term_months = self._reader.get(0, row, self.TERM_COL, (int, float))
 
-            rate_class = self._reader.get(0, row, self.RATE_CLASS_COL,
-                                          basestring)
-            state = self._reader.get(0, row, self.STATE_COL,
-                                     basestring)
-            utility = self._reader.get(0, row, self.UTILITY_COL,
-                                       basestring)
-            rate_class_alias = '-'.join([state, utility, rate_class])
+            # there's no fine-grained classification of customers
+            rate_class_alias = utility
             rate_class_ids = self.get_rate_class_ids_for_alias(rate_class_alias)
 
-            special_options = self._reader.get(0, row, self.SPECIAL_OPTIONS_COL,
-                                               basestring)
-            _assert_true(special_options in ['', 'POR', 'UCB', 'RR'])
-
             for col in xrange(self.PRICE_START_COL, self.PRICE_END_COL + 1):
+                price = self._reader.get(0, row, col,
+                                         (int, float, type(None)))
+                if price == None:
+                    continue
+                if price < 0:
+                    # many cells that look blank in Excel actually have
+                    # negative prices, such as (35,27) (in tablib's numbering)
+                    # where the price is -0.995
+                    continue
+
+                # (term col + 1) + greatest multiple of 4 <= (col - term col - 1)
+                # least multiple of 4 < x is 4 * (x / 4)
+                # TODO: rename
+                # TODO use time zone here
+                start_from_col = self.VOLUME_RANGE_START_COL + (
+                    col - self.VOLUME_RANGE_START_COL) / 4 * 4
+                start_from = self._reader.get(0, self.HEADER_ROW,
+                                              start_from_col, datetime)
+                start_until = date_to_datetime((Month(start_from) + 1).first)
+
                 min_vol, max_vol = volume_ranges[col - self.PRICE_START_COL]
-                price = self._reader.get(0, row, col, (int, float)) / 1000.
                 for rate_class_id in rate_class_ids:
                     quote = MatrixQuote(
                         start_from=start_from, start_until=start_until,
@@ -868,8 +892,7 @@ class ConstellationMatrixParser(QuoteParser):
                         valid_until=self._date + timedelta(days=1),
                         min_volume=min_vol, limit_volume=max_vol,
                         rate_class_alias=rate_class_alias,
-                        purchase_of_receivables=(special_options == 'POR'),
-                        price=price)
+                        purchase_of_receivables=False, price=price)
                     # TODO: rate_class_id should be determined automatically
                     # by setting rate_class
                     if rate_class_id is not None:
