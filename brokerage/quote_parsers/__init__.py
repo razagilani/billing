58d6d3b828ef51f10bd3e005a85f7099090e4580
? <dklothe@skylineinnovations.com>
2015-09-04 15:51:55 -0400

    split quote parser classes into separate files

---
+++ brokerage/quote_parsers/__init__.py
@@ -0,0 +1,5 @@
+from .aep import AEPMatrixParser
+from .amerigreen import AmerigreenMatrixParser
+from .champion import ChampionMatrixParser
+from .direct_energy import DirectEnergyMatrixParser
+from .usge import USGEMatrixParser
