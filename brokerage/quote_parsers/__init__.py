8911f8452cb2a0026a2f1433ab01e5a1cf992a9f
? <dklothe@skylineinnovations.com>
2015-09-09 15:29:55 -0400

    start test for Entrust and fix sheet titles

--- brokerage/quote_parsers/__init__.py
+++ brokerage/quote_parsers/__init__.py
@@ -6,6 +6,7 @@ from .amerigreen import AmerigreenMatrixParser
 from .champion import ChampionMatrixParser
 from .constellation import ConstellationMatrixParser
 from .direct_energy import DirectEnergyMatrixParser
+from .entrust import EntrustMatrixParser
 from .usge import USGEMatrixParser
 
 # mapping of each supplier's primary keys in database to its QuoteParser
@@ -17,5 +18,6 @@ CLASSES_FOR_SUPPLIERS = {
     928: ChampionMatrixParser,
     103: ConstellationMatrixParser,
     14: DirectEnergyMatrixParser,
+    56: EntrustMatrixParser,
     199: USGEMatrixParser,
 }
--- brokerage/quote_parsers/entrust.py
+++ brokerage/quote_parsers/entrust.py
@@ -8,40 +8,46 @@ from brokerage.brokerage_model import MatrixQuote
 from util.units import unit_registry
 
 
-class DirectEnergyMatrixParser(QuoteParser):
+class EntrustMatrixParser(QuoteParser):
     """Parser for Entrust spreadsheet.
     """
-    FILE_FORMAT = formats.xls
-
-    # TODO: not updated yet
-    HEADER_ROW = 51
-    VOLUME_RANGE_ROW = 51
-    QUOTE_START_ROW = 52
-    STATE_COL = 'B'
-    UTILITY_COL = 'C'
-    RATE_CLASS_COL = 'E'
-    SPECIAL_OPTIONS_COL = 'F'
-    TERM_COL = 'H'
-    PRICE_START_COL = 8
-    PRICE_END_COL = 13
+    FILE_FORMAT = formats.xlsx
 
     EXPECTED_SHEET_TITLES = [
-        'Daily Matrix Price',
-    ]
+        'IL - ComEd Matrix',
+        'OH - Duke Matrix',
+        'OH - Dayton Matrix',
+        'PA - PECO Matrix',
+        'PA - PPL Matrix',
+        'MD - BGE Matrix',
+        'MD - PEPCO Matrix',
+        'NJ - JCPL Matrix',
+        'NYSEG - A - Matrix',
+        'NYSEG - B - Matrix',
+        'NYSEG - C - Matrix',
+        'NYSEG - D - Matrix',
+        'NYSEG - E - Matrix',
+        'NYSEG - F - Matrix',
+        'NYSEG - G - Matrix',
+        'NYSEG - H - Matrix',
+        'NYSEG - I - Matrix',
+        'NY - NATGRID - A - Matrix',
+        'NY - NATGRID - B - Matrix',
+        'NY - NATGRID - C - Matrix',
+        'NY - NATGRID - D - Matrix',
+        'NY - NATGRID - E - Matrix',
+        'RG&E - B - Matrix',
+        'ConEd - H - Matrix',
+        'ConEd - I - Matrix',
+        'ConEd - J - Matrix']
+
     EXPECTED_CELLS = [
-        (0, 1, 0, 'Direct Energy HQ - Daily Matrix Price Report'),
-        (0, HEADER_ROW, 0, 'Contract Start Month'),
-        (0, HEADER_ROW, 1, 'State'),
-        (0, HEADER_ROW, 2, 'Utility'),
-        (0, HEADER_ROW, 3, 'Zone'),
-        (0, HEADER_ROW, 4, r'Rate Code\(s\)'),
-        (0, HEADER_ROW, 5, 'Product Special Options'),
-        (0, HEADER_ROW, 6, 'Billing Method'),
-        (0, HEADER_ROW, 7, 'Term'),
-    ]
+        (sheet, 4, 'F', 'Pricing for Commercial Customers')
+        for sheet in EXPECTED_SHEET_TITLES]
+
     DATE_CELL = (0, 3, 0, 'as of (\d+/\d+/\d+)')
 
-    EXPECTED_ENERGY_UNIT = unit_registry.MWh
+    EXPECTED_ENERGY_UNIT = unit_registry.kWh
 
     def _extract_quotes(self):
         pass
