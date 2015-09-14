89c39d10ece0e2d38e749b94d12a74c0d81ad666
? <dklothe@skylineinnovations.com>
2015-09-14 19:31:03 -0400

    add email addresses to supplier table in upgrade script, and upgrade schema version to 482dddf4fe5d

--- upgrade_scripts/v32/__init__.py
+++ upgrade_scripts/v32/__init__.py
@@ -7,18 +7,29 @@ imported with the data model uninitialized! Therefore this module should not
 import any other code that that expects an initialized data model without first
 calling :func:`.core.init_model`.
 """
+import re
 from sqlalchemy.orm import joinedload
 from core import init_model
 from core.extraction.applier import UtilBillApplier
 from core.extraction.extraction import LayoutExtractor, Field
-from core.model import Session, UtilBill, BoundingBox
+from core.model import Session, UtilBill, BoundingBox, Supplier
 from reebill.reebill_model import ReeBill
 from upgrade_scripts import alembic_upgrade, log
 from util.layout import Corners
 
+# TODO replace with "prod" when ready to deploy
+ENVIRONMENT_NAME = 'dev'
 
 def upgrade():
-    alembic_upgrade('1226d67c4c53')
+    alembic_upgrade('482dddf4fe5d')
 
     init_model()
     s = Session()
+
+    from brokerage.quote_parsers import CLASSES_FOR_SUPPLIERS
+    for supplier_id in CLASSES_FOR_SUPPLIERS:
+        supplier = s.query(Supplier).filter_by(id=supplier_id).one()
+        email_address = 'matrix-%s@billing-%s.nextility.net' % (
+            re.sub('\s+', '', supplier.name.lower()), ENVIRONMENT_NAME)
+        supplier.matrix_email_recipient = email_address
+    s.commit()
