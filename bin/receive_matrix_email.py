b88ede498a4bfa2cb1db9074c6ada8291bd6b1d6
? <dklothe@skylineinnovations.com>
2015-09-15 12:02:40 -0400

    use the right logger for reporting exceptions in bin/receive_matrix_email.py

--- bin/receive_matrix_email.py
+++ bin/receive_matrix_email.py
@@ -6,7 +6,8 @@ import logging
 from sys import stdin
 import traceback
 
-from brokerage.quote_email_processor import QuoteEmailProcessor, QuoteDAO
+from brokerage.quote_email_processor import QuoteEmailProcessor, QuoteDAO, \
+    LOG_NAME
 from brokerage.quote_parsers import CLASSES_FOR_SUPPLIERS
 from core import initialize
 from core.model import AltitudeSession, Session
@@ -17,7 +18,9 @@ if __name__ == '__main__':
         qep = QuoteEmailProcessor(CLASSES_FOR_SUPPLIERS, QuoteDAO())
         qep.process_email(stdin)
     except Exception as e:
-        logging.error('Error when processing email:\n%s' % (
+        logger = logging.getLogger(LOG_NAME)
+        logger.setLevel(logging.DEBUG)
+        logger.error('Error when processing email:\n%s' % (
             traceback.format_exc()))
         # it is important to exit with non-0 status when an error happened,
         # because that causes Postfix to send a bounce email with the error
