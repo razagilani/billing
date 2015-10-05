832d2112c2caecec5d3fcc40726e2d449d77e93e
? <dklothe@skylineinnovations.com>
2015-10-05 14:02:22 -0400

    add read-only API token from Librato to StatsD config file

--- conf/templates/config.js
+++ conf/templates/config.js
@@ -1,7 +1,8 @@
 {
   librato: {
     email:  "itops@nextility.com",
-    token:  "", # TODO
+    // read-only API token
+    token:  "3ee3f68e175297f0d12986d9cbbec2bac503c1f8101ef5c10b1c89579172392c",
     source: "<%= @env%>"
   }
   , backends: ["statsd-librato-backend"]
