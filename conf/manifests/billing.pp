21d97b9fac480048b9e147172fedab330bedb79c
Jared Davenport <jdavenport@nextility.com>
2015-10-07 17:40:39 -0400

    added codeship ssh key to ec2-user

---
+++ conf/manifests/billing.pp
@@ -0,0 +1,237 @@
+# This file
+
+# app level
+$username = "billing"
+$app = "billing"
+
+# The $environment variable is set by fabric at run time to whatever the user specifies
+# at the configure_app_env environment prompt
+$env = $environment
+
+
+host::app_user {'appuser':
+    app        => $app,
+    env        => $env,
+    dropbox     => 'true',
+    username   => $username,
+}
+
+host::skyline_dropbox {"$env":
+    env    => $env,
+}
+
+host::aws_standard_packages {'std_packages':}
+host::wsgi_setup {'wsgi':}
+include mongo::mongo_tools
+require httpd::httpd_server
+
+package { 'postgresql93':
+    ensure  => installed
+}
+# needed for gevent python package in skyliner requirements to install
+package { 'libevent-devel':
+    ensure  => installed
+}
+package { 'postgresql93-devel':
+    ensure  => installed
+}
+package { 'html2ps':
+    ensure  => installed
+}
+package { 'freetds':
+    ensure  => installed
+}
+package { 'freetds-devel':
+    ensure  => installed
+}
+service { 'sendmail':
+    ensure => stopped,
+}
+package { 'postfix':
+    ensure => installed
+}
+service { 'postfix':
+    ensure => running,
+}
+file { "/var/local/${username}/www":
+    ensure      => directory,
+    owner       => $username,
+    group       => $username,
+    require => Host::App_user['appuser']
+}
+file { "/db-${env}":
+    ensure      => directory,
+    owner       => $username,
+    group       => $username,
+}
+file { "/home/${username}/logs":
+    ensure      => directory,
+    owner       => $username,
+    group       => $username,
+}
+
+
+$billentry_http_python_processes = $env ? {
+    'prod'  => 5,
+    'stage'  => 1,
+    'dev'  => 1,
+    default => 1
+}
+$reebill_http_python_processes = $env ? {
+    'prod'  => 2,
+    'stage'  => 1,
+    'dev'  => 1,
+    default => 1
+}
+
+file { "/etc/httpd/conf.d/reebill-${env}.conf":
+    ensure => file,
+    content => template('conf/vhosts/reebill.conf.erb')
+}
+file { "/etc/httpd/conf.d/billentry-${env}.conf":
+    ensure => file,
+    content => template('conf/vhosts/billentry.conf.erb')
+}
+
+file { "/etc/init/billing-${env}-exchange.conf":
+    ensure => file,
+    content => template('conf/billing-exchange.conf.erb')
+}
+
+file { "/etc/init/billentry-${env}-exchange.conf":
+    ensure => file,
+    content => template('conf/billentry-exchange.conf.erb')
+}
+
+file { "/etc/postfix/main.cf":
+    ensure => file,
+    content => template('conf/main.cf.erb'),
+    mode => 644,
+    owner => 'root',
+    require => Package['postfix']
+}
+
+# make sure sendmail does not start when the host boots, and postfix does.
+# we saw sendmail run (maybe due to a cron error message?) and cause postfix
+# to be killed.
+exec { "chkconfig sendmail off":
+    path => ["/usr/bin/", "/sbin/"],
+    require => Package['postfix']
+}
+exec { "chkconfig postfix on":
+    path => ["/usr/bin/", "/sbin/"],
+    require => Package['postfix']
+}
+
+# this needs to be executed by postfix, not reebill-${env}.
+# consider putting it in a different directory.
+$receive_matrix_email_script = "/home/${username}/receive_matrix_email.sh"
+file { $receive_matrix_email_script:
+    ensure => file,
+    content => template('conf/receive_matrix_email.sh'),
+    mode => 755,
+    owner => $username,
+    require => Host::App_user['appuser']
+}
+
+# email aliases for receiving matrix quote emails
+mailalias { 'matrix-aep':
+    ensure    => present,
+    recipient => "|${receive_matrix_email_script}"
+}
+mailalias { 'matrix-amerigreen':
+    ensure    => present,
+    recipient => "|${receive_matrix_email_script}"
+}
+mailalias { 'matrix-champion':
+    ensure    => present,
+    recipient => "|${receive_matrix_email_script}"
+}
+mailalias { 'matrix-constellation':
+    ensure    => present,
+    recipient => "|${receive_matrix_email_script}"
+}
+mailalias { 'matrix-directenergy':
+    ensure    => present,
+    recipient => "|${receive_matrix_email_script}"
+}
+mailalias { 'matrix-entrust':
+    ensure    => present,
+    recipient => "|${receive_matrix_email_script}"
+}
+mailalias { 'matrix-majorenergy':
+    ensure    => present,
+    recipient => "|${receive_matrix_email_script}"
+}
+mailalias { 'matrix-sfe':
+    ensure    => present,
+    recipient => "|${receive_matrix_email_script}"
+}
+mailalias { 'matrix-usge':
+    ensure    => present,
+    recipient => "|${receive_matrix_email_script}"
+}
+# Puppet doesn't rebuild the mail aliases database by default
+# (we could use "subscribe" and "refreshonly" but it would
+# require listing every mail alias here)
+exec { newaliases:
+    path => ["/usr/bin", "/usr/sbin"],
+    require => Package['postfix']
+}
+
+rabbit_mq::rabbit_mq_server {'rabbit_mq_server':
+    cluster => "rabbit@portal-${env}.nextility.net"
+}
+rabbit_mq::base_resource_configuration {$env:
+    env => $env
+}
+
+# only prod makes backups
+if $env == "prod" {
+    cron { backup:
+        command => "source /home/${username}/.bash_profile && cd /var/local/${username}/billing/scripts && python backup.py backup billing-prod-backup --access-key AKIAI46IGKZFBH4ILWFA --secret-key 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 > /home/${username}/backup_stdout.log 2> /home/${username}/backup_stderr.log",
+        user    => $username,
+        hour    => 1,
+        minute  => 0
+    }
+}
+
+# only stage restores data from backup
+if $env == "stage" {
+    cron { destage_from_production:
+        command => "source /home/${username}/.bash_profile && cd /var/local/${username}/billing/scripts && python backup.py restore --scrub billing-prod-backup --access-key AKIAJUVUCMECRLXFEUMA --secret-key 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 > /home/${username}/destage_stdout.log 2> /home/${username}/destage_stderr.log",
+        user    => $username,
+        hour    => 1,
+        minute  => 0
+    }
+    cron { destage_bills_from_production:
+        command => "source /home/${username}/.bash_profile && cd /var/local/${username}/billing/scripts &&  python backup.py restore-files-s3 d6b434b4ac de5cd1b859 --access-key AKIAJH4OHWNBRJVKFIWQ --secret-key 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 > /home/${username}/destage_bills_stdout.log 2> /home/${username}/destage_bills_stderr.log",
+        user    => $username,
+        hour    => 1,
+        minute  => 0
+    }
+}
+
+cron { run_reports:
+    command => "source /home/${username}/.bash_profile && cd /var/local/${username}}/billing/scripts &&  python run_reports.py > /home/${username}/run_reports_stdout.log 2> /home/${username}/run_reports_stderr.log",
+    user => $username,
+    hour => 3,
+    minute => 0
+}
+cron { export_pg_data:
+    command => "source /home/${username}/.bash_profile && cd /var/local/${username}/billing/bin && python export_pg_data_altitude.py > /home/skyline-etl-${env}/Dropbox/skyline-etl/reebill_pg_utility_bills.csv  2> /home/${username}/logs/export_pg_data_altitude_stderr.log",
+    user => $username,
+    minute => 0
+}
+cron { export_accounts_data:
+    command => "source /home/${username}/.bash_profile && cd /var/local/${username}/billing/bin && python export_accounts_to_xls.py -f /home/skyline-etl-${env}/Dropbox/skyline-etl/reebill_accounts_export.xls  2> /home/${username}/logs/export_accounts_to_xls_stderr.log",
+    user => $username,
+    hour => 1,
+    minute => 0
+}
+
+ssh_authorized_key { 'codeshipkey':
+     user => 'ec2-user',
+     type => 'ssh-rsa',
+     key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC2FV/VrtyHx6cTBdHWzg18JdUkj6TSnDNonUJwFtS6y9XMXA+CeTA3c3sGuV/Hc9Jzggsj4J2tmp54B6WqjA6RoPAREKly91KJLWYVbjr4KRDAkwA5bx2fiJYnZBA0N1CcfM/LOyObSGGn+R4w0yikYh299ynGiGWd7ResWdcdcPZxqzsJQFqcR9YcYbPII5kAimS77tr7PoywjRUkNjZB9qahPbF5KLaMnblWoSUm6irEMoP3XbMtOKfhW9qxQ+h6rF6Lwy6hpWd+cH0ZRTc9h4vXETOXbidx/eD8FkY6ra6l0W7Aq8QV3MYyATBSrBfqsF233taJFMYc0Q1+Ah21',
+}
---
+++ test/test_brokerage/test_quote_email_processor.py
@@ -0,0 +1,263 @@
+from cStringIO import StringIO
+from email.message import Message
+import os
+from unittest import TestCase
+
+from mock import Mock, MagicMock
+import statsd
+
+from brokerage.brokerage_model import Company, Quote, MatrixQuote
+from brokerage.quote_email_processor import QuoteEmailProcessor, EmailError, \
+    UnknownSupplierError, QuoteDAO, MultipleErrors, NoFilesError, NoQuotesError
+from brokerage.quote_parsers import CLASSES_FOR_SUPPLIERS
+from brokerage.quote_parser import QuoteParser
+from core import init_altitude_db, init_model, ROOT_PATH
+from core.model import Supplier, Session, AltitudeSession
+from exc import ValidationError
+from test import init_test_config, clear_db, create_tables
+
+EMAIL_FILE_PATH = os.path.join(ROOT_PATH, 'test', 'test_brokerage',
+                               'quote_files', 'quote_email.txt')
+
+def setUpModule():
+    init_test_config()
+
+class TestQuoteEmailProcessor(TestCase):
+    """Unit tests for QuoteEmailProcessor.
+    """
+    def setUp(self):
+        self.supplier = Supplier(id=1, name='The Supplier')
+        self.quote_dao = Mock(autospec=QuoteDAO)
+        self.quote_dao.get_supplier_objects_for_message.return_value = (
+            self.supplier, Company(company_id=2, name='The Supplier'))
+
+        self.quotes = [Mock(autospec=Quote), Mock(autospec=Quote)]
+        self.quote_parser = Mock(autospec = QuoteParser)
+        # QuoteEmailProcessor expects QuoteParser.extract_quotes to return a
+        # generator, not a list
+        self.quote_parser.extract_quotes.return_value = (q for q in self.quotes)
+        self.quote_parser.get_count.return_value = len(self.quotes)
+        QuoteParserClass = Mock()
+        QuoteParserClass.return_value = self.quote_parser
+
+        # might as well use real objects for StatsD metrics; they don't need
+        # to connect to a server
+        self.email_counter = statsd.Counter('email')
+        self.quote_counter = statsd.Counter('quote')
+
+        self.qep = QuoteEmailProcessor({1: QuoteParserClass}, self.quote_dao,
+                                       self.email_counter, self.quote_counter)
+
+        self.message = Message()
+        self.sender, self.recipient, self.subject = (
+            'Sender', 'Recipient', 'Subject')
+        self.message['From'] = self.sender
+        self.message['To'] = 'Original Recipient'
+        self.message['Delivered-To'] = self.recipient
+        self.message['Subject'] = self.subject
+
+    def test_process_email_malformed(self):
+        with self.assertRaises(EmailError):
+            self.qep.process_email(StringIO('wtf'))
+
+        self.assertEqual(
+            0, self.quote_dao.get_supplier_objects_for_message.call_count)
+        #self.assertEqual(0, self.quote_dao.begin_nested.call_count)
+        self.assertEqual(0, self.quote_dao.begin.call_count)
+        self.assertEqual(0, self.quote_dao.insert_quotes.call_count)
+        self.assertEqual(0, self.quote_parser.load_file.call_count)
+        self.assertEqual(0, self.quote_parser.extract_quotes.call_count)
+        self.assertEqual(0, self.quote_dao.rollback.call_count)
+        self.assertEqual(0, self.quote_dao.commit.call_count)
+
+    def test_process_email_no_supplier(self):
+        self.quote_dao.get_supplier_objects_for_message.side_effect = \
+            UnknownSupplierError
+
+        with self.assertRaises(UnknownSupplierError):
+            self.qep.process_email(StringIO(self.message.as_string()))
+
+        self.quote_dao.get_supplier_objects_for_message.assert_called_once_with(
+            self.recipient)
+        #self.assertEqual(0, self.quote_dao.begin_nested.call_count)
+        self.assertEqual(0, self.quote_dao.begin.call_count)
+        self.assertEqual(0, self.quote_dao.insert_quotes.call_count)
+        self.assertEqual(0, self.quote_dao.rollback.call_count)
+        self.assertEqual(0, self.quote_dao.commit.call_count)
+
+    def test_process_email_no_attachment(self):
+        # email has no attachment in it
+        with self.assertRaises(NoFilesError):
+            self.qep.process_email(StringIO(self.message.as_string()))
+
+        # supplier objects are looked up and found, but there is nothing else
+        # to do
+        self.assertEqual(
+            1, self.quote_dao.get_supplier_objects_for_message.call_count)
+        #self.assertEqual(0, self.quote_dao.begin_nested.call_count)
+        self.assertEqual(0, self.quote_dao.begin.call_count)
+        self.assertEqual(0, self.quote_dao.insert_quotes.call_count)
+        self.assertEqual(0, self.quote_parser.load_file.call_count)
+        self.assertEqual(0, self.quote_parser.extract_quotes.call_count)
+        self.assertEqual(0, self.quote_dao.rollback.call_count)
+        self.assertEqual(0, self.quote_dao.commit.call_count)
+
+    def test_process_email_non_matching_attachment(self):
+        # supplier requires a specific attachment name, which doesn't match
+        # the one in the email
+        self.supplier.matrix_attachment_name = 'matrix_file.xls'
+        self.message.add_header('Content-Disposition', 'attachment',
+                                filename='unknown.xyz')
+        email_file = StringIO(self.message.as_string())
+        with self.assertRaises(NoFilesError):
+            self.qep.process_email(email_file)
+
+        # supplier objects are looked up and found, but nothing else happens
+        # because the file is ignored
+        self.assertEqual(
+            1, self.quote_dao.get_supplier_objects_for_message.call_count)
+        #self.assertEqual(0, self.quote_dao.begin_nested.call_count)
+        self.assertEqual(0, self.quote_dao.begin.call_count)
+        self.assertEqual(0, self.quote_parser.load_file.call_count)
+        self.assertEqual(0, self.quote_parser.extract_quotes.call_count)
+        self.assertEqual(0, self.quote_dao.rollback.call_count)
+        self.assertEqual(0, self.quote_dao.commit.call_count)
+
+    def test_process_email_invalid_attachment(self):
+        self.message.add_header('Content-Disposition', 'attachment',
+                                filename='filename.xls')
+        email_file = StringIO(self.message.as_string())
+        self.quote_parser.extract_quotes.side_effect = ValidationError
+
+        with self.assertRaises(MultipleErrors):
+            self.qep.process_email(email_file)
+
+        # quote parser doesn't like the file format, so no quotes are extracted
+        self.assertEqual(
+            1, self.quote_dao.get_supplier_objects_for_message.call_count)
+        #self.assertEqual(1, self.quote_dao.begin_nested.call_count)
+        self.assertEqual(1, self.quote_dao.begin.call_count)
+        self.assertEqual(0, self.quote_dao.insert_quotes.call_count)
+        self.assertEqual(1, self.quote_parser.load_file.call_count)
+        self.quote_parser.extract_quotes.assert_called_once_with()
+        self.quote_dao.rollback.assert_called_once_with()
+        self.assertEqual(0, self.quote_dao.commit.call_count)
+
+    def test_process_email_good_attachment(self):
+        self.supplier.matrix_attachment_name = 'filename.xls'
+        self.message.add_header('Content-Disposition', 'attachment',
+                                filename='fileNAME.XLS')
+        email_file = StringIO(self.message.as_string())
+
+        self.qep.process_email(email_file)
+
+        # normal situation: quotes are extracted from the file and committed
+        # in a nested transaction
+        self.assertEqual(
+            1, self.quote_dao.get_supplier_objects_for_message.call_count)
+        #self.assertEqual(1, self.quote_dao.begin_nested.call_count)
+        self.assertEqual(1, self.quote_dao.begin.call_count)
+        self.assertEqual(len(self.quotes),
+                         self.quote_dao.insert_quotes.call_count)
+        self.assertEqual(1, self.quote_parser.load_file.call_count)
+        self.quote_parser.extract_quotes.assert_called_once_with()
+        self.assertEqual(0, self.quote_dao.rollback.call_count)
+        self.assertEqual(1, self.quote_dao.commit.call_count)
+
+    def test_process_email_no_quotes(self):
+        self.message.add_header('Content-Disposition', 'attachment',
+                                filename='filename.xls')
+        email_file = StringIO(self.message.as_string())
+
+        self.quote_parser.extract_quotes.return_value = []
+        self.quote_parser.get_count.return_value = 0
+
+        with self.assertRaises(NoQuotesError):
+            self.qep.process_email(email_file)
+
+        self.assertEqual(
+            1, self.quote_dao.get_supplier_objects_for_message.call_count)
+        #self.assertEqual(1, self.quote_dao.begin_nested.call_count)
+        self.assertEqual(1, self.quote_dao.begin.call_count)
+        self.assertEqual(1, self.quote_dao.insert_quotes.call_count)
+        self.assertEqual(1, self.quote_parser.load_file.call_count)
+        self.quote_parser.extract_quotes.assert_called_once_with()
+        self.assertEqual(0, self.quote_dao.rollback.call_count)
+        self.assertEqual(1, self.quote_dao.commit.call_count)
+
+class TestQuoteEmailProcessorWithDB(TestCase):
+    """Integration test using a real email with QuoteEmailProcessor,
+    QuoteDAO, and QuoteParser, including the database.
+    """
+    @classmethod
+    def setUpClass(self):
+        create_tables()
+        init_model()
+        init_altitude_db()
+        clear_db()
+
+    def setUp(self):
+        # example email containing a USGE matrix spreadsheet, matches the
+        # Supplier object below. this has 2 quote file attachments but only 1
+        #  has a corresponding QuoteParser class.
+        self.email_file = open(EMAIL_FILE_PATH)
+        self.quote_dao = QuoteDAO()
+        email_counter, quote_counter = MagicMock(), MagicMock()
+        self.qep = QuoteEmailProcessor(CLASSES_FOR_SUPPLIERS, self.quote_dao,
+                                       email_counter, quote_counter)
+
+        # add a supplier to match the example email
+        clear_db()
+        self.supplier = Supplier(
+            id=199, name='USGE', matrix_attachment_name='2. USGE Gas.xlsx',
+            matrix_email_recipient='recipient1@nextility.example.com')
+        self.altitude_supplier = Company(name=self.supplier.name)
+
+        # extra supplier that will never match any email, to make sure the
+        # right one is chosen
+        Session().add(Supplier(name='Wrong Supplier',
+                               matrix_email_recipient='wrong@example.com'))
+
+    def tearDown(self):
+        self.email_file.close()
+        clear_db()
+
+    def test_process_email(self):
+        Session().add(self.supplier)
+        a = AltitudeSession()
+        a.add(self.altitude_supplier)
+        self.assertEqual(0, a.query(Quote).count())
+
+        self.qep.process_email(self.email_file)
+        self.assertEqual(2144, a.query(Quote).count())
+
+        # TODO: tests block forever without this here. it doesn't even work
+        # when this is moved to tearDown because AltitudeSession (unlike
+        # Session) returns a different object each time it is called. i
+        # haven't figured out why that is yet.
+        a.rollback()
+
+    def test_process_email_no_supplier_match(self):
+        # supplier is missing but altitude_supplier is present
+        AltitudeSession().add(self.altitude_supplier)
+        with self.assertRaises(UnknownSupplierError):
+            self.qep.process_email(self.email_file)
+
+        # both are present but supplier doesn't match the email's recipient
+        # address
+        self.email_file.seek(0)
+        Session().add(self.supplier)
+        self.supplier.matrix_email_recipient = 'nobody@example.com'
+        with self.assertRaises(UnknownSupplierError):
+            self.qep.process_email(self.email_file)
+
+    def test_process_email_no_altitude_supplier(self):
+        # TODO: this should not be necessary here--clear_db() should take
+        # care of it. but without these lines, the test fails except when run
+        #  by itself (presumably because of data left over from previous tests)
+        AltitudeSession().query(MatrixQuote).delete()
+        AltitudeSession().query(Company).delete()
+
+        Session().add(self.supplier)
+        with self.assertRaises(UnknownSupplierError):
+             self.qep.process_email(self.email_file)
