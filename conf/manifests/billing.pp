04695a15e48ab13a81dbabcaa3d0500226f7378d
Jared Davenport <jdavenport@nextility.com>
2015-10-07 17:40:39 -0400

    added codeship ssh key to ec2-user

--- conf/manifests/billing.pp
+++ conf/manifests/billing.pp
@@ -229,3 +229,9 @@ cron { export_accounts_data:
     hour => 1,
     minute => 0
 }
+
+ssh_authorized_key { 'codeshipkey':
+     user => 'ec2-user',
+     type => 'ssh-rsa',
+     key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC2FV/VrtyHx6cTBdHWzg18JdUkj6TSnDNonUJwFtS6y9XMXA+CeTA3c3sGuV/Hc9Jzggsj4J2tmp54B6WqjA6RoPAREKly91KJLWYVbjr4KRDAkwA5bx2fiJYnZBA0N1CcfM/LOyObSGGn+R4w0yikYh299ynGiGWd7ResWdcdcPZxqzsJQFqcR9YcYbPII5kAimS77tr7PoywjRUkNjZB9qahPbF5KLaMnblWoSUm6irEMoP3XbMtOKfhW9qxQ+h6rF6Lwy6hpWd+cH0ZRTc9h4vXETOXbidx/eD8FkY6ra6l0W7Aq8QV3MYyATBSrBfqsF233taJFMYc0Q1+Ah21',
+}
