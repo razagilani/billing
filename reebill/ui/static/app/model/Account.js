c6a45dc43805b4a8223906f5781487344a284422
? <dklothe@skylineinnovations.com>
2015-10-28 18:19:42 -0400

    add xbill as a subrepository

---
+++ reebill/ui/static/app/model/Account.js
@@ -0,0 +1,42 @@
+Ext.define('ReeBill.model.Account', {
+    extend: 'Ext.data.Model',
+    fields: [
+        // Received from server when the accounts list is loaded
+        {name: 'utility_account_id', type: 'int'},
+        {name: 'account', type: 'string'},
+        {name: 'tags', type: 'string'},
+        {name: 'utility_account_number', type: 'string'},
+        {name: 'utilityserviceaddress', type: 'string'},
+        {name: 'fb_utility_name', type: 'string'},
+        {name: 'fb_rate_class', type: 'string'},
+        {name: 'lastevent', type: 'string'},
+        {name: 'codename', type: 'string'},
+        {name: 'casualname', type: 'string'},
+        {name: 'primusname', type: 'string'},
+        {name: 'brokerage_account', type: 'boolean'},
+        {name: 'reebill_customer', type:'boolean'},
+
+        // Needed when a new account is created
+        {name: 'name', type: 'string'},
+        {name: 'ba_addressee', type: 'string'},
+        {name: 'ba_city', type: 'string'},
+        {name: 'ba_postal_code', type: 'string'},
+        {name: 'ba_state', type: 'string'},
+        {name: 'ba_street', type: 'string'},
+        {name: 'discount_rate', type: 'float'},
+        {name: 'late_charge_rate', type: 'float'},
+        {name: 'sa_addressee', type: 'string'},
+        {name: 'sa_city', type: 'string'},
+        {name: 'sa_postal_code', type: 'string'},
+        {name: 'sa_state', type: 'string'},
+        {name: 'sa_street', type: 'string'},
+        {name: 'service_type', type: 'string'},
+        {name: 'template_account'},
+        {name: 'payee', type:'string'},
+
+        // Needed to update references in an account being merged
+        {name: 'accounts_deleted'}
+
+    ],
+    idProperty: 'utility_account_id'
+});
