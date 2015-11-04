1184602926bfe249adc17ba263060279133a1cf5
Bill Van Besien <bvanbesien@nextility.com>
2015-11-04 10:35:01 -0500

    Added stub for USGE_Electric unit test

--- test/test_brokerage/test_quote_parser.py
+++ test/test_brokerage/test_quote_parser.py
@@ -162,6 +162,108 @@ class MatrixQuoteParsersTest(TestCase):
         self.assertEqual(False, q1.purchase_of_receivables)
         self.assertEqual(.07036, q1.price)
 
+    def test_usge_electric(self):
+        parser = USGEElectricMatrixParser()
+        self.assertEqual(0, parser.get_count())
+
+        with open(self.USGE_FILE_PATH, 'rb') as spreadsheet:
+            parser.load_file(spreadsheet)
+        parser.validate()
+
+        assert self.rate_class.rate_class_id == 1
+        quotes = list(parser.extract_quotes())
+        self.assertEqual(2448, len(quotes))
+
+        for quote in quotes:
+            quote.validate()
+
+        # each state has its own sheet. to make sure each sheet is done
+        # correctly, we check the first one in each (we determined the index
+        # of each one by counting the number of quotes in each sheet)
+
+        # KY check
+        q1 = quotes[0]
+        self.assertEqual('Columbia of Kentucky-Residential-Residential', q1.rate_class_alias)
+        self.assertEqual(self.rate_class.rate_class_id, q1.rate_class_id)
+        self.assertEqual(datetime(2015, 6, 1), q1.start_from)
+        self.assertEqual(datetime(2015, 7, 1), q1.start_until)
+        self.assertEqual(6, q1.term_months)
+        self.assertEqual(datetime.utcnow().date(), q1.date_received.date())
+        self.assertEqual(datetime(2015, 5, 4), q1.valid_from)
+        self.assertEqual(datetime(2015, 5, 5), q1.valid_until)
+        self.assertEqual(0, q1.min_volume)
+        self.assertEqual(False, q1.purchase_of_receivables)
+        self.assertEqual(.4729, q1.price)
+
+        # MD check
+        q1 = quotes[96]
+        self.assertEqual('Baltimore Gas & Electric-Residential-Residential', q1.rate_class_alias)
+        self.assertEqual(datetime(2015, 6, 1), q1.start_from)
+        self.assertEqual(datetime(2015, 7, 1), q1.start_until)
+        self.assertEqual(6, q1.term_months)
+        self.assertEqual(datetime.utcnow().date(), q1.date_received.date())
+        self.assertEqual(datetime(2015, 5, 4), q1.valid_from)
+        self.assertEqual(datetime(2015, 5, 5), q1.valid_until)
+        self.assertEqual(0, q1.min_volume)
+        self.assertEqual(False, q1.purchase_of_receivables)
+        self.assertEqual(.4793, q1.price)
+
+        # NJ check
+        q1 = quotes[288]
+        #self.assertEqual('Residential', q1.rate_class_alias)
+        #self.assertEqual(self.rate_class.rate_class_id, q1.rate_class_id)
+        self.assertEqual(datetime(2015, 7, 1), q1.start_from)
+        self.assertEqual(datetime(2015, 8, 1), q1.start_until)
+        self.assertEqual(6, q1.term_months)
+        self.assertEqual(datetime.utcnow().date(), q1.date_received.date())
+        self.assertEqual(datetime(2015, 5, 4), q1.valid_from)
+        self.assertEqual(datetime(2015, 5, 5), q1.valid_until)
+        self.assertEqual(0, q1.min_volume)
+        self.assertEqual(False, q1.purchase_of_receivables)
+        self.assertEqual(.5242, q1.price)
+
+        # NY check
+        q1 = quotes[528]
+        #self.assertEqual('Residential', q1.rate_class_alias)
+        #self.assertEqual(self.rate_class.rate_class_id, q1.rate_class_id)
+        self.assertEqual(datetime(2015, 6, 1), q1.start_from)
+        self.assertEqual(datetime(2015, 7, 1), q1.start_until)
+        self.assertEqual(6, q1.term_months)
+        self.assertEqual(datetime.utcnow().date(), q1.date_received.date())
+        self.assertEqual(datetime(2015, 5, 4), q1.valid_from)
+        self.assertEqual(datetime(2015, 5, 5), q1.valid_until)
+        self.assertEqual(0, q1.min_volume)
+        self.assertEqual(False, q1.purchase_of_receivables)
+        self.assertEqual(.6292, q1.price)
+
+        # OH check
+        q1 = quotes[1776]
+        #self.assertEqual('Residential', q1.rate_class_alias)
+        #self.assertEqual(self.rate_class.rate_class_id, q1.rate_class_id)
+        self.assertEqual(datetime(2015, 6, 1), q1.start_from)
+        self.assertEqual(datetime(2015, 7, 1), q1.start_until)
+        self.assertEqual(6, q1.term_months)
+        self.assertEqual(datetime.utcnow().date(), q1.date_received.date())
+        self.assertEqual(datetime(2015, 5, 4), q1.valid_from)
+        self.assertEqual(datetime(2015, 5, 5), q1.valid_until)
+        self.assertEqual(0, q1.min_volume)
+        self.assertEqual(False, q1.purchase_of_receivables)
+        self.assertEqual(.5630, q1.price)
+
+        # PA check
+        q1 = quotes[1968]
+        #self.assertEqual('Residential', q1.rate_class_alias)
+        #self.assertEqual(self.rate_class.rate_class_id, q1.rate_class_id)
+        self.assertEqual(datetime(2015, 6, 1), q1.start_from)
+        self.assertEqual(datetime(2015, 7, 1), q1.start_until)
+        self.assertEqual(6, q1.term_months)
+        self.assertEqual(datetime.utcnow().date(), q1.date_received.date())
+        self.assertEqual(datetime(2015, 5, 4), q1.valid_from)
+        self.assertEqual(datetime(2015, 5, 5), q1.valid_until)
+        self.assertEqual(0, q1.min_volume)
+        self.assertEqual(False, q1.purchase_of_receivables)
+        self.assertEqual(.4621, q1.price)
+
     def test_usge(self):
         parser = USGEMatrixParser()
         self.assertEqual(0, parser.get_count())
