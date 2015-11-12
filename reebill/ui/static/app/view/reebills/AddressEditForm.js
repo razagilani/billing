60b2b681a60c62a403cc9faab256878dbcdf542e
Raza Gilani <rgilani@skylineinnovations.com>
2015-11-12 13:47:00 -0500

    created a form for editing reebill addresses

---
+++ reebill/ui/static/app/view/reebills/AddressEditForm.js
@@ -0,0 +1,82 @@
+Ext.define('ReeBill.view.reebills.AddressEditForm', {
+    extend: 'Ext.form.Panel',
+    title: 'Edit Billing/Service Addresses',
+
+    alias: 'widget.addressEditForm',
+
+    bodyPadding: 15,
+    autoScroll: true,
+
+    defaults: {
+        anchor: '100%'
+    },
+
+    items: [{
+        xtype: 'fieldset',
+        title: 'Billing Address',
+        defaults: {
+            anchor: '100%',
+            labelWidth: 150
+        },
+        items: [{
+            xtype: 'textfield',
+            fieldLabel: 'Addressee',
+            name: 'ba_addressee'
+        },{
+            xtype: 'textfield',
+            fieldLabel: 'Street',
+            name: 'ba_street'
+        },{
+            xtype: 'textfield',
+            fieldLabel: 'City',
+            name: 'ba_city'
+        },{
+            xtype: 'textfield',
+            fieldLabel: 'State',
+            name: 'ba_state'
+        },{
+            xtype: 'textfield',
+            fieldLabel: 'Postal Code',
+            name: 'ba_postal_code'
+        }]
+    },{
+        xtype: 'fieldset',
+        title: 'Service Address',
+        defaults: {
+            anchor: '100%',
+            labelWidth: 150
+        },
+        items: [{
+            xtype: 'textfield',
+            fieldLabel: 'Addressee',
+            name: 'sa_addressee'
+        },{
+            xtype: 'textfield',
+            fieldLabel: 'Street',
+            name: 'sa_street'
+        },{
+            xtype: 'textfield',
+            fieldLabel: 'City',
+            name: 'sa_city'
+        },{
+            xtype: 'textfield',
+            fieldLabel: 'State',
+            name: 'sa_state'
+        },{
+            xtype: 'textfield',
+            fieldLabel: 'Postal Code',
+            name: 'sa_postal_code'
+        }]
+    }],
+
+    dockedItems: [{
+        dock: 'bottom',
+        xtype: 'toolbar',
+        items: ['->',{
+            xtype: 'button',
+            text: 'Save',
+            iconCls: 'silk-disk',
+            action: 'saveEditChanges'
+        }]
+    }]
+});
