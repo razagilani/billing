2fc8bf3bb36666ed1ab2736fa4d02f1d1536c1ae
? <dklothe@skylineinnovations.com>
2015-11-16 18:28:57 -0500

    split quote_parser.py into 3 files: quote_parser, spreadsheet_reader, validation (in preparation to support PDF)

---
+++ brokerage/validation.py
@@ -0,0 +1,19 @@
+# TODO: ValidationError should probably be specific to this module,
+# not a global thing. this kind of validation doesn't have anything in common
+#  with other validation.
+import re
+from core.exceptions import ValidationError
+
+def _assert_true(p):
+    if not p:
+        raise ValidationError('Assertion failed')
+
+
+def _assert_equal(a, b):
+    if a != b:
+        raise ValidationError("Expected %s, found %s" % (a, b))
+
+
+def _assert_match(regex, string):
+    if not re.match(regex, string):
+        raise ValidationError('No match for "%s" in "%s"' % (regex, string))
