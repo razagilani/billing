f144190b5816e2db9837ee6e8c083101368e5490
? <dklothe@skylineinnovations.com>
2015-12-03 17:37:51 -0500

    add stub of upgrade_scripts/v37/__init__.py

---
+++ upgrade_scripts/v37/__init__.py
@@ -0,0 +1,23 @@
+"""Upgrade script for version 27.
+
+Script must define `upgrade`, the function for upgrading.
+
+Important: For the purpose of allowing schema migration, this module will be
+imported with the data model uninitialized! Therefore this module should not
+import any other code that that expects an initialized data model without first
+calling :func:`.core.init_model`.
+"""
+import logging
+import os
+import subprocess
+
+from core.model import Session, AltitudeSession
+from core import init_model, get_db_params, init_altitude_db
+
+log = logging.getLogger(__name__)
+
+def upgrade():
+
+    init_model()
+    s = Session()
+    s.commit()
