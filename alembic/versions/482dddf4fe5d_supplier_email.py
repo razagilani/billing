4e2c2fc3c7de048fd57cb00c6314cae5f471570b
? <dklothe@skylineinnovations.com>
2015-12-07 13:17:51 -0500

    BILL-6643: add stderr handler for matrix quotes log read_quotes.log

---
+++ alembic/versions/482dddf4fe5d_supplier_email.py
@@ -0,0 +1,25 @@
+"""supplier_email
+
+Revision ID: 482dddf4fe5d
+Revises: 1953b5abb480
+Create Date: 2015-08-05 16:10:37.807261
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '482dddf4fe5d'
+down_revision = '1226d67c4c53'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+def upgrade():
+    op.add_column('supplier', sa.Column('matrix_email_recipient', sa.String(),
+                                        nullable=True, unique=True))
+    op.add_column('supplier',sa.Column('matrix_attachment_name', sa.String(),
+                                        nullable=True))
+
+
+def downgrade():
+    raise NotImplementedError
