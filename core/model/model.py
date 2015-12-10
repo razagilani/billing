6e28dca8f692d4d859106f7e96906ebddfc6217b
Bill Van Besien <bvanbesien@nextility.com>
2015-12-10 13:24:05 -0500

    Merged with default

--- core/model/model.py
+++ core/model/model.py
@@ -179,7 +179,7 @@ class _Base(object):
 Base = declarative_base(cls=_Base)
 AltitudeBase = declarative_base(cls=_Base)
 
-_schema_revision = '4f589e8d4cab'
+_schema_revision = '366f5ed78b68'
 
 
 def check_schema_revision(schema_revision=None):
