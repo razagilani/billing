ee849ed96c1d5a90acce64ffa39d6fa44cc224cb
? <dklothe@skylineinnovations.com>
2016-01-11 16:44:44 -0500

    add test/test_core/test_bill_receiver.py, start of tests for getting utility bill files for solar billing

---
+++ util/fix_mq.py
@@ -0,0 +1,24 @@
+import errno
+import os
+from core import ROOT_PATH
+
+__author__ = 'dan'
+
+
+class FixMQ(object):
+    """Context manager to avoid having to create a file inside the "mq"
+    directory in order to import it.
+    """
+    path = os.path.join(ROOT_PATH, 'mq', 'config.yml')
+    def __enter__(self):
+        with open(self.path, 'w') as mq_file:
+            mq_file.write('')
+        import mq
+        mq.init_config = lambda: {'connection_params': {}}
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            os.remove(self.path)
+        except OSError as e:
+            if e.errno != errno.ENOENT:
+                raise
