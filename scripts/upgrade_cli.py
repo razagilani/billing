9f6b876f49f759d5a7282b45d91b0cae3dc4af19
Bill Van Besien <bvanbesien@nextility.com>
2016-01-21 10:29:40 -0500

    More progress toward NJ page 1

---
+++ scripts/upgrade_cli.py
@@ -0,0 +1,21 @@
+#upgrade_cli.py
+#TODO 76483490: Move this CLI outside of the /billing package.
+
+"""Command-line interface to run a deployment upgrade script.
+
+Use by calling ``$python upgrade_cli.py version_number``
+
+"""
+
+from argparse import ArgumentParser
+parser = ArgumentParser(description='Deploy a software version upgrade.')
+parser.add_argument('version',
+                    help='The version we are upgrading to.')
+args = parser.parse_args()
+
+from core import init_logging, init_config
+init_logging()
+init_config()
+
+from upgrade_scripts import run_upgrade
+run_upgrade(args.version) 
