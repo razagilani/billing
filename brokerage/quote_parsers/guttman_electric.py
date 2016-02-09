558e0c0066b91eaca1b3a81ce0d6db8f1584592a
Raza Gilani <rgilani@skylineinnovations.com>
2016-02-09 13:32:26 -0500

    updated a comment

--- brokerage/quote_parsers/guttman_electric.py
+++ brokerage/quote_parsers/guttman_electric.py
@@ -87,7 +87,7 @@ class GuttmanElectric(QuoteParser):
                                                   valid_from_row, 'C',
                                                   'Published: (.*)',
                                                   parse_datetime)
-            #TODO: set valid_until to the same day at 5:00 PM 
+            #TODO: set valid_until to the same day at 5:00 = 22:00 UTC (in the winter)
             valid_until = valid_from + timedelta(days=1)
 
             for table_start_row in xrange(self.RATE_START_ROW,
