df249619d28738c46b370aa87113bd118e3c398b
? <dklothe@skylineinnovations.com>
2016-02-26 17:39:48 -0500

    convert to pytest

--- test/test_brokerage/test_quote_parsers/test_direct_energy.py
+++ test/test_brokerage/test_quote_parsers/test_direct_energy.py
@@ -1,27 +1,28 @@
 from datetime import datetime
 
 from brokerage.quote_parsers import DirectEnergyMatrixParser
-from test.test_brokerage.test_quote_parsers import QuoteParserTestCase
+
+# this must be imported to get the "quotes" fixture passed as as argument to
+# the test method below
+from test.test_brokerage.test_quote_parsers import quotes
 
 
-class TestDirectEnergy(QuoteParserTestCase):
-    FILE_NAME = 'Matrix 1 Example - Direct Energy.xls'
-    ALIASES = ['Direct-electric-CT-CLP-37, R35--']
-    PARSER_CLASS = DirectEnergyMatrixParser
-    EXPECTED_COUNT = 106554
+# these variables are accessed inside the "quotes" fixture function
+FILE_NAME = 'Matrix 1 Example - Direct Energy.xls'
+ALIASES = ['Direct-electric-CT-CLP-37, R35--']
+PARSER_CLASS = DirectEnergyMatrixParser
+EXPECTED_COUNT = 106554
 
-    def test_direct_energy(self):
-        q = self.quotes[0]
-        self.assertEqual(datetime(2015, 5, 1), q.start_from)
-        self.assertEqual(datetime(2015, 6, 1), q.start_until)
-        self.assertEqual(6, q.term_months)
-        self.assertEqual(datetime(2015, 5, 4), q.valid_from)
-        self.assertEqual(datetime(2015, 5, 5), q.valid_until)
-        self.assertEqual(0, q.min_volume)
-        self.assertEqual(75000, q.limit_volume)
-        self.assertEqual(self.ALIASES[0], q.rate_class_alias)
-        self.assertEqual(self.RATE_CLASS_ID, q.rate_class_id)
-        self.assertEqual(False, q.purchase_of_receivables)
-        self.assertEqual(.07036, q.price)
-
-
+def test_direct_energy(quotes):
+    q = quotes[0]
+    assert q.start_from == datetime(2015, 5, 1)
+    assert q.start_until == datetime(2015, 6, 1)
+    assert q.term_months == 6
+    assert q.valid_from == datetime(2015, 5, 4)
+    assert q.valid_until == datetime(2015, 5, 5)
+    assert q.min_volume == 0
+    assert q.limit_volume == 75000
+    assert q.rate_class_alias == ALIASES[0]
+    assert q.rate_class_id == 1
+    assert q.purchase_of_receivables == False
+    assert q.price == .07036
