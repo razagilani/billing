fd5d311f2109bbc2fbfb3e4616c3db3abe4f4da1
? <dklothe@skylineinnovations.com>
2016-02-29 17:40:32 -0500

    fix test failure

--- reebill/views.py
+++ reebill/views.py
@@ -205,16 +205,16 @@ class Views(object):
                 'service_type': reebill_customer.service if
                 reebill_customer else '',
                 # not shown in grid?
-                # 'ba_addressee': ua.fb_billing_address.addressee,
-                # 'ba_city': ua.fb_billing_address.city,
-                # 'ba_postal_code':ua.fb_billing_address.postal_code,
-                # 'ba_state':ua.fb_billing_address.state,
-                # 'ba_street':ua.fb_billing_address.street,
-                # 'sa_addressee': ua.fb_service_address.addressee,
-                # 'sa_city': ua.fb_service_address.city,
-                # 'sa_postal_code':ua.fb_service_address.postal_code,
-                # 'sa_state':ua.fb_service_address.state,
-                # 'sa_street':ua.fb_service_address.street
+                'ba_addressee': ua.fb_billing_address.addressee,
+                'ba_city': ua.fb_billing_address.city,
+                'ba_postal_code':ua.fb_billing_address.postal_code,
+                'ba_state':ua.fb_billing_address.state,
+                'ba_street':ua.fb_billing_address.street,
+                'sa_addressee': ua.fb_service_address.addressee,
+                'sa_city': ua.fb_service_address.city,
+                'sa_postal_code':ua.fb_service_address.postal_code,
+                'sa_state':ua.fb_service_address.state,
+                'sa_street':ua.fb_service_address.street
             }
 
         rows = list(rows_dict.itervalues())
