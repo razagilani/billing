e96f07946911c6f7737bbc1bcba4afcfef7031bd
? <dklothe@skylineinnovations.com>
2016-02-29 15:52:24 -0500

    merge with BILL-6691-refactor-tests (default bookmark) to get new test pattern

--- test/test_brokerage/test_quote_parser.py
+++ test/test_brokerage/test_quote_parser.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from datetime import datetime
 import re
 from os.path import join, basename
@@ -35,9 +36,11 @@ class QuoteParserTest(TestCase):
         class ExampleQuoteParser(QuoteParser):
             NAME = 'example'
             reader = Mock()
-
             def __init__(self):
-                super(ExampleQuoteParser, self).__init__()
+                super(ExampleQuoteParser, self).__init__(
+                    brokerage_dao=Mock(
+                        load_rate_class_aliases=Mock(
+                            return_value=defaultdict(lambda: [1]))))
                 self.reader = reader
 
             def _load_rate_class_aliases(self):
@@ -83,8 +86,6 @@ class MatrixQuoteParsersTest(TestCase):
     DIRECTORY = join(ROOT_PATH, 'test', 'test_brokerage', 'quote_files')
     AEP_FILE_PATH = join(DIRECTORY,
                          'AEP Energy Matrix 3.0 2015-11-12.xls')
-    DIRECT_ENERGY_FILE_PATH = join(DIRECTORY,
-                                   'Matrix 1 Example - Direct Energy.xls')
     USGE_FILE_PATH = join(DIRECTORY, 'Matrix 2a Example - USGE.xlsx')
     USGE_ELECTRIC_FILE_PATH = join(DIRECTORY, 'USGE Matrix Pricing - ELEC - 20160205.xlsx')
     USGE_ELECTRIC_ANOMALY_PATH = join(DIRECTORY, 'USGEMatrixPricing-ELEC-20151130.xlsx')
@@ -195,36 +196,6 @@ class MatrixQuoteParsersTest(TestCase):
 
     def tearDown(self):
         clear_db()
-
-    def test_direct_energy(self):
-        parser = DirectEnergyMatrixParser()
-        self.assertEqual(0, parser.get_count())
-
-        with open(self.DIRECT_ENERGY_FILE_PATH, 'rb') as spreadsheet:
-            parser.load_file(spreadsheet)
-        parser.validate()
-        self.assertEqual(0, parser.get_count())
-
-        quotes = list(parser.extract_quotes())
-        self.assertEqual(106554, len(quotes))
-        self.assertEqual(106554, parser.get_count())
-        for quote in quotes:
-            quote.validate()
-
-        # since there are so many, only check one
-        q1 = quotes[0]
-        self.assertEqual(datetime(2015, 5, 1), q1.start_from)
-        self.assertEqual(datetime(2015, 6, 1), q1.start_until)
-        self.assertEqual(6, q1.term_months)
-        self.assertEqual(datetime.utcnow().date(), q1.date_received.date())
-        self.assertEqual(datetime(2015, 5, 4), q1.valid_from)
-        self.assertEqual(datetime(2015, 5, 5), q1.valid_until)
-        self.assertEqual(0, q1.min_volume)
-        self.assertEqual(75000, q1.limit_volume)
-        self.assertEqual('Direct-electric-CT-CLP-37, R35--', q1.rate_class_alias)
-        self.assertEqual(self.rate_class.rate_class_id, q1.rate_class_id)
-        self.assertEqual(False, q1.purchase_of_receivables)
-        self.assertEqual(.07036, q1.price)
 
     def test_usge_electric(self):
         parser = USGEElectricMatrixParser()
